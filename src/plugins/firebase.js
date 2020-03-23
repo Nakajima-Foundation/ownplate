19487a35c3a526a6edec1ddb9b8f52ee5f6773bd
Isamu Arimoto <isamu@to-kyo.to>
2020-03-23 21:18:02 +0900

    load firebase user before page loading.

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,17 +1,11 @@
 import firebase from "firebase";
+import firebaseConfig from "@/config/firebase";
 
 // ** 本番環境
 if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
-  });
+  firebase.initializeApp(firebaseConfig);
 }
 
 export const db = firebase.firestore();
 export const storage = firebase.storage();
+export const auth = firebase.auth();
