7f126842639d704c74b22d1479aca1636dd090a8
Isamu Arimoto <isamu@to-kyo.to>
2020-03-24 14:16:25 +0900

    add i18n

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,114 +1,140 @@
 <template>
-  <div>
-    <b-navbar>
-      <template slot="brand">
-        <b-navbar-item tag="router-link" :to="{ path: '/' }">
-          <img
-            src="https://www.gstatic.com/devrel-devsite/prod/vf4743e4237527d72f4be8582639e4a529166b52e9bb628e797b1ed38800b278b/firebase/images/lockup.png"
-            alt="Lightweight UI components for Vue.js based on Bulma"
+<div>
+  <b-navbar>
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img
+          src="https://www.gstatic.com/devrel-devsite/prod/vf4743e4237527d72f4be8582639e4a529166b52e9bb628e797b1ed38800b278b/firebase/images/lockup.png"
+          alt="Lightweight UI components for Vue.js based on Bulma"
           />
-          <!-- <p class="p-font bold logo">
-            OwnPlate
-          </p> -->
-        </b-navbar-item>
-      </template>
+        {{ $t('greeting') }}
+        <!-- <p class="p-font bold logo">
+             OwnPlate
+             </p> -->
+      </b-navbar-item>
+    </template>
 
-      <template slot="end">
-        <b-navbar-dropdown
-          class="b-navbar-item"
-          spaced="true"
-          hoverable
-          right
-          icon-left="github-circle"
+    <template slot="end">
+      <b-navbar-dropdown
+        class="b-navbar-item"
+        spaced="true"
+        hoverable
+        right
+        icon-left="github-circle"
         >
-          <b-navbar-item href="/dist/mypage">
-            <span class="icon">
-              <i class="fas fa-th-list"></i>
-            </span>
-            <span class="nav-item">mypage</span>
-          </b-navbar-item>
-          <b-navbar-item href="/dist/mypage/profile">
-            <span class="icon">
-              <i class="fas fa-user-circle"></i>
-            </span>
-            <span class="nav-item">profile</span>
-          </b-navbar-item>
-          <b-navbar-item href="/dist/mypage/payment">
-            <span class="icon">
-              <i class="fas fa-money-check"></i>
-            </span>
-            <span class="nav-item">setting</span>
-          </b-navbar-item>
-          <b-navbar-item href="/dist/mypage/agreement">
-            <span class="icon">
-              <i class="fas fa-id-card"></i>
-            </span>
-            <span class="nav-item">payment</span>
-          </b-navbar-item>
-          <b-navbar-item href="/dist/mypage/billinghistory">
-            <span class="icon">
-              <i class="fas fa-history"></i>
-            </span>
-            <span class="nav-item">history</span>
-          </b-navbar-item>
-        </b-navbar-dropdown>
-      </template>
-    </b-navbar>
+        <b-navbar-item href="/dist/mypage">
+          <span class="icon">
+            <i class="fas fa-th-list"></i>
+          </span>
+          <span class="nav-item">mypage</span>
+        </b-navbar-item>
+        <b-navbar-item href="/dist/mypage/profile">
+          <span class="icon">
+            <i class="fas fa-user-circle"></i>
+          </span>
+          <span class="nav-item">profile</span>
+        </b-navbar-item>
+        <b-navbar-item href="/dist/mypage/payment">
+          <span class="icon">
+            <i class="fas fa-money-check"></i>
+          </span>
+          <span class="nav-item">setting</span>
+        </b-navbar-item>
+        <b-navbar-item href="/dist/mypage/agreement">
+          <span class="icon">
+            <i class="fas fa-id-card"></i>
+          </span>
+          <span class="nav-item">payment</span>
+        </b-navbar-item>
+        <b-navbar-item href="/dist/mypage/billinghistory">
+          <span class="icon">
+            <i class="fas fa-history"></i>
+          </span>
+          <span class="nav-item">history</span>
+        </b-navbar-item>
+      </b-navbar-dropdown>
+    </template>
+  </b-navbar>
 
-    <!-- pagesのコンポーネントが読み込まれる -->
-    <nuxt style="max-width:100%;" v-if="loaded" ></nuxt>
-    <!-- pagesのコンポーネントが読み込まれる -->
+  <!-- pagesのコンポーネントが読み込まれる -->
+  <nuxt style="max-width:100%;" v-if="loaded" ></nuxt>
+  <!-- pagesのコンポーネントが読み込まれる -->
 
-    <footer class="footer" style="background-color: black!important;">
-      <div class="footer-sections">
-        <div class="columns is-desktop" style="margin-top:-3rem;">
-          <div class="column">
-            <div class="level-left">
-              <p class="level-item p-font">
-                <a>
-                  <span class="icon has-text-info">
-                    <i class="far fa-file-alt"></i>
-                  </span>
-                  Private
-                </a>
-              </p>
-              <p class="level-item p-font">
-                <a>
-                  <span class="icon has-text-info">
-                    <i class="far fa-file-alt"></i>
-                  </span>
-                  Tokushoho
-                </a>
-              </p>
-              <p class="level-item p-font">
-                <a>
-                  <span class="icon has-text-info">
-                    <i class="far fa-file-alt"></i>
-                  </span>
-                  Riyokiyku
-                </a>
-              </p>
-              <p class="level-item p-font">
-                <a>
-                  <span class="icon has-text-info">
-                    <i class="fas fa-info-circle"></i>
-                  </span>
-                  Contact
-                </a>
-              </p>
-            </div>
+  <footer class="footer" style="background-color: black!important;">
+    <div class="footer-sections">
+      <div class="columns is-desktop" style="margin-top:-3rem;">
+        <div class="column">
+          <div class="level-left">
+            <p class="level-item p-font">
+              <a>
+                <span class="icon has-text-info">
+                  <i class="far fa-file-alt"></i>
+                </span>
+                Private
+              </a>
+            </p>
+            <p class="level-item p-font">
+              <a>
+                <span class="icon has-text-info">
+                  <i class="far fa-file-alt"></i>
+                </span>
+                Tokushoho
+              </a>
+            </p>
+            <p class="level-item p-font">
+              <a>
+                <span class="icon has-text-info">
+                  <i class="far fa-file-alt"></i>
+                </span>
+                Riyokiyku
+              </a>
+            </p>
+            <p class="level-item p-font">
+              <a>
+                <span class="icon has-text-info">
+                  <i class="fas fa-info-circle"></i>
+                </span>
+                Contact
+              </a>
+            </p>
           </div>
-          <div class="column">
-            <div class="level-left">
-              <p class="level-item p-font">
-                <a>
-                  <span class="icon has-text-info">
-                    <i class="far fa-envelope"></i>
-                  </span>
-                  Mail
-                </a>
-              </p>
-            </div>
+        </div>
+        <div class="column">
+          <div class="level-left">
+            <p class="level-item p-font">
+              <a>
+                <span class="icon has-text-info">
+                  <i class="far fa-envelope"></i>
+                </span>
+                Mail
+              </a>
+            </p>
+            <p class="level-item p-font">
+              <a>
+                <span class="icon has-text-info">
+                  <i class="fas fa-info-circle"></i>
+                </span>
+                <nuxt-link
+                  v-if="$i18n.locale !== 'en'"
+                  :to="switchLocalePath('en')"
+                  >
+                  English
+                </nuxt-link>
+                <nuxt-link
+                  v-if="$i18n.locale !== 'es'"
+                  :to="switchLocalePath('es')"
+                  >
+                  Español
+                </nuxt-link>
+                <nuxt-link
+                  v-if="$i18n.locale !== 'ja'"
+                  :to="switchLocalePath('ja')"
+                  >
+                  日本語
+                </nuxt-link>
+              </a>
+            </p>
+          </div>
 
             <div class="mainichi-info" style="margin-top:1rem;">
               <p class="p-font-mini">
