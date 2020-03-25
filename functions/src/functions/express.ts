3228893b4007f5d21e469d38cb320a54607ab262
Isamu Arimoto <isamu@to-kyo.to>
2020-03-25 14:17:13 +0900

    update express and test

--- functions/src/functions/express.ts
+++ functions/src/functions/express.ts
@@ -10,10 +10,11 @@ if (!admin.apps.length) {
   admin.initializeApp();
 }
 
-//let db = admin.firestore();
-//export const updateDb = (_db) => {
-//  db = _db;
-//}
+let db = admin.firestore();
+
+export const updateDb = (_db) => {
+  db = _db;
+}
 
 export const logger = async (req, res, next) => {
   next();
@@ -22,43 +23,54 @@ export const hello_response = async (req, res) =>{
   res.json({message: "hello"});
 };
 
-const ogpPage = async (req: any, res: any) => {
-  const { restaurantName } = req.params;
-
-  const escapeHtml = (str: string): string => {
-    if (typeof str !== 'string') {
-      return '';
-    }
-    const mapping:any = {
-      '&': '&amp;',
-      "'": '&#x27;',
-      '`': '&#x60;',
-      '"': '&quot;',
-      '<': '&lt;',
-      '>': '&gt;',
-    };
-    return str.replace(/[&'`"<>]/g, function(match) {
-      return mapping[match]
-    });
+const escapeHtml = (str: string): string => {
+  if (typeof str !== 'string') {
+    return '';
   }
+  const mapping:any = {
+    '&': '&amp;',
+    "'": '&#x27;',
+    '`': '&#x60;',
+    '"': '&quot;',
+    '<': '&lt;',
+    '>': '&gt;',
+  };
+  return str.replace(/[&'`"<>]/g, function(match) {
+    return mapping[match]
+    });
+}
 
-  const title = "Adana Restaurant";
+
+const ogpPage = async (req: any, res: any) => {
+
+  const { restaurantName } = req.params;
+  const restaurant = await db.doc(`restaurants/${restaurantName}`).get();
+
+  const template_data = fs.readFileSync('./templates/index.html',  {encoding: 'utf8'});
+
+  if (!restaurant || !restaurant.exists) {
+    return res.status(404).send(template_data);
+  }
+  const restaurant_data: any = restaurant.data();
+
+  const title = restaurant_data.restaurantName;
+  const image = restaurant_data.restProfilePhoto;
+
+  const regex = /<title.*title>/;
+  const metas =
+    [
+      `<title>${escapeHtml(title)}</title>`,
+      `<meta property="og:title" content="${escapeHtml(title)}" />`,
+      `<meta property="og:site_name" content="${escapeHtml(title)}" />`,
+      `<meta property="og:type" content="website" />`,
+      `<meta property="og:url" content="https://staging.ownplate.today/r/${restaurantName}" />`,
+      `<meta property="og:description" content="Japanese comfort food" />`,
+      `<meta property="og:image" content="${image}" />`,
+    ].join("\n");
 
   res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
-  fs.readFile('./templates/index.html', 'utf8', (err, data) => {
-    const regex = /<title.*title>/
-    const metas =
-      [
-        `<title>${escapeHtml(title)}</title>`,
-        `<meta property="og:title" content="${escapeHtml(title)}" />`,
-        `<meta property="og:site_name" content="${escapeHtml(title)}" />`,
-        `<meta property="og:type" content="website" />`,
-        `<meta property="og:url" content="https://staging.ownplate.today/r/${restaurantName}" />`,
-        `<meta property="og:description" content="Japanese comfort food" />`,
-        `<meta property="og:image" content="https://static.wixstatic.com/media/bafa8d_9cd1d487e24140aba7b6885e198cb62e~mv2_d_4928_3264_s_4_2.jpg/v1/fill/w_980,h_1350,al_c,q_85,usm_0.66_1.00_0.01/bafa8d_9cd1d487e24140aba7b6885e198cb62e~mv2_d_4928_3264_s_4_2.webp" />`,
-      ].join("\n");
-    res.send(data.replace(regex, metas));
-  });
+  res.send(template_data.replace(regex, metas));
+
 };
 
 router.get('/hello',
--- functions/tests/express_test.ts
+++ functions/tests/express_test.ts
@@ -3,26 +3,74 @@ import { should } from 'chai';
 
 import * as express from '../src/functions/express';
 import * as test_helper from './test_helper';
+import * as test_db_helper from './test_db_helper';
 
 should();
 
+const adminDB = test_db_helper.adminDB();
+
+express.updateDb(adminDB);
 const app = express.app;
 const request = supertest(app);
 
+test_db_helper.initHook();
+
+const good_cafe_data = {
+  restProfilePhoto: "https://example.com/images",
+  restaurantName: "Good cafe",
+  streetAddress: "NY",
+  city: "NY",
+  state: "NY",
+  zip: "123",
+  phoneNumber: "+01-0000-0000",
+  url: "https://example.com",
+  uid: "123123",
+  defaultTaxRate: 10,
+  publicFlag: true,
+};
+
 describe('express function', () => {
-  it ('express test', async function() {
+  before(async () => {
+    await adminDB.doc(`restaurants/testbar`).set(good_cafe_data);
+  });
+
+  it ('express simple test', async function() {
     const response = await request.get('/users');
     response.status.should.equal(404);
 
     const hello_response = await request.get('/1.0/hello');
     hello_response.status.should.equal(200);
 
-    const restaurant_response = await request.get('/r/hello');
+
+    const restaurant_response = await request.get('/r/testbar');
     restaurant_response.status.should.equal(200);
 
     const meta_tag = test_helper.parse_meta(restaurant_response.text);
 
     meta_tag['og:title'].should.not.empty;
+    meta_tag['og:site_name'].should.not.empty;
+    meta_tag['og:type'].should.not.empty;
+
+    const restaurant_error_response = await request.get('/r/testbar2');
+    restaurant_error_response.status.should.equal(404);
+  });
+
+
+  it ('express simple test', async function() {
+    // const db_data = await adminDB.doc(`restaurants/testbar`).get();
+    const response = await request.get('/users');
+    response.status.should.equal(404);
+
+    const hello_response = await request.get('/1.0/hello');
+    hello_response.status.should.equal(200);
+
+    const restaurant_response = await request.get('/r/testbar');
+    restaurant_response.status.should.equal(200);
+
+    const meta_tag = test_helper.parse_meta(restaurant_response.text);
+
+    meta_tag['og:title'].should.not.empty;
+    meta_tag['og:title'].should.equal(good_cafe_data.restaurantName);
     meta_tag['og:site_name'].should.not.empty;
     meta_tag['og:type'].should.not.empty;
 
