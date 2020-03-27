686b881e3815693cdab105f4609cc86ba43523e3
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-03-27 06:28:56 -0700

    input

--- src/pages/admin/restaurants/create.vue
+++ src/pages/admin/restaurants/create.vue
@@ -1,57 +1,21 @@
 <template>
   <span>
-    <section class="hero is-info">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title">
-            <span class="under_line">PAGE</span>
-          </h1>
-        </div>
-      </div>
-    </section>
-
     <section class="section" style="background-color:#fffafa">
-      <div class="columns is-mobile is-centered">
-        PageList
-      </div>
-
-      <div class="content">
-        <ol type="A">
-          <div class="columns is-mobile is-left">
-            <p class="p-bold">【Reataurant】</p>
-          </div>
-          <li><router-link to="/r/1710478afd0304e4974">Menu</router-link></li>
-          <li><router-link to="/r/1710478afd0304e4974/order/123456789/">Order</router-link></li>
-          <li><router-link to="/r/1710478afd0304e4974/order/123456789/thanks">ThankYou</router-link></li>
-        </ol>
-      </div>
+      <input type="text" v-model="restaurantId" placeholder="Restaurant Id" />
+      <p>URL: https://ownplate.today/r/{{ restaurantId }}</p>
     </section>
   </span>
 </template>
 
-<script>
+<script> 
 export default {
-  name: "HomePage",
-
-  components: {},
   data() {
-    return {};
-  },
-
-  mounted() {},
-
-  methods: {
+    return {
+      restaurantId:""
+    };
   }
 };
 </script>
 
 <style lang="scss">
-.hero {
-  .title {
-    color: rgb(255, 255, 255) !important;
-    .under_line {
-      background: linear-gradient(transparent 70%, #ffe600 40%);
-    }
-  }
-}
 </style>
