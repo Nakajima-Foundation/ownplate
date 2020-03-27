9c2fe8abf12b656ffa9487e2e0abf11aa2840863
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-03-27 10:55:16 -0700

    form and firebase.functions

--- src/pages/admin/restaurants/create.vue
+++ src/pages/admin/restaurants/create.vue
@@ -1,16 +1,19 @@
 <template>
   <span>
     <section class="section" style="background-color:#fffafa">
-      <input type="text" v-model="restaurantId" placeholder="Restaurant Id" 
-       v-on:input="validate"/>
-      <p>{{errors}}</p>
-      <p>URL: https://ownplate.today/r/{{ restaurantId }}</p>
+      <form @submit.prevent="handleSubmit">
+        <input type="text" v-model="restaurantId" placeholder="Restaurant Id" 
+        v-on:input="validate"/>
+        <p>{{errors}}</p>
+        <p>URL: https://ownplate.today/r/{{ restaurantId }}</p>
+        <button type="submit">Submit</button>
+      </form>
     </section>
   </span>
 </template>
 
 <script> 
-import { db } from "~/plugins/firebase.js";
+import { db, functions } from "~/plugins/firebase.js";
 export default {
   data() {
     return {
@@ -39,6 +42,13 @@ export default {
       if (doc.exists) {
         this.errors.push("already taken")
       }
+    },
+    async handleSubmit() {
+      console.log("handleSubmit");
+      const context = { restaurantId:this.restaurantId };
+      const createRestaurant = functions.httpsCallable('createRestaurant');
+      const result = (await createRestaurant(context)).data;
+      console.log("result", result);
     }
   }
 };
--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -2,6 +2,7 @@ import * as firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/storage";
 import "firebase/auth";
+import "firebase/functions";
 
 import firebaseConfig from "@/config/firebase";
 
@@ -13,3 +14,4 @@ if (!firebase.apps.length) {
 export const db = firebase.firestore();
 export const storage = firebase.storage();
 export const auth = firebase.auth();
+export const functions = firebase.functions();
