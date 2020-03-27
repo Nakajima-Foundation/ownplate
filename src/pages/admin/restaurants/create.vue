7e1aa022cd0a86878065a18db1302abad954bebb
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-03-27 06:43:26 -0700

    validate

--- src/pages/admin/restaurants/create.vue
+++ src/pages/admin/restaurants/create.vue
@@ -1,7 +1,9 @@
 <template>
   <span>
     <section class="section" style="background-color:#fffafa">
-      <input type="text" v-model="restaurantId" placeholder="Restaurant Id" />
+      <input type="text" v-model="restaurantId" placeholder="Restaurant Id" 
+       v-on:input="validate"/>
+      <p>{{errors}}</p>
       <p>URL: https://ownplate.today/r/{{ restaurantId }}</p>
     </section>
   </span>
@@ -11,8 +13,18 @@
 export default {
   data() {
     return {
+      errors:["no error 1"],
       restaurantId:""
     };
+  },
+  methods: {
+    validate() {
+      console.log("validate", this.restaurantId)
+      this.errors = [];
+      if (this.restaurantId.length < 5) {
+        this.errors.push("too short")
+      }
+    }
   }
 };
 </script>
