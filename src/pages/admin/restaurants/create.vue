6ebc3ae058ce4a2c1d246b04370f82fddd51eaa5
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-03-27 05:54:41 -0700

    create restaurant page

--- src/pages/admin/restaurants/create.vue
+++ src/pages/admin/restaurants/create.vue
@@ -23,14 +23,6 @@
           <li><router-link to="/r/1710478afd0304e4974">Menu</router-link></li>
           <li><router-link to="/r/1710478afd0304e4974/order/123456789/">Order</router-link></li>
           <li><router-link to="/r/1710478afd0304e4974/order/123456789/thanks">ThankYou</router-link></li>
-          <div class="columns is-mobile is-left">
-            <p class="p-bold" style="margin-top:2rem;">【Admin】</p>
-          </div>
-          <li><router-link to="/admin/restaurants/create">Create a new Reataurant (new)</router-link></li>
-          <li><router-link to="/admin/restaurants/new">Create a new Reataurant</router-link></li>
-          <li><router-link to="/admin/restaurants/">Your Reataurant</router-link></li>
-          <li><router-link to="/admin/user/signin">Admin Signin</router-link></li>
-          <li><router-link to="/admin/user/signup">Admin Signup</router-link></li>
         </ol>
       </div>
     </section>
@@ -49,38 +41,11 @@ export default {
   mounted() {},
 
   methods: {
-    sessionClear() {
-      alert("クリアしました");
-      window.localStorage.clear();
-    }
   }
-
-  // APIcall
-  // async mounted() {
-  //   await this.$axios.$post('/users', { name: 'yoko' })
-
-  //   const users = await this.$axios.$get('/users')
-  //   console.log(users) // [{ id: 0, name: 'taro' }, { id: 1, name: 'yoko' }]
-  // }
 };
 </script>
 
 <style lang="scss">
-@include mq(spmd) {
-  // 引数を個別に指定
-  .second_section {
-    .column {
-      text-align: center !important;
-    }
-  }
-}
-
-.second_section {
-  max-width: 600px;
-  margin-left: auto;
-  margin-right: auto;
-}
-
 .hero {
   .title {
     color: rgb(255, 255, 255) !important;
