b10cc6d3fc2649c4180fdf0122067bd095e53fe7
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-03-29 16:53:53 -0700

    ul and li

---
+++ src/pages/admin/restaurants/_restaurantId/orders/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="section" style="background-color:#fffafa">
+    <h2 class="p-big bold">
+      Orders
+    </h2>
+    <ul>
+      <li v-for="order in orders" :key="order.id">{{ order.title }}</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import { db } from "~/plugins/firebase.js";
+
+export default {
+  data() {
+    return {
+      orders: [{
+        id:111,
+        title:"one"
+      }, {
+        id:222,
+        title:"two"
+      }, {
+        id:333,
+        title:"three"
+      }]
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
