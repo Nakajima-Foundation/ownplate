564ed1fe20403a71a907ae20919e6b8180baaa86
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-03-30 11:15:24 -0700

    Back Button

---
+++ src/components/BackButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-button
+    style="margin-right:auto"
+    type="is-primary"
+    class="counter-button"
+    icon-left="arrow-left"
+    rounded
+    outlined
+    @click="goBack()"
+  >
+    Back
+  </b-button>
+</template>
+
+<script>
+export default {
+  methods: {
+    goBack() {
+      this.$router.push({
+        path: `/admin/restaurants/`
+      });
+    }
+  }
+}
+</script>
