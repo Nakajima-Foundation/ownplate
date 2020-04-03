0641711a458be8380a17028fb498cbe410180950
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-04-02 17:00:49 -0700

    test code

---
+++ src/pages/test/sms.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>Hello World</p>
+    <button id="signInButton">Foo</button>
+  </div>
+</template>
+
+<script>
+import { RecaptchaVerifier } from "~/plugins/firebase.js";
+
+export default {
+  mounted() {
+    console.log("mounted", RecaptchaVerifier);
+    window.recaptchaVerifier = new RecaptchaVerifier('signInButton', {
+      'size': 'invisible',
+      'callback': function(response) {
+      // reCAPTCHA solved, allow signInWithPhoneNumber.
+      console.log("onSignInSubmit");
+    }
+});
+  }
+}
+</script>
--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -15,3 +15,4 @@ export const db = firebase.firestore();
 export const storage = firebase.storage();
 export const auth = firebase.auth();
 export const functions = firebase.functions();
+export const RecaptchaVerifier = firebase.auth.RecaptchaVerifier;
