ac832f524aa8fcf7ee9a706e41f99766dd772646
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-04-06 21:48:04 -0700

    quick fix for user

--- src/pages/r/_restaurantId/index.vue
+++ src/pages/r/_restaurantId/index.vue
@@ -162,7 +162,7 @@ export default {
   methods: {
     handleCheckOut() {
       // The user has clicked the CheckOut button
-      if (this.user.phoneNumber) {
+      if (this.user && this.user.phoneNumber) {
         this.goCheckout();
       } else {
         this.loginVisible = true;
