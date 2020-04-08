e4fb7579d5325fc880347f4afe435608c4161d48
Isamu Arimoto <isamu@to-kyo.to>
2020-04-09 06:08:26 +0900

    add order counter test

--- functions/tests/order_test.ts
+++ functions/tests/order_test.ts
@@ -1,9 +1,38 @@
 import { should } from 'chai';
+import { expect } from 'chai';
+
+import * as firestore from './../src/functions/firestore'
+
+import * as constant from './../src/common/constant';
+import * as test_db_helper from './test_db_helper';
+
+const adminDB = test_db_helper.adminDB();
 
 should()
 
 describe('Order function', () => {
-  it ('Order function test', async function() {
+  it ('Order function, orderCounter test', async function() {
+    // create restaurant.
+    await adminDB.doc(`restaurants/testbar`).set({
+      orderCount: 10,
+    });
+    // create order
+    await adminDB.doc(`restaurants/testbar/orders/hoge`).set({
+      status: constant.order_status.new_order
+    });
+
+    const orderdata = await adminDB.doc(`restaurants/testbar/orders/hoge`).get();
+
+    //  get order
+    await firestore.orderCreate(adminDB, orderdata, {});
+
+    const restaurantDoc = await adminDB.doc(`restaurants/testbar`).get();
+    expect(restaurantDoc).to.exist;
+    const data = restaurantDoc.data();
+    expect(data).to.exist;
+    if (data) {
+      data.orderCount.should.equal(11);
+    }
     true.should.equal(true);
   })
 });
