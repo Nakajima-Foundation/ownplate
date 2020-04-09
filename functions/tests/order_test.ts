9c7910d5aa041f1ae4fac05a6d52fa18ff1db663
Isamu Arimoto <isamu@to-kyo.to>
2020-04-09 10:43:29 +0900

    add error handle.

--- functions/tests/order_test.ts
+++ functions/tests/order_test.ts
@@ -13,25 +13,62 @@ should()
 describe('Order function', () => {
   it ('Order function, orderCounter test', async function() {
     // create restaurant.
-    await adminDB.doc(`restaurants/testbar`).set({
+    await adminDB.doc(`restaurants/testbar1`).set({
       orderCount: 10,
       foodTax: 5,
       alcoholTax: 8,
     });
     // create menu.
-    await adminDB.doc(`restaurants/testbar/menus/hoge1`).set({
+    await adminDB.doc(`restaurants/testbar1/menus/hoge1`).set({
       price: 100,
       tax: "food",
     });
 
-    await adminDB.doc(`restaurants/testbar/menus/hoge2`).set({
+    // create order
+    await adminDB.doc(`restaurants/testbar1/orders/hoge`).set({
+      status: constant.order_status.new_order,
+      order: {
+        hoge1: 10,
+      },
+    });
+
+    //  get order
+    const orderdata = await adminDB.doc(`restaurants/testbar1/orders/hoge`).get();
+
+    // call function
+    await firestore.orderCreate(adminDB, orderdata, {});
+
+    // check order counter on restaurant
+    const restaurantDoc = await adminDB.doc(`restaurants/testbar1`).get();
+    expect(restaurantDoc).to.exist;
+    const data = restaurantDoc.data();
+    expect(data).to.exist;
+    if (data) {
+      data.orderCount.should.equal(11);
+    }
+  })
+
+  it ('Order function, calculate test', async function() {
+    // create restaurant.
+    await adminDB.doc(`restaurants/testbar2`).set({
+      orderCount: 10,
+      foodTax: 5,
+      alcoholTax: 8,
+    });
+    // create menu.
+    await adminDB.doc(`restaurants/testbar2/menus/hoge1`).set({
+      price: 100,
+      tax: "food",
+    });
+
+    await adminDB.doc(`restaurants/testbar2/menus/hoge2`).set({
       price: 50,
       tax: "alcohol",
     });
 
 
     // create order
-    await adminDB.doc(`restaurants/testbar/orders/hoge`).set({
+    await adminDB.doc(`restaurants/testbar2/orders/hoge`).set({
       status: constant.order_status.new_order,
       order: {
         hoge1: 10,
@@ -40,22 +77,13 @@ describe('Order function', () => {
     });
 
     //  get order
-    const orderdata = await adminDB.doc(`restaurants/testbar/orders/hoge`).get();
+    const orderdata = await adminDB.doc(`restaurants/testbar2/orders/hoge`).get();
 
     // call function
     await firestore.orderCreate(adminDB, orderdata, {});
 
-    // check order counter on restaurant
-    const restaurantDoc = await adminDB.doc(`restaurants/testbar`).get();
-    expect(restaurantDoc).to.exist;
-    const data = restaurantDoc.data();
-    expect(data).to.exist;
-    if (data) {
-      data.orderCount.should.equal(11);
-    }
-
     //  check order
-    const newOrderDoc = await adminDB.doc(`restaurants/testbar/orders/hoge`).get();
+    const newOrderDoc = await adminDB.doc(`restaurants/testbar2/orders/hoge`).get();
     const newOrderData = newOrderDoc.data();
 
     const sub_total = 100 * 10 + 50 * 5;
@@ -69,4 +97,43 @@ describe('Order function', () => {
       newOrderData.total.should.equal(total);
     }
   })
+
+  it ('Order function, error test', async function() {
+    // create restaurant.
+    await adminDB.doc(`restaurants/testbar3`).set({
+      orderCount: 10,
+      foodTax: 5,
+      alcoholTax: 8,
+    });
+    // create menu.
+    await adminDB.doc(`restaurants/testbar3/menus/hoge1`).set({
+      price: 100,
+      tax: "food",
+    });
+    // create order
+    await adminDB.doc(`restaurants/testbar3/orders/hoge`).set({
+      status: constant.order_status.new_order,
+      order: {
+        hoge1: 10,
+        hoge2: 5,
+      },
+    });
+
+    //  get order
+    const orderdata = await adminDB.doc(`restaurants/testbar3/orders/hoge`).get();
+
+    // call function
+    await firestore.orderCreate(adminDB, orderdata, {});
+
+    //  check order
+    const newOrderDoc = await adminDB.doc(`restaurants/testbar3/orders/hoge`).get();
+    const newOrderData = newOrderDoc.data();
+
+    expect(newOrderData).to.exist;
+    if (newOrderData) {
+      newOrderData.status.should.equal(constant.order_status.error);
+    }
+  })
+
+
 });
