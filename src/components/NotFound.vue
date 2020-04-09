bf907278857936e826d2c66e4c72ee073ea9a5e3
Isamu Arimoto <isamu@to-kyo.to>
2020-04-10 06:42:58 +0900

    add 404 image

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    404
+<div>
+  <div class="notFoundPage">
+    <p>
+      <img src="@/assets/404_logo.svg" class="image" />
+    </p>
   </div>
+  <div class="notFoundPageText">
+      The page you're looking for can't be found.<br />
+      Try contacting the restaurant in other way
+  </div>
+</div>
 </template>
+
+<style lang="scss" scoped>
+.notFoundPageText {
+  text-align: center;
+  margin-bottom: 80px;
+  font-size: 1.5rem;
+}
+.image {
+  width: 50%;
+  margin: auto;
+  margin-top: 150px;
+  margin-bottom: 80px;
+}
+.notFoundPage {
+  margin: auto;
+}
+</style>
