4bd295b9ce2b64ccd01628c10d823cd41b1b3c55
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-04-10 14:23:36 -0700

    no single instance

--- src/plugins/stripe.js
+++ src/plugins/stripe.js
@@ -1,10 +1,6 @@
-
-let stripe = null; // Single Instance
-
 export const getStripeInstance = () => {
-	if (stripe === null) {
-		const stripeAPIToken = process.env.STRIPE_API_KEY;
-		stripe = Stripe(stripeAPIToken);
-	}
-	return stripe;
+	const stripeAPIToken = process.env.STRIPE_API_KEY;
+	return Stripe(stripeAPIToken);
 }
+
+// LATER: Move the client-side Stripe API here, such as checkoutCreate
