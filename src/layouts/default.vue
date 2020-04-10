82083d283a4bc78236b49021722596ec66b53c10
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-04-09 21:20:35 -0700

    simplified footer

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,170 +1,74 @@
 <template>
-<div>
-  <b-navbar>
-    <template slot="brand">
-      <b-navbar-item tag="router-link" :to="{ path: '/' }">
-        <img
-          src="/OwnPlate-Logo-Horizontal-YellowWhite.svg"
-          alt="Lightweight UI components for Vue.js based on Bulma"
-          />
-        <!-- <p class="p-font bold logo">
-             OwnPlate
-             </p> -->
-      </b-navbar-item>
-    </template>
+  <div>
+    <b-navbar>
+      <template slot="brand">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <img
+            src="/OwnPlate-Logo-Horizontal-YellowWhite.svg"
+            alt="Lightweight UI components for Vue.js based on Bulma"
+            />
+        </b-navbar-item>
+      </template>
 
-    <template slot="end">
-      <b-navbar-dropdown
-        class="b-navbar-item"
-        spaced="true"
-        hoverable
-        right
-        icon-left="github-circle"
-        >
-        <b-navbar-item href="/dist/mypage">
-          <span class="icon">
-            <i class="fas fa-th-list"></i>
-          </span>
-          <span class="nav-item">mypage</span>
-        </b-navbar-item>
-        <b-navbar-item href="/dist/mypage/profile">
-          <span class="icon">
-            <i class="fas fa-user-circle"></i>
-          </span>
-          <span class="nav-item">profile</span>
-        </b-navbar-item>
-        <b-navbar-item href="/dist/mypage/payment">
-          <span class="icon">
-            <i class="fas fa-money-check"></i>
-          </span>
-          <span class="nav-item">setting</span>
-        </b-navbar-item>
-        <b-navbar-item href="/dist/mypage/agreement">
-          <span class="icon">
-            <i class="fas fa-id-card"></i>
-          </span>
-          <span class="nav-item">payment</span>
-        </b-navbar-item>
-        <b-navbar-item href="/dist/mypage/billinghistory">
-          <span class="icon">
-            <i class="fas fa-history"></i>
-          </span>
-          <span class="nav-item">history</span>
-        </b-navbar-item>
-        <b-navbar-item v-if="hasUser" @click="signout">
-          <span class="icon">
-            <i class="fas fa-sign-out-alt"></i>
-          </span>
-          <span class="nav-item">Sign Out</span>
-        </b-navbar-item>
-      </b-navbar-dropdown>
-    </template>
-  </b-navbar>
+      <template slot="end">
+        <b-navbar-dropdown
+          class="b-navbar-item"
+          spaced="true"
+          hoverable
+          right
+          icon-left="github-circle"
+          >
+          <b-navbar-item href="/dist/mypage">
+            <span class="icon">
+              <i class="fas fa-th-list"></i>
+            </span>
+            <span class="nav-item">mypage</span>
+          </b-navbar-item>
+          <b-navbar-item href="/dist/mypage/profile">
+            <span class="icon">
+              <i class="fas fa-user-circle"></i>
+            </span>
+            <span class="nav-item">profile</span>
+          </b-navbar-item>
+          <b-navbar-item href="/dist/mypage/payment">
+            <span class="icon">
+              <i class="fas fa-money-check"></i>
+            </span>
+            <span class="nav-item">setting</span>
+          </b-navbar-item>
+          <b-navbar-item href="/dist/mypage/agreement">
+            <span class="icon">
+              <i class="fas fa-id-card"></i>
+            </span>
+            <span class="nav-item">payment</span>
+          </b-navbar-item>
+          <b-navbar-item href="/dist/mypage/billinghistory">
+            <span class="icon">
+              <i class="fas fa-history"></i>
+            </span>
+            <span class="nav-item">history</span>
+          </b-navbar-item>
+          <b-navbar-item v-if="hasUser" @click="signout">
+            <span class="icon">
+              <i class="fas fa-sign-out-alt"></i>
+            </span>
+            <span class="nav-item">Sign Out</span>
+          </b-navbar-item>
+        </b-navbar-dropdown>
+      </template>
+    </b-navbar>
 
-  <!-- pagesのコンポーネントが読み込まれる -->
-  <nuxt style="max-width:100%;background:#FBF9F9" v-if="$store.getters.userWasInitialized"></nuxt>
-  <!-- pagesのコンポーネントが読み込まれる -->
+    <!-- approproate component under pages will be displayed -->
+    <nuxt style="max-width:100%;background:#FBF9F9" v-if="$store.getters.userWasInitialized"></nuxt>
 
-  <footer class="footer">
-    <div class="footer-sections">
-      <div class="columns is-desktop" style="margin-top:-3rem;">
-        <div class="column">
-          <div class="level-left">
-            <p class="level-item p-font">
-              <a>
-                <span class="icon has-text-info">
-                  <i class="far fa-file-alt"></i>
-                </span>
-                Private
-              </a>
-            </p>
-            <p class="level-item p-font">
-              <a>
-                <span class="icon has-text-info">
-                  <i class="far fa-file-alt"></i>
-                </span>
-                Tokushoho
-              </a>
-            </p>
-            <p class="level-item p-font">
-              <a>
-                <span class="icon has-text-info">
-                  <i class="far fa-file-alt"></i>
-                </span>
-                Riyokiyku
-              </a>
-            </p>
-            <p class="level-item p-font">
-              <a>
-                <span class="icon has-text-info">
-                  <i class="fas fa-info-circle"></i>
-                </span>
-                Contact
-              </a>
+    <footer class="footer">
+      <div class="footer-sections">
+        <div class="columns is-desktop" style="margin-top:-3rem;">
+          <div class="content has-text-left" style="color:white">
+            <p class="p-font-mini">
+              'Operated by Singularity Society.'
             </p>
           </div>
-        </div>
-        <div class="column">
-          <div class="level-left">
-            <p class="level-item p-font">
-              <a>
-                <span class="icon has-text-info">
-                  <i class="far fa-envelope"></i>
-                </span>
-                Mail
-              </a>
-            </p>
-            <p class="level-item p-font">
-              <a>
-                <span class="icon has-text-info">
-                  <i class="fas fa-info-circle"></i>
-                </span>
-                <nuxt-link
-                  v-if="$i18n.locale !== 'en'"
-                  :to="switchLocalePath('en')"
-                  >
-                  English
-                </nuxt-link>
-                <nuxt-link
-                  v-if="$i18n.locale !== 'es'"
-                  :to="switchLocalePath('es')"
-                  >
-                  Español
-                </nuxt-link>
-                <nuxt-link
-                  v-if="$i18n.locale !== 'ja'"
-                  :to="switchLocalePath('ja')"
-                  >
-                  日本語
-                </nuxt-link>
-              </a>
-            </p>
-          </div>
-
-            <div class="mainichi-info" style="margin-top:1rem;">
-              <p class="p-font-mini">
-                TEL
-                <span class="title" style="margin-left:1rem;">
-                  0120-000-000
-                </span>
-              </p>
-              <p class="p-font-mini">
-                hugahuga 000-0000-0000
-              </p>
-              <p class="p-font-mini">
-                TIme AM10:00〜PM5（Excluding weekends and holidays）
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div class="content has-text-left">
-          <p class="p-font-mini">
-            hogahugapiyo
-          </p>
-          <p class="p-font-mini">
-            Copyright Nakajima. All rights reserved.
-          </p>
         </div>
       </div>
     </footer>
@@ -248,12 +152,8 @@ export default {
   padding-left: 0.8rem;
 }
 
-.mainichi-info .p-font-mini {
+.p-font-mini {
   line-height: 1.6rem;
 }
 
-.logo {
-  color: rgb(255, 255, 255);
-  font-weight: 900;
-}
 </style>
