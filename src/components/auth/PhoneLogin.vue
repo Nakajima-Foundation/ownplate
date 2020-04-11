214a796c3ce4db0672233a8aef32fb0ea43a7443
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-04-11 09:18:18 -0700

    removed phone number

--- src/components/auth/PhoneLogin.vue
+++ src/components/auth/PhoneLogin.vue
@@ -1,67 +1,58 @@
 <template>
   <div>
-    <form
-      v-show="confirmationResult === null"
-      @submit.prevent="handleSubmit">
-      <b-field
-        :label="$t('sms.countryCode')">
-        <b-select
-          v-model="countryCode">
+    <form v-show="confirmationResult === null" @submit.prevent="handleSubmit">
+      <b-field :label="$t('sms.countryCode')">
+        <b-select v-model="countryCode">
           <option
             v-for="country in countries"
             :value="country.code"
-            :key="country.code">
-            {{ $t(country.name) }}
-          </option>
+            :key="country.code"
+          >{{ $t(country.name) }}</option>
         </b-select>
       </b-field>
       <b-field
         :type="hasError ? 'is-danger' : 'is-success'"
         :message="hasError ? $t(errors[0]) : $t('sms.notice')"
-        :label="$t('sms.phonenumber')">
-        <b-input type="text"
+        :label="$t('sms.phonenumber')"
+      >
+        <b-input
+          type="text"
           v-model="phoneNumber"
           v-on:input="validatePhoneNumber"
           maxlength="30"
-          :placeholder="$t('sms.pleasetype')" />
+          :placeholder="$t('sms.pleasetype')"
+        />
       </b-field>
-      <div
-        id="signInButton"
-        style="margin-bottom:0.5rem" />
+      <div id="signInButton" style="margin-bottom:0.5rem" />
       <b-button
         type="is-primary"
         :loading="isLoading"
         @click="handleSubmit"
-        :disabled="!readyToSendSMS">
-        {{$t('sms.send')}}
-      </b-button>
-      <b-button @click="$emit('dismissed')">
-        {{$t('button.cancel')}}
-      </b-button>
+        :disabled="!readyToSendSMS"
+      >{{$t('sms.send')}}</b-button>
+      <b-button @click="$emit('dismissed')">{{$t('button.cancel')}}</b-button>
     </form>
-    <form
-      v-if="confirmationResult !== null"
-      @submit.prevent="handleCode">
+    <form v-if="confirmationResult !== null" @submit.prevent="handleCode">
       <b-field
         :type="hasError ? 'is-danger' : 'is-success'"
         :message="hasError ? $t(errors[0]) : ''"
-        :label="$t('sms.verificationCode')">
-        <b-input type="text"
+        :label="$t('sms.verificationCode')"
+      >
+        <b-input
+          type="text"
           v-model="verificationCode"
           v-on:input="validateVerificationCode"
           maxlength="16"
-          :placeholder="$t('sms.typeVerificationCode')" />
+          :placeholder="$t('sms.typeVerificationCode')"
+        />
       </b-field>
       <b-button
         type="is-primary"
         :loading="isLoading"
         @click="handleCode"
-        :disabled="!readyToSendVerificationCode">
-        {{$t('sms.sendVerificationCode')}}
-      </b-button>
-      <b-button @click="$emit('dismissed')">
-        {{$t('button.cancel')}}
-      </b-button>
+        :disabled="!readyToSendVerificationCode"
+      >{{$t('sms.sendVerificationCode')}}</b-button>
+      <b-button @click="$emit('dismissed')">{{$t('button.cancel')}}</b-button>
     </form>
   </div>
 </template>
@@ -74,30 +65,30 @@ export default {
     return {
       isLoading: false,
       countries: [
-        { code:"+1", name:"sms.country.us" },
-        { code:"+81", name:"sms.country.ja" },
+        { code: "+1", name: "sms.country.us" },
+        { code: "+81", name: "sms.country.ja" }
       ],
       countryCode: "+1",
-      phoneNumber:"650-555-1234",
-      errors:[],
-      recaptchaVerifier : () => {},
+      phoneNumber: "",
+      errors: [],
+      recaptchaVerifier: () => {},
       recaptchaVerified: false,
       recaptchaWidgetId: null,
       confirmationResult: null,
       verificationCode: "",
       result: {}
-    }
+    };
   },
   mounted() {
-    this.recaptchaVerifier = new authObject.RecaptchaVerifier('signInButton', {
-      'size': 'normal',
-      'callback': (response) => {
+    this.recaptchaVerifier = new authObject.RecaptchaVerifier("signInButton", {
+      size: "normal",
+      callback: response => {
         // reCAPTCHA solved, allow signInWithPhoneNumber.
         console.log("verified", response);
         this.recaptchaVerified = true;
       }
     });
-    this.recaptchaVerifier.render().then((widgetId) => {
+    this.recaptchaVerifier.render().then(widgetId => {
       this.recaptchaWidgetId = widgetId;
       console.log("widdgetId", widgetId);
     });
@@ -110,33 +101,36 @@ export default {
       return !this.hasError;
     },
     hasError() {
-      return this.errors.length > 0
+      return this.errors.length > 0;
     }
   },
   methods: {
     validatePhoneNumber() {
-      this.errors = []
-      const regex = /^[0-9()\-]*$/
+      this.errors = [];
+      const regex = /^[0-9()\-]*$/;
       if (!regex.test(this.phoneNumber)) {
-        this.errors.push("sms.invalidPhoneNumber")
+        this.errors.push("sms.invalidPhoneNumber");
       }
     },
     validateVerificationCode() {
-      this.errors = []
-      const regex = /^[0-9]*$/
+      this.errors = [];
+      const regex = /^[0-9]*$/;
       if (!regex.test(this.verificationCode)) {
-        this.errors.push("sms.invalidValidationCode")
+        this.errors.push("sms.invalidValidationCode");
       }
     },
     async handleSubmit() {
       console.log("submit");
       try {
         this.isLoading = true;
-        this.confirmationResult = await auth.signInWithPhoneNumber(this.countryCode + this.phoneNumber, this.recaptchaVerifier);
+        this.confirmationResult = await auth.signInWithPhoneNumber(
+          this.countryCode + this.phoneNumber,
+          this.recaptchaVerifier
+        );
         console.log("result", this.confirmationResult);
-      } catch(error) {
+      } catch (error) {
         console.log("error", error);
-        this.errors = [ "sms." + error.code ];
+        this.errors = ["sms." + error.code];
       } finally {
         this.isLoading = false;
       }
@@ -146,18 +140,20 @@ export default {
       this.errors = [];
       try {
         this.isLoading = true;
-        let result = await this.confirmationResult.confirm(this.verificationCode);
+        let result = await this.confirmationResult.confirm(
+          this.verificationCode
+        );
         console.log("success!", result);
         this.confirmationResult = null; // so that we can re-use this
         this.verificationCode = "";
         this.$emit("dismissed");
-      } catch(error) {
+      } catch (error) {
         console.log("error", error);
-        this.errors =  [ "sms." + error.code ];
+        this.errors = ["sms." + error.code];
       } finally {
         this.isLoading = false;
       }
     }
   }
-}
+};
 </script>
