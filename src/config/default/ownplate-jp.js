84044a9bfb8f653964f15250dcc84a6664b164e1
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-04-17 17:41:35 -0700

    jp

---
+++ src/config/default/ownplate-jp.js
@@ -0,0 +1,13 @@
+export default {
+  apiKey: "AIzaSyAAkqHp7A4d7NY1RDPLigUibDtrkhWxZCM",
+  authDomain: "ownplate-jp.firebaseapp.com",
+  databaseURL: "https://ownplate-jp.firebaseio.com",
+  projectId: "ownplate-jp",
+  storageBucket: "ownplate-jp.appspot.com",
+  messagingSenderId: "346339540564",
+  appId: "1:346339540564:web:c0a6bd01892e01e4c1f7c1",
+  measurementId: "G-0C7G39952F",
+  ownPlate: {
+    releasName: "alpha"
+  }
+};
