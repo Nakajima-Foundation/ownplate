e735a9d58cc20be7578426539f75e2280e5da484
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-04-22 05:11:48 -0700

    almost

--- functions/src/functions/order.ts
+++ functions/src/functions/order.ts
@@ -1,5 +1,6 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions'
 import * as utils from '../stripe/utils'
+import * as constant from '../common/constant'
 
 export const update = async (db: FirebaseFirestore.Firestore, data: any, context: functions.https.CallableContext) => {
   const uid = utils.validate_auth(context);
@@ -15,11 +16,29 @@ export const update = async (db: FirebaseFirestore.Firestore, data: any, context
 
     // BUGBUG: We need to add some rules
     const orderRef = db.doc(`restaurants/${restaurantId}/orders/${orderId}`)
-    await orderRef.update({
-      status
-    })
 
-    return { success: true }
+    return await db.runTransaction(async transaction => {
+      const snapshot = await transaction.get(orderRef);
+      const order = snapshot.data();
+      if (!order) {
+        throw new functions.https.HttpsError('invalid-argument', 'This order does not exist.')
+      }
+
+      if (order.status < constant.order_status.validation_ok || order.status >= constant.order_status.customer_picked_up) {
+        throw new functions.https.HttpsError('failed-precondition', 'It is not possible to change state from the current state.')
+      }
+      const isNewStatusValid: Boolean = (status === constant.order_status.order_canceled)
+        || (status === constant.order_status.order_accepted)
+        || (status === constant.order_status.cooking_completed)
+      if (!isNewStatusValid) {
+        throw new functions.https.HttpsError('permission-denied', 'The user does not have an authority to perform this operation.')
+      }
+
+      transaction.update(orderRef, {
+        status
+      })
+      return { success: true }
+    })
   } catch (error) {
     throw utils.process_error(error)
   }
