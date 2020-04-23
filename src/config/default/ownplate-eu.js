17da49076bb1059a3f6ab76a814048d87b2f69d4
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-04-23 14:47:27 -0700

    eu support

---
+++ src/config/default/ownplate-eu.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCLhDPHPeKZ_jw9OG4CE-O2ZxXeEnLWjkw",
+  authDomain: "ownplate-eu.firebaseapp.com",
+  databaseURL: "https://ownplate-eu.firebaseio.com",
+  projectId: "ownplate-eu",
+  storageBucket: "ownplate-eu.appspot.com",
+  messagingSenderId: "875408575790",
+  appId: "1:875408575790:web:91ca59aeda88277f47febf",
+  measurementId: "G-5H2NQFNWST"
+};
+
+export const ownPlateConfig = {
+  releasName: "alpha"
+};
