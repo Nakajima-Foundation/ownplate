071f0b73d1ec3c8cb85d17cec174d827cb1d0dbd
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-04-26 21:17:03 -0700

    test message

--- src/app/admin/OrderInfoPage.vue
+++ src/app/admin/OrderInfoPage.vue
@@ -259,6 +259,7 @@ export default {
         const { data } = await orderUpdate({
           restaurantId: this.restaurantId(),
           orderId: this.orderId,
+          smsMessage: "Test Message",
           status: newStatus
         });
         console.log("update", data);
