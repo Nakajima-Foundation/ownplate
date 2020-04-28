504b3856e9f56b72ca72b2ff6271d02122982c97
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-04-28 15:21:40 -0700

    beginning

---
+++ src/app/user/OrderHistory.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="section">
+    <h2>Order History</h2>
+  </section>
+</template>
+
+<script>
+import { db, firestore, functions } from "~/plugins/firebase.js";
+
+export default {
+  data() {
+    return {};
+  },
+  async created() {
+    console.log("created", this.uid);
+    if (this.uid) {
+      const snapshott = await db
+        .collectionGroup("orders")
+        .where("uid", "==", this.uid)
+        .get();
+      console.log("snapshot", snapshot, snapshot.docs);
+    }
+  },
+  computed: {
+    uid() {
+      return this.$store.getters.uidUser;
+    }
+  }
+};
+</script>
