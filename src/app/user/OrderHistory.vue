1e9a3944872886c6e5c77f18df97be8486f42dcb
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-05 22:08:55 -0700

    auto login for order history

--- src/app/user/OrderHistory.vue
+++ src/app/user/OrderHistory.vue
@@ -7,46 +7,45 @@
       @selected="orderSelected($event)"
       :order="order"
     />
+    <b-modal :active.sync="loginVisible" :width="640">
+      <div class="card">
+        <div class="card-content">
+          <phone-login v-on:dismissed="handleDismissed" />
+        </div>
+      </div>
+    </b-modal>
   </section>
 </template>
 
 <script>
 import { db, firestore, functions } from "~/plugins/firebase.js";
 import OrderedInfo from "~/app/admin/Order/OrderedInfo";
+import PhoneLogin from "~/app/auth/PhoneLogin";
 
 export default {
   components: {
-    OrderedInfo
+    OrderedInfo,
+    PhoneLogin
   },
   data() {
     return {
+      loginVisible: false,
       detatcher: () => {},
       orders: []
     };
   },
   async created() {
     console.log("created", this.uid);
-    if (this.uid) {
-      this.detatcher = db
-        .collectionGroup("orders")
-        .where("uid", "==", this.uid)
-        .orderBy("timePlaced", "desc")
-        .limit(25)
-        .onSnapshot(snapshot => {
-          this.orders = snapshot.docs.map(doc => {
-            const order = doc.data();
-            order.restaurantId = doc.ref.path.split("/")[1];
-            order.id = doc.id;
-            // HACK: Remove it later
-            order.timePlaced =
-              (order.timePlaced && order.timePlaced.toDate()) || new Date();
-            return order;
-          });
-        });
-    }
+    this.loginVisible = !this.uid;
+    this.getHistory();
   },
   destroyed() {
     this.detatcher();
+  },
+  watch: {
+    uid(newValue) {
+      this.getHistory();
+    }
   },
   computed: {
     uid() {
@@ -54,6 +53,35 @@ export default {
     }
   },
   methods: {
+    getHistory() {
+      if (this.uid) {
+        this.detatcher();
+        this.detatcher = db
+          .collectionGroup("orders")
+          .where("uid", "==", this.uid)
+          .orderBy("timePlaced", "desc")
+          .limit(25)
+          .onSnapshot(snapshot => {
+            this.orders = snapshot.docs.map(doc => {
+              const order = doc.data();
+              order.restaurantId = doc.ref.path.split("/")[1];
+              order.id = doc.id;
+              // HACK: Remove it later
+              order.timePlaced =
+                (order.timePlaced && order.timePlaced.toDate()) || new Date();
+              return order;
+            });
+          });
+      }
+    },
+    handleDismissed(success) {
+      console.log("handleDismissed", success);
+      if (success) {
+        this.loginVisible = false;
+      } else {
+        this.$router.push("/");
+      }
+    },
     orderSelected(order) {
       this.$router.push({
         path: "/r/" + order.restaurantId + "/order/" + order.id
