f70258f3e9c0045e5982cd8abc26dc3b74dea4ac
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-07 16:01:41 -0700

    query

--- src/app/super/AllOrders.vue
+++ src/app/super/AllOrders.vue
@@ -2,6 +2,12 @@
   <section class="section">
     <back-button url="/s" />
     <h2>All Orders</h2>
+    <ordered-info
+      v-for="order in orders"
+      :key="order.id"
+      @selected="orderSelected($event)"
+      :order="order"
+    />
   </section>
 </template>
 
@@ -14,6 +20,37 @@ export default {
   components: {
     OrderedInfo,
     BackButton
+  },
+  data() {
+    return {
+      orders: [],
+      detacher: null
+    };
+  },
+  created() {
+    this.detatcher = db
+      .collectionGroup("orders")
+      .orderBy("timePlaced", "desc")
+      .limit(25)
+      .onSnapshot(snapshot => {
+        this.orders = snapshot.docs.map(doc => {
+          const order = doc.data();
+          order.restaurantId = doc.ref.path.split("/")[1];
+          order.id = doc.id;
+          // HACK: Remove it later
+          order.timePlaced =
+            (order.timePlaced && order.timePlaced.toDate()) || new Date();
+          return order;
+        });
+      });
+  },
+  destroyed() {
+    this.detatcher && this.detatcher();
+  },
+  methods: {
+    orderSelected(order) {
+      console.log("orderSelected", order);
+    }
   }
 };
 </script>
