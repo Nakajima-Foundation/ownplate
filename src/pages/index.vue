7959381c4a87ee89c2709771990ea381bdc2957d
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-07 20:37:19 -0700

    link to Admin page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -22,6 +22,13 @@
           </p>
         </div>
       </div>
+      <div v-if="isAdmin" class="card block">
+        <div class="card-content">
+          <p style="text-align:center; font-weight:bold; font-size:1.5em">
+            <router-link to="/s">Admin Page</router-link>
+          </p>
+        </div>
+      </div>
     </section>
   </div>
 </template>
@@ -40,6 +47,15 @@ export default {
   mounted() {},
 
   computed: {
+    isAdmin() {
+      return this.credentials && this.credentials.admin;
+    },
+    credentials() {
+      return this.user && this.user.credentials;
+    },
+    user() {
+      return this.$store.state.user;
+    },
     hideUsersLink() {
       return releaseConfig.hideUsersLink;
     }
