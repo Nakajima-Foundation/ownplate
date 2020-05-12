04c21581aa5db783cf90bc838438c52a24bde5f0
Isamu Arimoto <isamu@to-kyo.to>
2020-05-13 08:56:39 +0900

    remove debug code

--- src/plugins/sentry.js
+++ src/plugins/sentry.js
@@ -5,7 +5,6 @@ import { Vue as VueIntegration } from '@sentry/integrations';
 import { sentryDsn } from "@/config/project";
 
 if (sentryDsn) {
-  console.log("AAA");
   Sentry.init({
     dsn: sentryDsn,
     integrations: [new VueIntegration({Vue, attachProps: true})],
