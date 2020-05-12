eba8a7783de7dd6e84be1e22975b59307a25c15b
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-12 06:55:50 -0700

    tax in small font

--- src/components/Price.vue
+++ src/components/Price.vue
@@ -1,13 +1,19 @@
 <template>
   <span>
-    <span v-if="shopInfo.taxInclude">{{$tc('tax.include', taxObj.total_price_i18n)}}<br/></span>
-    <span v-if="!shopInfo.taxInclude">{{$tc('tax.exclude', taxObj.price_i18n)}}<br/></span>
-
+    <span v-if="shopInfo.taxInclude">
+      {{$tc('tax.price', taxObj.total_price_i18n) }}
+      <span class="tax">{{$tc('tax.include')}}</span>
+      <br />
+    </span>
+    <span v-if="!shopInfo.taxInclude">
+      {{$tc('tax.price', taxObj.price_i18n)}}
+      <span class="tax">{{$tc('tax.exclude')}}</span>
+      <br />
+    </span>
   </span>
 </template>
 
 <script>
-
 export default {
   name: "Price",
   props: {
@@ -23,17 +29,26 @@ export default {
   computed: {
     taxObj() {
       const price = Number(this.menu.price);
-      const multiple =  this.$store.getters.stripeRegion.multiple;
-      const taxRate =  this.menu.tax === "alcohol" ? this.shopInfo.alcoholTax : this.shopInfo.foodTax;
-      const tax = Math.round(((price * taxRate) / 100 )* multiple) / multiple;
+      const multiple = this.$store.getters.stripeRegion.multiple;
+      const taxRate =
+        this.menu.tax === "alcohol"
+          ? this.shopInfo.alcoholTax
+          : this.shopInfo.foodTax;
+      const tax = Math.round(((price * taxRate) / 100) * multiple) / multiple;
       const total = price + tax;
 
       return {
-        price_i18n: this.$n(price,  'currency'),
-        tax_i18n: this.$n(tax, 'currency'),
-        total_price_i18n: this.$n(total,  'currency')
+        price_i18n: this.$n(price, "currency"),
+        tax_i18n: this.$n(tax, "currency"),
+        total_price_i18n: this.$n(total, "currency")
       };
     }
-  },
-}
+  }
+};
 </script>
+
+<style scoped>
+.tax {
+  font-size: 0.6em;
+}
+</style>
