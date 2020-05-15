9027386f3f38e26d6441ba506f9aeee3c5b0a524
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-15 03:28:28 -0700

    request works with Promise

--- functions/src/functions/line.ts
+++ functions/src/functions/line.ts
@@ -1,25 +1,55 @@
 import * as functions from 'firebase-functions'
 import * as utils from '../stripe/utils'
-//import * as https from 'https'
+import * as https from 'https'
 
 export const validate = async (db: FirebaseFirestore.Firestore, data: any, context: functions.https.CallableContext) => {
   const { token } = data;
   utils.validate_params({ token })
 
+  // https://stackoverflow.com/questions/43486393/how-to-make-an-http-request-in-cloud-functions-for-firebase
   try {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
       /*
-  const options = {
-    url: 'https://api.line.me/v1/oauth/verify',
-    headers: {
-      'Authorization': `Bearer ${token}`
-    }
-  };
-      https.request(options, (res) => {
-        resolve({ success: true, res });
-      });
+      const options = {
+        url: 'https://api.line.me/v1/oauth/verify',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      };
       */
-      resolve({ success: true })
+      https.get('https://encrypted.google.com/', (res) => {
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
+
+        res.on('data', (d) => {
+          console.log(d);
+        });
+        res.on('end', () => {
+          resolve({ success: true })
+        })
+      }).on('error', (e) => {
+        console.error(e);
+        reject({ error: e });
+      });
+      /*
+      const options = {
+        //url: 'https://api.line.me/v1/oauth/verify',
+        hostname: 'api.line.me',
+        port: 80,
+        path: '/v1/oauth/verify',
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      };
+      try {
+        https.request(options, (res) => {
+          resolve({ success: true, res });
+        });
+      } catch (error) {
+        reject({ error });
+      }
+      */
     })
   } catch (error) {
     throw utils.process_error(error)
