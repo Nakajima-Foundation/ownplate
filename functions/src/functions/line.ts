59a3b98df5ca18789fbad85845c0db8b056431b8
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-15 13:30:34 -0700

    clean up

--- functions/src/functions/line.ts
+++ functions/src/functions/line.ts
@@ -3,10 +3,39 @@ import * as utils from '../stripe/utils'
 import * as https from 'https'
 import * as url from 'url';
 
+const request = (_url: string, _options: any, postData?: any) => {
+  const parsedURL = url.parse(_url);
+  const options = Object.assign({
+    hostname: parsedURL.host,
+    port: 443,
+    path: parsedURL.pathname,
+    method: "GET"
+  }, _options)
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+      res.setEncoding('utf8');
+      let body = "";
+      res.on('data', (chunk) => {
+        body += chunk;
+      });
+      res.on('end', () => {
+        resolve(JSON.parse(body))
+      })
+    })
+    req.on('error', (e) => {
+      console.error(e);
+      reject({ error: e });
+    });
+    if (postData) {
+      req.write(postData)
+    }
+    req.end();
+  })
+}
+
 export const validate = async (db: FirebaseFirestore.Firestore, data: any, context: functions.https.CallableContext) => {
   const { code, redirect_uri, client_id } = data;
   utils.validate_params({ code, redirect_uri, client_id })
-  const foo = url.parse("https://api.line.me/oauth2/v2.1/token");
 
   const LINE_SECRET_KEY = functions.config().line.secret;
   const params = {
@@ -19,41 +48,15 @@ export const validate = async (db: FirebaseFirestore.Firestore, data: any, conte
   const postData = Object.keys(params).map(key => {
     return key + "=" + encodeURIComponent(params[key]);
   }).join("&");
-  console.log(postData);
 
-  const options = {
-    hostname: foo.host,
-    port: 443,
-    path: foo.pathname,
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': Buffer.byteLength(postData)
-    }
-  }
-
-  // https://stackoverflow.com/questions/43486393/how-to-make-an-http-request-in-cloud-functions-for-firebase
   try {
-    return new Promise((resolve, reject) => {
-      const req = https.request(options, (res) => {
-        console.log('statusCode:', res.statusCode);
-        console.log('headers:', res.headers);
-        res.setEncoding('utf8');
-        let body = "";
-        res.on('data', (chunk) => {
-          body += chunk;
-        });
-        res.on('end', () => {
-          resolve(JSON.parse(body))
-        })
-      })
-      req.on('error', (e) => {
-        console.error(e);
-        reject({ error: e });
-      });
-      req.write(postData)
-      req.end();
-    })
+    return request("https://api.line.me/oauth2/v2.1/token", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(postData)
+      }
+    }, postData)
   } catch (error) {
     throw utils.process_error(error)
   }
