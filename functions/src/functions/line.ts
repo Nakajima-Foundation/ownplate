f5ac9d5ebd064404d629327d995fe0c8114dc871
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-15 10:39:57 -0700

    step by step

--- functions/src/functions/line.ts
+++ functions/src/functions/line.ts
@@ -1,23 +1,17 @@
 import * as functions from 'firebase-functions'
 import * as utils from '../stripe/utils'
 import * as https from 'https'
+import * as url from 'url';
 
 export const validate = async (db: FirebaseFirestore.Firestore, data: any, context: functions.https.CallableContext) => {
   const { token } = data;
   utils.validate_params({ token })
+  const foo = url.parse("https://api.line.me/oauth2/v2.1/token");
 
   // https://stackoverflow.com/questions/43486393/how-to-make-an-http-request-in-cloud-functions-for-firebase
   try {
     return new Promise((resolve, reject) => {
-      /*
-      const options = {
-        url: 'https://api.line.me/v1/oauth/verify',
-        headers: {
-          'Authorization': `Bearer ${token}`
-        }
-      };
-      */
-      https.get('https://api.line.me/oauth2/v2.1/token', (res) => {
+      const req = https.request(foo.href!, (res) => {
         console.log('statusCode:', res.statusCode);
         console.log('headers:', res.headers);
 
@@ -29,10 +23,37 @@ export const validate = async (db: FirebaseFirestore.Firestore, data: any, conte
         res.on('end', () => {
           resolve({ success: true, body })
         })
-      }).on('error', (e) => {
+      })
+      req.on('error', (e) => {
         console.error(e);
         reject({ error: e });
       });
+      //req.write(postData)
+      req.end();
+
+      /*
+      const options = {
+        url: 'https://api.line.me/v1/oauth/verify',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      };
+      */
+      //const postData = "hello";
+      /*
+      const options = {
+        // https://api.line.me/oauth2/v2.1/token works with get
+        //url: 'https://api.line.me/v2/oauth/accessToken',
+        hostname: 'encrypted.google.com',
+        port: 443,
+        path: '/',
+        method: 'GET'
+        /*
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': Buffer.byteLength(postData)
+        }
+        */
       /*
       const options = {
         //url: 'https://api.line.me/v1/oauth/verify',
