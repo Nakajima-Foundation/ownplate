c0023f31e3d48b31a692572195158ad76d30613a
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-15 20:53:29 -0700

    clean up

--- functions/src/functions/line.ts
+++ functions/src/functions/line.ts
@@ -6,24 +6,25 @@ import * as admin from 'firebase-admin'
 export const validate = async (db: FirebaseFirestore.Firestore, data: any, context: functions.https.CallableContext) => {
   const { code, redirect_uri, client_id } = data;
   utils.validate_params({ code, redirect_uri, client_id })
-
   const LINE_SECRET_KEY = functions.config().line.secret;
-  const params = {
-    grant_type: "authorization_code",
-    code,
-    redirect_uri,
-    client_id,
-    client_secret: LINE_SECRET_KEY
-  };
 
   try {
-    // access_token, id_token, expires_in, refresh_token, scope, token_type
-    const result = await netutils.postForm("https://api.line.me/oauth2/v2.1/token", params)
+    // We validate the OAuth token (code) given to the redirected page.
+    // Result: access_token, id_token, expires_in, refresh_token, scope, token_type
+    const result = await netutils.postForm("https://api.line.me/oauth2/v2.1/token", {
+      grant_type: "authorization_code",
+      code,
+      redirect_uri,
+      client_id,
+      client_secret: LINE_SECRET_KEY
+    })
     if (!result.id_token) {
       throw new functions.https.HttpsError('invalid-argument',
         'Validation failed.', { params: result }
       )
     }
+
+    // We verify this code.
     // amr, aud, exp, iat, iss, name, sub
     const verified = await netutils.postForm('https://api.line.me/oauth2/v2.1/verify', {
       id_token: result.id_token,
@@ -34,6 +35,8 @@ export const validate = async (db: FirebaseFirestore.Firestore, data: any, conte
         'Verification failed.', { params: verified }
       )
     }
+
+    // We ask Firebase to create a custom token for this LINE user
     const customeToken = await admin.auth().createCustomToken(`line:${verified.sub}`)
 
     return { result, verified, customeToken };
