21d4f956d5dfc82f5593782c2f896c149d6d8a41
Yuta <yuta.ok@gmail.com>
2020-05-16 18:17:49 -0700

    Add "Add" button to the item card.

--- src/app/user/Restaurant/ItemCard.vue
+++ src/app/user/Restaurant/ItemCard.vue
@@ -17,8 +17,15 @@
             class="t-body2 c-text-black-medium m-t-8"
           >{{ allergensDescription }}</div>
         </div>
-        <div class="p-r-16 p-t-16 p-b-16" v-if="image !== null">
-          <img :src="image" width="96" height="96" class="is-pulled-right r-4" />
+        <div class="p-r-16 p-t-16 p-b-16">
+          <div class="w-96 is-pulled-right">
+            <div v-if="image !== null" class="p-b-8">
+              <img :src="image" width="96" height="96" class="r-4" />
+            </div>
+            <div @click="pushCount" class="op-button-pill bg-primary-bg w-96">
+              <span>Add</span>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -61,7 +68,7 @@
             <div class="level-left">
               <div
                 @click="pullCount"
-                class="op-button-pill bg-status-red-bg w-96 m-r-8"
+                class="op-button-pill bg-status-red-bg w-96"
                 :disabled="count === 0"
               >
                 <i class="material-icons c-status-red">remove</i>
@@ -69,7 +76,7 @@
             </div>
             <div class="t-h4 c-primary">{{ count }}</div>
             <div class="level-right">
-              <div @click="pushCount" class="op-button-pill bg-primary-bg w-96 m-r-8">
+              <div @click="pushCount" class="op-button-pill bg-primary-bg w-96">
                 <i class="material-icons">add</i>
               </div>
             </div>
@@ -142,8 +149,8 @@ export default {
     },
     openMenuFlag() {
       if (this.openMenuFlag && this.count == 0) {
-        this.setCount(this.count + 1);
-        // this.setCount(this.count + 0); // Need discussion about the auto add feature.
+        // this.setCount(this.count + 1);
+        this.setCount(this.count + 0); // Only by tapping "Add" will do both open card and add item.
       }
     }
   },
