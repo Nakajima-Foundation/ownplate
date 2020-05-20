1aa9af25c0ad1bc32c952fcce1567ab3506c3c86
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-20 07:05:00 -0700

    a bit of design

--- src/components/ErrorPopup.vue
+++ src/components/ErrorPopup.vue
@@ -1,11 +1,12 @@
 <template>
   <div v-if="error">
-    <b-modal :active.sync="isVisible" :width="640">
-      <div class="card">
-        <div class="card-content">
-          <h1>{{$t('errorPage.popup.title')}}</h1>
-          <p>{{message}}</p>
-          <p>{{$t("errorPage.popup.message1")}}</p>
+    <b-modal :active.sync="isVisible" :width="488">
+      <div class="op-dialog p-t-24 p-l-24 p-r-24 p-b-24">
+        <div class="t-h6 c-text-black-disabled p-b-8">{{$t('errorPage.popup.title')}}</div>
+        <div class="m-t-8">{{message}}</div>
+        <div class="m-t-8">{{$t("errorPage.popup.message1")}}</div>
+        <div class="m-t-24 align-center">
+          <div class="op-button-small tertiary" @click="close">{{$t('menu.close')}}</div>
         </div>
       </div>
     </b-modal>
@@ -32,6 +33,11 @@ export default {
         return this.$t("errorPage.code." + this.error.code);
       }
       return "";
+    }
+  },
+  methods: {
+    close() {
+      this.isVisible = false;
     }
   }
 };
