64eedd1beb6b8e2770a1dabeba14df9217c3d0ef
Yuta <yuta.ok@gmail.com>
2020-05-21 12:17:39 -0700

    Extract Share Popup from ShopInfo and Make a component.

--- src/app/user/Restaurant/ShopInfo.vue
+++ src/app/user/Restaurant/ShopInfo.vue
@@ -1,43 +1,5 @@
 <template>
   <div>
-    <!-- Share / Review -->
-    <div class="m-t-8 align-center">
-      <div class="op-button-text m-r-8" @click="openShare()">
-        <i class="material-icons">launch</i>
-        <span>Share</span>
-      </div>
-      <!-- <sharing-buttons :title="name" :url="shareUrl()" /> -->
-    </div>
-    <!-- Share Popup-->
-    <b-modal :active.sync="sharePopup" :width="488" scroll="keep">
-      <div class="op-dialog p-t-24 p-l-24 p-r-24 p-b-24">
-        <div class="t-h6 c-text-black-disabled p-b-8">Share</div>
-        <div class="cols">
-          <div>
-            <qrcode :value="url" :options="{ width: 160 }"></qrcode>
-          </div>
-          <div>
-            <nuxt-link to="#" @click.native="copyClipboard(url)" event>
-              <div class="op-button-text m-t-8">
-                <i class="material-icons">file_copy</i>
-                <span>{{$t('shopInfo.copyUrl')}}</span>
-              </div>
-            </nuxt-link>
-            <div
-              class="m-l-8 t-body2 c-text-black-disabled"
-              style="word-break: break-all;"
-            >{{this.url}}</div>
-            <div class="m-t-24">
-              <sharing-buttons :title="shopInfo.restaurantName" :url="url" />
-            </div>
-          </div>
-        </div>
-        <div class="m-t-24 align-center">
-          <div class="op-button-small tertiary" @click="closeShare()">Close</div>
-        </div>
-      </div>
-    </b-modal>
-
     <!-- Restaurant Details -->
     <div class="m-t-24">
       <div class="t-h6 c-text-black-disabled">{{$t('shopInfo.restaurantDetails')}}</div>
@@ -122,7 +84,7 @@
             </div>
           </template>
         </div>
-        <!-- Want to update to popup version -->
+        <!-- # Want to update to popup version -->
         <!--
 				<div class="align-center">
           <div class="op-status c-status-green bg-status-green-bg m-t-16">Open Now</div>
@@ -146,12 +108,8 @@ import {
   formatNational,
   formatURL
 } from "~/plugins/phoneutil.js";
-import SharingButtons from "~/app/user/Common/SharingButtons";
 
 export default {
-  components: {
-    SharingButtons
-  },
   props: {
     shopInfo: {
       type: Object,
@@ -164,8 +122,7 @@ export default {
       url: this.shareUrl(),
       days: daysOfWeek,
       weekday: d.getDay(),
-      today: d,
-      sharePopup: false
+      today: d
     };
   },
   computed: {
@@ -244,12 +201,6 @@ export default {
     },
     validDate(date) {
       return !this.isNull(date.start) && !this.isNull(date.end);
-    },
-    openShare() {
-      this.sharePopup = true;
-    },
-    closeShare() {
-      this.sharePopup = false;
     }
   }
 };
