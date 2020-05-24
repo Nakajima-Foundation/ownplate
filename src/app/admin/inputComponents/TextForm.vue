bdea4a83a6fdc3e6498ea852ba04aef338ba6beb
Isamu Arimoto <isamu@to-kyo.to>
2020-05-25 06:55:47 +0900

    missing file

---
+++ src/app/admin/inputComponents/TextForm.vue
@@ -0,0 +1,60 @@
+<template>
+<div>
+  <div class="field is-horizontal">
+    <div class="field-body">
+      <h4>
+        {{$t(titleKey)}}
+        <span class="p-font bold" style="color:#CB4B4B">*</span>
+      </h4>
+    </div>
+  </div>
+  <b-field :type="error.length > 0 ? 'is-danger' : 'is-success'">
+    <b-input
+      :value="value"
+      type="text"
+      :placeholder="$t(placeHolder)"
+      @input="input"
+      :maxlength="maxlength"
+      ></b-input>
+  </b-field>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: "TextForm",
+  props: {
+    required: {
+      type: Boolean,
+      required: false,
+      'default': () => (true)
+    },
+    titleKey: {
+      type: String,
+      required: true
+    },
+    placeHolder: {
+      type: String,
+      required: true
+    },
+    maxlength: {
+      type: Number,
+      required: true
+    },
+    error : {
+      type: Array,
+      required: true
+    },
+    value : {
+      type: String,
+      required: true
+    },
+  },
+  methods: {
+    input(e) {
+      this.$emit('input', e)
+    },
+  }
+}
+</script>
