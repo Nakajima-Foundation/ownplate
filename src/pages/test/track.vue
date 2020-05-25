f69798f8d09a292f711912382d80d566b70fd00e
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-25 08:47:31 -0700

    taku

--- src/pages/test/track.vue
+++ src/pages/test/track.vue
@@ -2,10 +2,10 @@
   <section class="section">
     <h1>Track</h1>
     <div>
-      <a href="/t?id=OASw5vLIieYKOUDhOGj5">Enter 丸皿中華</a>
+      <a href="/t?id=OASw5vLIieYKOUDhOGj5">Enter Taku</a>
     </div>
     <div>
-      <a href="/t?id=OASw5vLIieYKOUDhOGj5">Leave 丸皿中華</a>
+      <a href="/t?id=OASw5vLIieYKOUDhOGj5">Leave Taku</a>
     </div>
   </section>
 </template>
