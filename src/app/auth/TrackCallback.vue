3be12ca1eed1220a344ff361c5a41a38d4157b0b
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-25 12:03:52 -0700

    fixed typo

--- src/app/auth/TrackCallback.vue
+++ src/app/auth/TrackCallback.vue
@@ -34,7 +34,7 @@ export default {
           const user = await auth.signInWithCustomToken(data.customToken);
           console.log("signInWithCustomToken", user);
           if (user) {
-            this.$router.replace(`/t/record/${data.nonce}`);
+            this.$router.replace(`/t/${data.nonce}`);
           }
         } else {
           console.error("validatin failed", data);
