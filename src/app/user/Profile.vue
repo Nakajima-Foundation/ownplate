c21c70a057bc60282312e47939bde6038a83dfb4
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-26 13:57:33 -0700

    profile page

---
+++ src/app/user/Profile.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="section">
+    <h1>{{"profile.title"}}</h1>
+    <b-field class="m-t-8" :label="'profile.loginStatus'">
+      <p>{{loginStatus}}</p>
+    </b-field>
+    <div v-if="user">
+      <b-field class="m-t-8" :label="'profile.displayName'">
+        <p>{{displayName}}</p>
+      </b-field>
+      <div v-if="user.phoneNumber">
+        <b-field class="m-t-8" :label="'profile.lineConnection'">
+          <p>{{lineConnection}}</p>
+        </b-field>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { parsePhoneNumber, formatNational } from "~/plugins/phoneutil.js";
+
+export default {
+  computed: {
+    user() {
+      return this.$store.state.user;
+    },
+    lineConnection() {
+      return "to be implemented";
+    },
+    displayName() {
+      return this.user?.displayName || "profile.undefined";
+    },
+    loginStatus() {
+      if (this.user) {
+        console.log(this.user);
+        if (this.user.email) {
+          return `${"profile.status.email"} ${this.user.email}`;
+        } else if (this.user.phoneNumber) {
+          const number = parsePhoneNumber(this.user.phoneNumber);
+          return `${"profile.status.phone"} ${formatNational(number)}`;
+        } else if (this.user.uid.slice(0, 5) === "line:") {
+          return "profile.status.line";
+        }
+        return "profile.status.unexpected";
+      }
+      return "profile.status.none";
+    }
+  }
+};
+</script>
