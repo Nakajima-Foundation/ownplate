41da129749a608dc6c67d4d3edfadc7623d2fc00
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-31 13:52:44 -0700

    few comments

--- batch/migrate_payment.js
+++ batch/migrate_payment.js
@@ -1,3 +1,5 @@
+// npm run batch -- ./batch/migrate_payment.js
+
 import * as admin from "firebase-admin";
 
 // PROJECT=123 npm run customclaims
@@ -7,6 +9,7 @@ console.log(`project: ${project}`);
 
 const main = async () => {
   const fRun = (process.argv[2] === "--doit")
+  console.log(fRun ? "Doing it" : "Dry run (add '--doit' option to really do it)")
   const serviceAccount = await import(`./keys/${project}-firebase-adminsdk.json`);
 
   admin.initializeApp({
@@ -19,21 +22,28 @@ const main = async () => {
   const promisses = docs.map(async doc => {
     const admin = doc.data();
     const refPublic = doc.ref.collection("public");
-    const refStripe = refPublic.doc("stripe");
-    const stripe = (await refStripe.get()).data();
-    if (stripe) {
-      const refPayment = refPublic.doc("payment");
-      const payment = (await refPayment.get()).data();
-      if (!payment) {
-        const newData = {
-          stripe: stripe.stripeAccount,
-          inStore: false
-        };
-        console.log(`set ${JSON.stringify(newData)} at ${refPayment.path}`);
+    const refStripe = refPublic.doc("stripe"); // reference to old data
+    const refPayment = refPublic.doc("payment"); // reference to new data
+    await db.runTransaction(async tx => {
+      const stripe = (await tx.get(refStripe)).data(); // old data
+      if (stripe) {
+        const payment = (await tx.get(refPayment)).data(); // new data
+        if (!payment) { // no need to migrate twice
+          const newData = {
+            stripe: stripe.stripeAccount,
+            inStore: false
+          };
+          console.log(`set ${JSON.stringify(newData)} at ${refPayment.path}`);
+          if (fRun) {
+            tx.set(refPayment, newData);
+          }
+        }
+        // Note: We are NOT deleting old data
       }
-    }
+    })
   });
   await Promise.all(promisses);
+  console.log("Note: This migration code is safe to run more than once")
 
   process.exit(0);
 };
