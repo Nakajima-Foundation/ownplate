7de024394ec1bb15adbe74afd28938896ee07d3f
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-05-31 20:11:23 -0700

    clean up

--- batch/template.js
+++ batch/template.js
@@ -1,22 +1,19 @@
-// npm run batch -- ./batch/template.js
+// PROJECT=123 npm run batch -- ./batch/template.js
 import * as admin from "firebase-admin";
 
-// PROJECT=123 npm run customclaims
 const project = process.env.PROJECT || "ownplate-dev";
-
 console.log(`project: ${project}`);
 const fRun = (process.argv[2] === "--doit")
 console.log(fRun ? "Doing it" : "Dry run (add '--doit' option to really do it)")
 
 const main = async () => {
   const serviceAccount = await import(`./keys/${project}-firebase-adminsdk.json`);
-
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: `https://${project}.firebaseio.com`
   });
-
   const db = admin.firestore();
+
   const refCollection = db.collection("users");
   const next = async (query) => {
     const doc = (await query.limit(1).get()).docs[0];
