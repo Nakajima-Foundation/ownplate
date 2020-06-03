4de54be27656b3b91a857ab4231909468437330c
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-06-02 22:22:19 -0700

    readonly/stripe

--- functions/src/stripe/customer.ts
+++ functions/src/stripe/customer.ts
@@ -26,17 +26,25 @@ export const update = async (db: FirebaseFirestore.Firestore, data: any, context
   utils.validate_params({ tokenId });
   const stripe = utils.get_stripe();
 
-  const refStripe = db.doc(`/users/${uid}/system/stripe`)
+  const refStripeSystem = db.doc(`/users/${uid}/system/stripe`)
+  const refStripeReadOnly = db.doc(`/users/${uid}/readonly/stripe`)
 
   const result = { result: {} }
   try {
     const token = await stripe.tokens.retrieve(tokenId);
+    const card = {
+      last4: token.card?.last4,
+      brand: token.card?.brand
+    }
     console.log("***token", token);
     await db.runTransaction(async (tr) => {
-      const stripeInfo = (await tr.get(refStripe)).data();
+      const stripeInfo = (await tr.get(refStripeSystem)).data();
       if (!stripeInfo) {
         throw new functions.https.HttpsError('invalid-argument', 'This user does not have a stripe customer.')
       }
+      tr.set(refStripeReadOnly, {
+        card
+      }, { merge: true })
 
       result.result = await stripe.customers.update(stripeInfo.customerId, {
         source: tokenId
