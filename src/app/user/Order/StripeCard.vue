fda18709b063c348ddeab3da8bb354c792a7c94a
Yuta <yuta.ok@gmail.com>
2020-06-03 18:06:28 -0700

    Add CVC description popup.

--- src/app/user/Order/StripeCard.vue
+++ src/app/user/Order/StripeCard.vue
@@ -1,13 +1,59 @@
 <template>
   <div>
-    <div v-if="storedCard" class="m-t-16 m-l-16">
+    <div v-if="storedCard" class="bg-surface r-8 d-low m-t-8 p-l-16 p-r-16 p-t-16 p-b-16">
       <b-checkbox v-model="useStoredCard">
-        <span>{{storedCard.brand}}</span>
-        <span>**** **** **** {{storedCard.last4}}</span>
+        <span class="t-body1 c-text-black-high">
+          <span>{{storedCard.brand}}</span>
+          <span>**** **** **** {{storedCard.last4}}</span>
+        </span>
       </b-checkbox>
     </div>
-    <div v-show="!useStoredCard" class="bg-surface r-8 d-low m-t-8 p-l-16 p-r-16 p-t-16 p-b-16">
-      <div id="card-element"></div>
+    <div v-show="!useStoredCard">
+      <!-- Enter New Card -->
+      <div class="bg-surface r-8 d-low m-t-8 p-l-16 p-r-16 p-t-16 p-b-16">
+        <div id="card-element"></div>
+      </div>
+      <!-- About CVC -->
+      <div>
+        <!-- CVC Button -->
+        <div class="align-right">
+          <div class="op-button-text" @click="openCVC()">
+            <i class="material-icons">help_outline</i>
+            <span>{{$t('order.whatsCVC')}}</span>
+          </div>
+        </div>
+        <!-- CVC Popup-->
+        <b-modal :active.sync="CVCPopup" :width="488" scroll="keep">
+          <div class="op-dialog p-t-24 p-l-24 p-r-24 p-b-24">
+            <!-- Title -->
+            <div class="t-h6 c-text-black-disabled">{{$t('order.whatsCVC')}}</div>
+
+            <!-- 4 Digits -->
+            <div class="bg-form r-8 align-center m-t-24 p-l-24 p-r-24 p-t-24 p-b-24">
+              <div class="t-h6 c-text-black-medium">Visa, MasterCard, JCB, Diners Club, DISCOVER</div>
+              <div class="t-subtitle1 c-status-blue m-t-8">{{$t('order.3digitsCVC')}}</div>
+              <div class="m-t-8">
+                <img src="~/static/CVC-3digits-1.svg" />
+              </div>
+              <div class="m-t-4">
+                <img src="~/static/CVC-3digits-2.svg" />
+              </div>
+            </div>
+
+            <!-- 3 Digits -->
+            <div class="bg-form r-8 align-center m-t-24 p-l-24 p-r-24 p-t-24 p-b-24">
+              <div class="t-h6 c-text-black-medium">American Express</div>
+              <div class="t-subtitle1 c-status-blue m-t-8">{{$t('order.4digitsCVC')}}</div>
+              <div class="m-t-8">
+                <img src="~/static/CVC-4digits-Amex.svg" />
+              </div>
+            </div>
+            <div class="m-t-24 align-center">
+              <div class="op-button-small tertiary" @click="closeCVC()">{{$t('menu.close')}}</div>
+            </div>
+          </div>
+        </b-modal>
+      </div>
     </div>
   </div>
 </template>
@@ -23,7 +69,8 @@ export default {
       storedCard: null,
       useStoredCard: false,
       elementStatus: { complete: false },
-      cardElement: {}
+      cardElement: {},
+      CVCPopup: false
     };
   },
   async mounted() {
@@ -87,6 +134,12 @@ export default {
         this.elementStatus = status;
         this.$emit("change", status);
       });
+    },
+    openCVC() {
+      this.CVCPopup = true;
+    },
+    closeCVC() {
+      this.CVCPopup = false;
     }
   }
 };
