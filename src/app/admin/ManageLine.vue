6155b65079d20ae6574d33638eb89ade91de824a
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-06-05 16:11:16 -0700

    toggle notification

--- src/app/admin/ManageLine.vue
+++ src/app/admin/ManageLine.vue
@@ -10,22 +10,76 @@
         </span>
       </b-button>
     </div>
+    <div class="m-t-8">
+      <div v-for="lineUser in lineUsers" :key="lineUser.id" @click="handleToggle(lineUser)">
+        <i :class="iconClass(lineUser)" />
+        {{ lineUser.displayName }}
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
 import { db } from "~/plugins/firebase.js";
 export default {
-  async mounted() {
-    const userId = this.$route.query.userId;
+  data() {
+    return {
+      lineUsers: [],
+      detacher: null
+    };
+  },
+  async created() {
+    const lineId = this.$route.query.userId;
     const displayName = this.$route.query.displayName;
-    console.log("***", userId, displayName);
-    await db.doc(`restaurants/${this.restaurantId()}/lines/${userId}`).set(
-      {
-        displayName
-      },
-      { merge: true }
-    );
+    if (lineId && displayName) {
+      try {
+        await db.doc(`restaurants/${this.restaurantId()}/lines/${lineId}`).set(
+          {
+            displayName,
+            notify: true
+          },
+          { merge: true }
+        );
+        console.log("registered lineId", lineId);
+        this.$router.replace(location.pathname);
+      } catch (error) {
+        console.error(error.message);
+        // BUGBUG: code
+        this.$store.commit("setErrorMessage", {
+          code: "line.validation",
+          message2: error.message,
+          error
+        });
+      }
+    }
+  },
+  async mounted() {
+    this.detacher = db
+      .collection(`restaurants/${this.restaurantId()}/lines`)
+      .onSnapshot(snapshot => {
+        this.lineUsers = snapshot.docs.map(doc => {
+          const data = doc.data();
+          return {
+            ...data,
+            id: doc.id
+          };
+        });
+      });
+  },
+  destroyed() {
+    this.detacher && this.detacher();
+  },
+  methods: {
+    async handleToggle(lineUser) {
+      await db
+        .doc(`restaurants/${this.restaurantId()}/lines/${lineUser.id}`)
+        .update({
+          notify: !lineUser.notify
+        });
+    },
+    iconClass(lineUser) {
+      return lineUser.notify ? "far fa-check-square" : "far fa-square";
+    }
   },
   computed: {
     lineAuth() {
