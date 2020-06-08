fb05633be21b4993cd70979ecb2a271e76ab640b
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-06-08 09:55:54 -0700

    works with path=/

--- src/plugins/line.js
+++ src/plugins/line.js
@@ -17,7 +17,7 @@ export const lineAuthURL = (path, options, channelId) => {
     { state, nonce }));
   const date = new Date();
   date.setTime(date.getTime() + 5 * 60 * 1000); // five minutes
-  const cookie = `line_params=${encodeURIComponent(params)}; expires=${date.toUTCString()}; path=${path}`;
+  const cookie = `line_params=${encodeURIComponent(params)}; expires=${date.toUTCString()}; path=/`;
   console.log(cookie);
   document.cookie = cookie;
   const queryString = Object.keys(query)
@@ -30,10 +30,9 @@ export const lineAuthURL = (path, options, channelId) => {
 
 export const lineGuard = (nonce, state) => {
   const cookies = Cookie.parse(document.cookie);
-  //console.log(cookies);
   const params = JSON.parse(cookies.line_params);
-  //console.log("*** lineGuard", params, state, nonce, params.nonce);
 
+  //console.log("lineGuard", state, params.state);
   if (state !== params.state || nonce !== params.nonce) {
     throw new Error("invalid state");
   }
@@ -43,5 +42,6 @@ export const lineGuard = (nonce, state) => {
 export const lineVerify = (state) => {
   const cookies = Cookie.parse(document.cookie);
   const params = JSON.parse(cookies.line_params);
+  //console.log("lineVerify", state, params.state);
   return (state === params.state);
 }
