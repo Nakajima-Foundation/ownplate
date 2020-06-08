d8298232c8ecdbde747e9a89ee6cc26f79bf1a82
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-06-08 15:09:13 -0700

    nofityCancel

--- functions/src/lang/en.ts
+++ functions/src/lang/en.ts
@@ -4,6 +4,7 @@ export const resource = {
     msg_order_accepted: "Your order has been accepted.",
     msg_cooking_completed: "Your order is ready to pick up.",
     msg_order_canceled: "Your order has been canceled.",
-    msg_order_placed: "You've got a new order."
+    msg_order_placed: "You've got a new order.",
+    msg_order_canceled_by_user: "The user canceled an order."
   }
 }
--- functions/src/lang/ja.ts
+++ functions/src/lang/ja.ts
@@ -4,6 +4,7 @@ export const resource = {
     msg_order_accepted: "ありがとうございます。ご注文は受理されました。",
     msg_cooking_completed: "ご注文の準備ができました。ピックアップが可能です。",
     msg_order_canceled: "あなたの注文はキャンセルされました。",
-    msg_order_placed: "新たな注文が入りました。"
+    msg_order_placed: "新たな注文が入りました。",
+    msg_order_canceled_by_user: "お客様により注文がキャンセルされました。"
   }
 }
