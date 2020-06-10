549cc0e0586b771e3b7a22514cc7a9b7edc8390d
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-06-10 15:07:23 -0700

    not found

--- src/app/admin/OrderInfoPage.vue
+++ src/app/admin/OrderInfoPage.vue
@@ -1,162 +1,175 @@
 <template>
   <div>
-    <!-- Order Header Area -->
-    <div class="columns is-gapless">
-      <!-- Left Gap -->
-      <div class="column is-narrow w-24"></div>
-      <!-- Center Column -->
-      <div class="column">
-        <div class="m-l-24 m-r-24">
-          <!-- Back Button and Restaurant Profile -->
-          <div>
-            <!-- Back Button -->
-            <back-button :url="parentUrl" class="m-t-24" />
+    <template v-if="notFound">
+      <not-found />
+    </template>
+    <template v-else>
+      <!-- Order Header Area -->
+      <div class="columns is-gapless">
+        <!-- Left Gap -->
+        <div class="column is-narrow w-24"></div>
+        <!-- Center Column -->
+        <div class="column">
+          <div class="m-l-24 m-r-24">
+            <!-- Back Button and Restaurant Profile -->
+            <div>
+              <!-- Back Button -->
+              <back-button :url="parentUrl" class="m-t-24" />
 
-            <!-- Restaurant Profile -->
-            <div class="is-inline-flex flex-center m-l-16 m-t-24">
-              <div>
-                <img :src="shopInfo.restProfilePhoto" class="w-36 h-36 r-36 cover" />
+              <!-- Restaurant Profile -->
+              <div class="is-inline-flex flex-center m-l-16 m-t-24">
+                <div>
+                  <img :src="shopInfo.restProfilePhoto" class="w-36 h-36 r-36 cover" />
+                </div>
+                <div class="t-h6 c-text-black-high m-l-8">{{ shopInfo.restaurantName }}</div>
               </div>
-              <div class="t-h6 c-text-black-high m-l-8">{{ shopInfo.restaurantName }}</div>
             </div>
           </div>
         </div>
+        <!-- Right Gap -->
+        <div class="column is-narrow w-24"></div>
       </div>
-      <!-- Right Gap -->
-      <div class="column is-narrow w-24"></div>
-    </div>
 
-    <!-- Order Body Area -->
-    <div class="columns is-gapless">
-      <!-- Left Gap -->
-      <div class="column is-narrow w-24"></div>
+      <!-- Order Body Area -->
+      <div class="columns is-gapless">
+        <!-- Left Gap -->
+        <div class="column is-narrow w-24"></div>
 
-      <!-- Left Column -->
-      <div class="column">
-        <div class="m-l-24 m-r-24">
-          <div class="bg-surface r-8 d-low p-l-24 p-r-24 p-t-24 p-b-24 m-t-24">
-            <!-- Order Overview -->
-            <div>
-              <div class="align-center">
-                <div class="is-inline-flex">
-                  <!-- Order ID -->
-                  <div class="t-h4 c-text-black-high">{{orderName}}</div>
+        <!-- Left Column -->
+        <div class="column">
+          <div class="m-l-24 m-r-24">
+            <div class="bg-surface r-8 d-low p-l-24 p-r-24 p-t-24 p-b-24 m-t-24">
+              <!-- Order Overview -->
+              <div>
+                <div class="align-center">
+                  <div class="is-inline-flex">
+                    <!-- Order ID -->
+                    <div class="t-h4 c-text-black-high">{{orderName}}</div>
 
-                  <!-- Total Charge -->
-                  <div class="m-l-16">
-                    <div class="t-caption c-text-black-medium">{{$t('order.totalCharge')}}</div>
-                    <div class="t-body1 c-textl-black-high is-inline-flex flex-center">
-                      <div>{{$n(orderInfo.totalCharge, 'currency')}}</div>
-                      <div v-if="hasStripe" class="m-l-4">
-                        <i :class="'fab fa-cc-stripe stripe_'+orderInfo.payment.stripe"></i>
+                    <!-- Total Charge -->
+                    <div class="m-l-16">
+                      <div class="t-caption c-text-black-medium">{{$t('order.totalCharge')}}</div>
+                      <div class="t-body1 c-textl-black-high is-inline-flex flex-center">
+                        <div>{{$n(orderInfo.totalCharge, 'currency')}}</div>
+                        <div v-if="hasStripe" class="m-l-4">
+                          <i :class="'fab fa-cc-stripe stripe_'+orderInfo.payment.stripe"></i>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
 
-              <!-- Cancel Button -->
-              <div class="m-t-24 align-center">
-                <b-button
-                  class="b-reset op-button-pill h-36 bg-status-red-bg"
-                  :disabled="!isValidTransition('order_canceled')"
-                  @click="openCancel()"
-                >
-                  <i class="material-icons c-status-red s-18 m-l-8">delete</i>
-                  <span class="c-status-red t-button">{{ $t("admin.order.cancelButton" )}}</span>
-                </b-button>
-              </div>
+                <!-- Cancel Button -->
+                <div class="m-t-24 align-center">
+                  <b-button
+                    class="b-reset op-button-pill h-36 bg-status-red-bg"
+                    :disabled="!isValidTransition('order_canceled')"
+                    @click="openCancel()"
+                  >
+                    <i class="material-icons c-status-red s-18 m-l-8">delete</i>
+                    <span class="c-status-red t-button">{{ $t("admin.order.cancelButton" )}}</span>
+                  </b-button>
+                </div>
 
-              <!-- Cancel Popup-->
-              <b-modal :active.sync="cancelPopup" :width="488" scroll="keep">
-                <div class="op-dialog p-t-24 p-l-24 p-r-24 p-b-24">
-                  <div class="t-h6 c-text-black-disabled">{{$t("admin.order.cancelTitle")}}</div>
-                  <div class="t-body1 c-text-black-high m-t-24">{{$t("admin.order.cancelMessage")}}</div>
-                  <!-- CTA: Call -->
-                  <div v-if="orderInfo.phoneNumber" class="m-t-24 align-center">
-                    <div>
-                      <a :href="nationalPhoneURI">
-                        <div class="op-button-small w-256 secondary">{{nationalPhoneNumber}}</div>
-                      </a>
-                    </div>
-                    <div class="t-subtitle2 c-text-black-medium m-t-8">{{orderInfo.name}}</div>
-                  </div>
-                  <!-- CTA: Cancel -->
-                  <div class="align-center m-t-16">
-                    <b-button
-                      class="b-reset op-button-small d-low bg-status-red w-256"
-                      :loading="updating==='order_canceled'"
-                      @click="handleCancel"
-                    >
-                      <span class="c-text-white-full">{{$t("admin.order.delete")}}</span>
-                    </b-button>
-                    <div class="t-subtitle2 c-status-red m-t-8">{{$t("admin.order.deleteConfirm")}}</div>
-                  </div>
-                  <!-- CTA: Close -->
-                  <div class="m-t-24 align-center">
+                <!-- Cancel Popup-->
+                <b-modal :active.sync="cancelPopup" :width="488" scroll="keep">
+                  <div class="op-dialog p-t-24 p-l-24 p-r-24 p-b-24">
+                    <div class="t-h6 c-text-black-disabled">{{$t("admin.order.cancelTitle")}}</div>
                     <div
-                      class="op-button-small tertiary"
-                      @click="closeCancel()"
-                    >{{$t('menu.close')}}</div>
+                      class="t-body1 c-text-black-high m-t-24"
+                    >{{$t("admin.order.cancelMessage")}}</div>
+                    <!-- CTA: Call -->
+                    <div v-if="orderInfo.phoneNumber" class="m-t-24 align-center">
+                      <div>
+                        <a :href="nationalPhoneURI">
+                          <div class="op-button-small w-256 secondary">{{nationalPhoneNumber}}</div>
+                        </a>
+                      </div>
+                      <div class="t-subtitle2 c-text-black-medium m-t-8">{{orderInfo.name}}</div>
+                    </div>
+                    <!-- CTA: Cancel -->
+                    <div class="align-center m-t-16">
+                      <b-button
+                        class="b-reset op-button-small d-low bg-status-red w-256"
+                        :loading="updating==='order_canceled'"
+                        @click="handleCancel"
+                      >
+                        <span class="c-text-white-full">{{$t("admin.order.delete")}}</span>
+                      </b-button>
+                      <div
+                        class="t-subtitle2 c-status-red m-t-8"
+                      >{{$t("admin.order.deleteConfirm")}}</div>
+                    </div>
+                    <!-- CTA: Close -->
+                    <div class="m-t-24 align-center">
+                      <div
+                        class="op-button-small tertiary"
+                        @click="closeCancel()"
+                      >{{$t('menu.close')}}</div>
+                    </div>
+                  </div>
+                </b-modal>
+
+                <!-- Pickup Time -->
+                <div class="m-t-24 align-center">
+                  <div class="t-caption c-text-black-medium">{{$t('order.timeToPickup')}}</div>
+                  <div class="t-body1 c-textl-black-high m-t-4">{{timePlaced}}</div>
+                </div>
+
+                <!-- Phone Number -->
+                <div v-if="orderInfo.phoneNumber" class="align-center m-t-16">
+                  <div class="t-caption c-text-black-medium">{{$t('sms.phonenumber')}}</div>
+                  <div class="t-body1 m-t-4">
+                    <div>
+                      <a :href="nationalPhoneURI">{{nationalPhoneNumber}}</a>
+                    </div>
+                    <div>{{orderInfo.name}}</div>
                   </div>
                 </div>
-              </b-modal>
-
-              <!-- Pickup Time -->
-              <div class="m-t-24 align-center">
-                <div class="t-caption c-text-black-medium">{{$t('order.timeToPickup')}}</div>
-                <div class="t-body1 c-textl-black-high m-t-4">{{timePlaced}}</div>
               </div>
 
-              <!-- Phone Number -->
-              <div v-if="orderInfo.phoneNumber" class="align-center m-t-16">
-                <div class="t-caption c-text-black-medium">{{$t('sms.phonenumber')}}</div>
-                <div class="t-body1 m-t-4">
-                  <div>
-                    <a :href="nationalPhoneURI">{{nationalPhoneNumber}}</a>
-                  </div>
-                  <div>{{orderInfo.name}}</div>
+              <!-- Order Status -->
+              <div>
+                <div
+                  v-for="orderState in orderStates"
+                  :key="orderState"
+                  class="align-center m-t-24"
+                >
+                  <b-button
+                    class="op-button-medium w-256"
+                    :class="classOf(orderState)"
+                    :loading="updating===orderState"
+                    :disabled="!isValidTransition(orderState)"
+                    @click="handleChangeStatus(orderState)"
+                  >{{ $t("order.status." + orderState) }}</b-button>
                 </div>
-              </div>
-            </div>
-
-            <!-- Order Status -->
-            <div>
-              <div v-for="orderState in orderStates" :key="orderState" class="align-center m-t-24">
-                <b-button
-                  class="op-button-medium w-256"
-                  :class="classOf(orderState)"
-                  :loading="updating===orderState"
-                  :disabled="!isValidTransition(orderState)"
-                  @click="handleChangeStatus(orderState)"
-                >{{ $t("order.status." + orderState) }}</b-button>
-              </div>
-              <div class="align-center m-t-24">
-                <b-button
-                  class="op-button-medium w-256"
-                  :class="classOf('customer_picked_up')"
-                  :loading="updating==='customer_picked_up'"
-                  :disabled="!isValidTransition('customer_picked_up')"
-                  @click="handleComplete()"
-                >{{ $t("order.status." + 'customer_picked_up') }}</b-button>
+                <div class="align-center m-t-24">
+                  <b-button
+                    class="op-button-medium w-256"
+                    :class="classOf('customer_picked_up')"
+                    :loading="updating==='customer_picked_up'"
+                    :disabled="!isValidTransition('customer_picked_up')"
+                    @click="handleComplete()"
+                  >{{ $t("order.status." + 'customer_picked_up') }}</b-button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <!-- Right Column -->
-      <div class="column">
-        <div class="m-l-24 m-r-24">
-          <div class="m-t-24">
-            <!-- Order Items -->
-            <ordered-item v-for="id in ids" :key="id" :item="items[id]" />
+        <!-- Right Column -->
+        <div class="column">
+          <div class="m-l-24 m-r-24">
+            <div class="m-t-24">
+              <!-- Order Items -->
+              <ordered-item v-for="id in ids" :key="id" :item="items[id]" />
+            </div>
           </div>
         </div>
+        <!-- Right Gap -->
+        <div class="column is-narrow w-24"></div>
       </div>
-      <!-- Right Gap -->
-      <div class="column is-narrow w-24"></div>
-    </div>
+    </template>
   </div>
 </template>
 
@@ -173,11 +186,13 @@ import {
 } from "~/plugins/phoneutil.js";
 import { stripeConfirmIntent, stripeCancelIntent } from "~/plugins/stripe.js";
 import moment from "moment";
+import NotFound from "~/components/NotFound";
 
 export default {
   components: {
     BackButton,
-    OrderedItem
+    OrderedItem,
+    NotFound
   },
 
   data() {
@@ -189,7 +204,8 @@ export default {
       orderInfo: {},
       canceling: false,
       detacher: [],
-      cancelPopup: false
+      cancelPopup: false,
+      notFound: false
     };
   },
 
@@ -212,10 +228,16 @@ export default {
       });
     const order_detacher = db
       .doc(`restaurants/${this.restaurantId()}/orders/${this.orderId}`)
-      .onSnapshot(order => {
-        if (order.exists) {
-          const order_data = order.data();
-          this.orderInfo = order_data;
+      .onSnapshot({
+        next: order => {
+          if (order.exists) {
+            const order_data = order.data();
+            this.orderInfo = order_data;
+          }
+        },
+        error: error => {
+          console.error("error", error.message);
+          this.notFound = true;
         }
       });
     this.detacher = [restaurant_detacher, menu_detacher, order_detacher];
