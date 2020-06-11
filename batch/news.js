b3c61c3c815d00543ff3a00eef35800d879e163d
Isamu Arimoto <isamu@to-kyo.to>
2020-06-11 09:14:06 +0900

    add current

--- batch/news.js
+++ batch/news.js
@@ -6,26 +6,36 @@ const dirpath = "./docs/news/";
 const release_notes = "./docs/RELEASES.md";
 const write_data = [];
 
-const release_note_fd = fs.openSync(release_notes, "w");
-
-fs.writeSync(release_note_fd, "# Release notes\n");
-
-const files = fs.readdirSync(dirpath).filter((file) => {
-  return file.match(/^(\d+)\.md$/);
-}).sort().reverse().map((file) => {
-
+const getData = (file) => {
   const date = `${file.substring(0,4)}-${file.substring(4,6)}-${file.substring(6,8)}`;
   const md = fs.readFileSync(dirpath + file, 'utf8');
   const tmp = md.split("\n");
   const title = tmp.shift();
   const body = tmp.join("\n");
-
-  write_data.push({
+  return {
     title,
     date,
     markdown: body,
-  });
-  fs.writeSync(release_note_fd, body);
+  };
+};
+
+const release_note_fd = fs.openSync(release_notes, "w");
+
+fs.writeSync(release_note_fd, "# Release notes\n");
+fs.writeSync(release_note_fd, "\n");
+
+const currentData = getData("current.md");
+fs.writeSync(release_note_fd, currentData.markdown);
+fs.writeSync(release_note_fd, "\n");
+
+
+const files = fs.readdirSync(dirpath).filter((file) => {
+  return file.match(/^(\d+)\.md$/);
+}).sort().reverse().map((file) => {
+  const data = getData(file);
+
+  write_data.push(data);
+  fs.writeSync(release_note_fd, data.markdown);
   fs.writeSync(release_note_fd, "\n");
 });
 fs.closeSync(release_note_fd);
