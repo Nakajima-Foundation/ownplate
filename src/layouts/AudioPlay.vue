a8324f2d4c063043d759577b8eb00c083721ef96
Isamu Arimoto <isamu@to-kyo.to>
2020-06-16 09:57:42 +0900

    fix audio

--- src/layouts/AudioPlay.vue
+++ src/layouts/AudioPlay.vue
@@ -1,11 +1,9 @@
 <template>
   <div>
-    <iframe src="/silence.mp3" allow="autoplay" id="audio_s" style="display:none"></iframe>
     <audio id="audio" ref="audio" :src="soundFile">
     </audio>
   </div>
 </template>
-
 <script>
 export default {
   data() {
@@ -20,19 +18,19 @@ export default {
   },
   methods: {
     async enableSound() {
+      console.log("enableSound");
       if (!this.pleyedSilent) {
-        console.log("default: enableSound");
+        console.log("silent play");
+        this.$refs.audio.setAttribute("src", "/silence.mp3");
+        this.$refs.audio.currentTime = 0;
+        await this.$refs.audio.play();
 
         this.pleyedSilent = true;
         this.$store.commit("soundEnable");
       }
     },
     async play() {
-      console.log(this.$refs.audio)
-      // this.$refs.source.src = this.soundFile;
       this.$refs.audio.setAttribute("src", this.soundFile);
-      //  a.src = this.soundFile;
-      // this.$refs.audio.pause();
       this.$refs.audio.currentTime = 0;
       await this.$refs.audio.play();
       return;
@@ -42,7 +40,6 @@ export default {
     async "$store.state.orderEvent"() {
       await this.play();
       console.log(`soundEnable = ${this.$store.state.soundEnable}, soundOn=${this.$store.state.soundOn}, soundFile=${this.$store.state.soundFile}`);
-      console.log(this.$store.state.orderEvent);
     },
   }
 }
