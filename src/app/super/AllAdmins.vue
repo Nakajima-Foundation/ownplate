0f0a464ac984890d280e71077b8ecfe42cc98a74
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-06-23 10:26:45 -0700

    list of names

--- src/app/super/AllAdmins.vue
+++ src/app/super/AllAdmins.vue
@@ -1,5 +1,35 @@
 <template>
   <section class="section">
-    <p>All Admins</p>
+    <h2>All Admins</h2>
+    <table>
+      <tr v-for="admin in admins" :key="admin.id">
+        <td>{{admin.name}}</td>
+      </tr>
+    </table>
   </section>
 </template>
+
+<script>
+import { db } from "~/plugins/firebase.js";
+
+export default {
+  data() {
+    return {
+      admins: [],
+      detacher: null
+    };
+  },
+  created() {
+    this.detacher = db
+      .collection("admins")
+      .limit(100)
+      .onSnapshot(snapshot => {
+        this.admins = snapshot.docs.map(this.doc2data("admin"));
+        console.log(this.admins);
+      });
+  },
+  destroyed() {
+    this.detatcher && this.detatcher();
+  }
+};
+</script>
