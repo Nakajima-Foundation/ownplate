00b306934a0f1acb13daf74bbe555de614587c76
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-07-07 10:22:28 -0700

    Profiles Page

---
+++ src/app/super/Profiles.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="section">
+    <back-button url="/s" />
+    <h2>Profiles</h2>
+  </section>
+</template>
+
+<script>
+import BackButton from "~/components/BackButton";
+export default {
+  components: {
+    BackButton
+  }
+};
+</script>
