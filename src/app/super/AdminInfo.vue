ef0e0502bc8573c36a521238b01bd2678d27c93f
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-07-07 15:55:43 -0700

    getCustomClaims

--- src/app/super/AdminInfo.vue
+++ src/app/super/AdminInfo.vue
@@ -1,8 +1,10 @@
 <template>
   <section class="section">
     <back-button url="/s/admins" />
-    <h1>Admin Info</h1>
-    <h4>Restaurants</h4>
+    <h1>Custome Claims</h1>
+    <div>Admin: {{ customClaims.admin }}</div>
+    <div>Operator: {{ customClaims.operator || false }}</div>
+    <h1>Restaurants</h1>
     <div v-for="restaurant in restaurants" :key="restaurant.id">
       <restaurant :restaurant="restaurant"></restaurant>
     </div>
@@ -20,20 +22,27 @@ export default {
   },
   data() {
     return {
+      customClaims: {},
       restaurants: []
     };
   },
   computed: {
+    adminId() {
+      return this.$route.params.adminId;
+    },
     superDispatch() {
       return functions.httpsCallable("superDispatch");
     }
   },
   async mounted() {
-    const { data } = await this.superDispatch();
-    console.log("***", data);
+    const { data } = await this.superDispatch({
+      cmd: "getCustomeClaims",
+      uid: this.adminId
+    });
+    this.customClaims = data.result;
     const snapshot = await db
       .collection("/restaurants/")
-      .where("uid", "==", this.$route.params.adminId)
+      .where("uid", "==", this.adminId)
       .get();
     this.restaurants = snapshot.docs.map(this.doc2data("admin"));
     console.log(this.restaurants);
