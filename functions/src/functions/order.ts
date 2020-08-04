f07193adcdafd9d88ee1589554466983f748f51c
Isamu Arimoto <isamu@to-kyo.to>
2020-08-04 17:02:12 +0900

    more log

--- functions/src/functions/order.ts
+++ functions/src/functions/order.ts
@@ -212,6 +212,8 @@ const notifyRestaurant = async (db: FirebaseFirestore.Firestore, messageId: stri
         await twilio.phoneCall(restaurant);
         const datestr = moment().format("YYYY-MM-DD");
         await db.doc(`/restaurants/${restaurantId}/log/${datestr}/phoneLog/${orderId}`).set({
+          restaurantId,
+          date: datestr,
           orderId,
           phoneNumber: restaurant.phoneNumber,
           updatedAt: admin.firestore.Timestamp.now(),
