6b63d83e90c5ed01563a5e08f16fc2650e5d998b
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-08-05 04:22:05 -0700

    clean up

--- src/app/admin/OrderHistory.vue
+++ src/app/admin/OrderHistory.vue
@@ -12,7 +12,10 @@
             <!-- Back Button and Restaurant Profile -->
             <div class="level-left flex-1">
               <!-- Back Button -->
-              <back-button url="/admin/restaurants/" class="m-t-24 m-r-16" />
+              <back-button
+                :url="`/admin/restaurants/${restaurantId()}/orders`"
+                class="m-t-24 m-r-16"
+              />
 
               <!-- Restaurant Profile -->
               <div class="is-inline-flex flex-center m-t-24">
@@ -45,17 +48,7 @@
           <!-- Date and Sound -->
           <div class="level">
             <!-- Select Date -->
-            <div class="level-left">
-              <b-select v-model="dayIndex" class="m-t-24">
-                <option v-for="day in lastSeveralDays" :value="day.index" :key="day.index">
-                  {{ $d(day.date, "short") }} {{ orderCounter[moment(day.date).format("YYYY-MM-DD")] }}
-                  <span
-                    v-if="day.index === pickUpDaysInAdvance"
-                  >{{ $t("date.today") }}</span>
-                </option>
-              </b-select>
-            </div>
-
+            <div class="level-left"></div>
             <div class="level-right"></div>
           </div>
         </div>
@@ -107,19 +100,8 @@ export default {
   data() {
     return {
       shopInfo: {},
-      orders: [],
-      dayIndex: 0,
-      order_detacher: () => {}
+      orders: []
     };
-  },
-  watch: {
-    dayIndex() {
-      this.updateQueryDay();
-      this.dateWasUpdated();
-    },
-    "$route.query.day"() {
-      this.updateDayIndex();
-    }
   },
   async created() {
     this.checkAdminPermission();
@@ -131,71 +113,10 @@ export default {
       return;
     }
     this.shopInfo = restaurantDoc.data();
-    this.dayIndex = this.getPickUpDaysInAdvance();
 
-    if (this.$route.query.day) {
-      this.updateDayIndex();
-    }
-    this.dateWasUpdated();
-  },
-  destroyed() {
-    this.order_detacher();
-  },
-  computed: {
-    orderCounter() {
-      return this.lastSeveralDays.reduce((tmp, day) => {
-        const count = (
-          this.$store.state.orderObj[moment(day.date).format("YYYY-MM-DD")] ||
-          []
-        ).length;
-        if (count > 0) {
-          tmp[moment(day.date).format("YYYY-MM-DD")] = "(" + count + ")";
-        }
-        return tmp;
-      }, {});
-    },
-    pickUpDaysInAdvance() {
-      return this.getPickUpDaysInAdvance();
-    },
-    lastSeveralDays() {
-      return Array.from(Array(10 + this.pickUpDaysInAdvance).keys()).map(
-        index => {
-          const date = midNight(this.pickUpDaysInAdvance - index);
-          return { index, date };
-        }
-      );
-    }
-  },
-  methods: {
-    updateDayIndex() {
-      const dayIndex =
-        this.lastSeveralDays.findIndex(day => {
-          return (
-            moment(day.date).format("YYYY-MM-DD") === this.$route.query.day
-          );
-        }) || 0;
-      this.dayIndex = dayIndex > 0 ? dayIndex : 0;
-    },
-    updateQueryDay() {
-      const day = moment(this.lastSeveralDays[this.dayIndex].date).format(
-        "YYYY-MM-DD"
-      );
-      this.$router.push({
-        path: "/admin/restaurants/" + this.restaurantId() + "/orders?day=" + day
-      });
-    },
-    dateWasUpdated() {
-      this.order_detacher();
-      let query = db
-        .collection(`restaurants/${this.restaurantId()}/orders`)
-        .where("timePlaced", ">=", this.lastSeveralDays[this.dayIndex].date);
-      if (this.dayIndex > 0) {
-        query = query.where(
-          "timePlaced",
-          "<",
-          this.lastSeveralDays[this.dayIndex - 1].date
-        );
-      }
+    let query = db.collection(`restaurants/${this.restaurantId()}/orders`);
+    //  .where("timePlaced", ">=", this.lastSeveralDays[this.dayIndex].date);
+    /*
       this.order_detacher = query.onSnapshot(result => {
         let orders = result.docs.map(this.doc2data("order"));
         orders = orders.sort((order0, order1) => {
@@ -215,17 +136,15 @@ export default {
           return order;
         });
       });
-    },
+    */
+  },
+  computed: {},
+  methods: {
     orderSelected(order) {
       this.$router.push({
         path:
           "/admin/restaurants/" + this.restaurantId() + "/orders/" + order.id
       });
-    },
-    getPickUpDaysInAdvance() {
-      return this.isNull(this.shopInfo.pickUpDaysInAdvance)
-        ? 3
-        : this.shopInfo.pickUpDaysInAdvance;
     }
   }
 };
