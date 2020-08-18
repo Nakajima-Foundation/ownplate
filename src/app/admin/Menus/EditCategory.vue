32207b0424b4f07ee4ab6dcbd9771c4d2f1138ac
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-08-18 09:00:53 -0700

    edit category UI

--- src/app/admin/Menus/EditCategory.vue
+++ src/app/admin/Menus/EditCategory.vue
@@ -1,21 +1,20 @@
 <template>
   <b-modal :active.sync="isVisible" :width="488">
     <div class="op-dialog p-t-24 p-l-24 p-r-24 p-b-24">
-      <div>
-        <div class="m-t-4" v-for="(category, index) in categories" :key="category">
-          {{ category }}
-          <b-button
-            class="b-reset op-button-pill h-36 bg-status-red-bg m-l-8"
-            @click="handleDelete(index)"
-          >
+      <div style="height:16em;overflow-y:scroll;border:1px #ccc solid;border-radius:4px">
+        <div class="p-l-4 p-r-4" v-for="(category, index) in categories" :key="category">
+          <span style="position:relative; top:10px">{{ category }}</span>
+          <b-button class="b-reset h-36 m-l-8" @click="handleDelete(index)">
             <i class="material-icons c-status-red s-18 p-l-8 p-r-8">delete</i>
           </b-button>
         </div>
       </div>
-      <b-input placeholder="New Category" v-model="newEntry" />
-      <b-button @click="handleAdd">
-        <i class="material-icons">add</i>
-      </b-button>
+      <div class="cols m-t-16">
+        <b-input class="flex-1" placeholder="New Category" v-model="newEntry" />
+        <b-button class="b-reset m-l-8 op-button-pill h-36 bg-form" @click="handleAdd">
+          <i class="material-icons c-primary">add</i>
+        </b-button>
+      </div>
     </div>
   </b-modal>
 </template>
