48fcb7a560d9d68c636c66e77397207623a92f6f
Isamu Arimoto <isamu@to-kyo.to>
2020-11-17 17:29:36 +0900

    add new stamp image

--- src/app/admin/Pdf.vue
+++ src/app/admin/Pdf.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <button @click="download">Download example</button>
+    <button @click="download">Download menu example</button><br/>
+    <button @click="download2">Download logo</button><br/>
 
   </div>
 </template>
@@ -10,7 +11,7 @@ import { db } from "~/plugins/firebase.js";
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 
-import logosvg from "!raw-loader!../../static/Omochikaeri-Logo-Stack-YellowBlack.svg";
+import logosvg from "!raw-loader!../../static/pr/50mm-QR-Blank.svg";
 
 import _ from 'lodash';
 
@@ -68,13 +69,6 @@ export default {
           {
             width: '20%',
             stack: [
-              /*
-              { ul: [
-                'Option 1',
-                'Option 2',
-                'Option 3',
-              ]},
-               */
               { text:  menu.itemDescription },
               { text:  menu.price + "円", bold: true },
             ]
@@ -106,66 +100,69 @@ export default {
       content.push({ svg: logosvg, width: 150 });
       content.push({ text: 'こんにちは。これはPDFのサンプルです。日本語が入力可能かどうか確認しています。これが表示できればとても助かります。', margin: [0, 10] });
       content.push({ qr: 'https://nodejs.keicode.com/' , fit: '50' });
-      /*
-          'No styling here, this is a standard paragraph',
-          { text: 'これはマージンを指定しています。', margin: [0, 10]},
-          { text: '複数のスタイルを指定しています。', style: ['h1', 'style2'] },
-          {
-            // to treat a paragraph as a bulleted list, set an array of items under the ul key
-            ul: [
-              'Item 1',
-              'Item 2',
-              'Item 3',
-              { text: 'Item 4', bold: true },
-            ]
-          },
-          {
-            layout: 'lightHorizontalLines', // optional
-            table: {
-              // headers are automatically repeated if the table spans over multiple pages
-              // you can declare how many rows should be treated as headers
-              headerRows: 1,
-              widths: [ '*', 'auto', 100, '*' ],
 
-              body: [
-                [ 'First', 'Second', 'Third', 'The last one' ],
-                [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
-                [ { text: 'Bold value', bold: true }, 'Val 2', 'Val 3', 'Val 4' ]
-              ]
-            }
+      const docDefinition = {
+        content,
+        images,
+        styles: {
+          title: {
+            font: 'GenShin',
+            fontSize: 24,
           },
-        ],
-*/
-
-      content.push({
-        pageBreak: 'before',
-        text: 'First page',
-        absolutePosition: {
-          x: 40,
-          y: 0
+          h1: {
+            font: 'GenShin',
+            fontSize: 18,
+            bold: true
+          },
+          style2: {
+            alignment: 'right',
+            color: 'blue',
+          }
+        },
+        defaultStyle: {
+          font: 'GenShin',
+          fontSize: 14,
         }
-      });
+      };
+      const pdfDoc = pdfMake.createPdf(docDefinition).download();
+    },
+
+    download2() {
+      pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+      const host = location.protocol + "//" + location.host + "/fonts/";
+      pdfMake.fonts = {
+        GenShin: {
+          normal: host + 'GenShinGothic-Normal-Sub.ttf',
+          bold: host + 'GenShinGothic-Normal-Sub.ttf',
+          italics: host + 'GenShinGothic-Normal-Sub.ttf',
+          bolditalics: host + 'GenShinGothic-Normal-Sub.ttf'
+        }
+      };
+
+      const content = [
+        { text: this.restaurantInfo.restaurantName, style: 'title' },
+        { text: this.shareUrl(), style: 'title' },
+      ];
+      const images = {
+        logo: location.protocol + "//" + location.host + '/OwnPlate-Logo-Stack-YellowBlack.png',
+        menu: location.protocol + "//" + location.host + '/test.jpg', // TODO: Set default menu image
+      };
+      // TODO: fix Japanese kansuuji encoding issue
+
       content.push({ svg: logosvg,
-                     width: 240,
+                     width: 251,
                      absolutePosition: {
-                       x: 40,
-                       y: 100
+                       x: 60,
+                       y: 130
                      }
                    });
       content.push({
         qr: this.shareUrl(),
-        fit: '50',
+        fit: 75,
         absolutePosition: {
-          x: 135,
-          y: 120
-        }
-      });
-
-      content.push({
-        text: 'Second page',
-        absolutePosition: {
-          x: 40,
-          y: 20
+          x: 148,
+          y: 266
         }
       });
 
@@ -195,6 +192,7 @@ export default {
       console.log(docDefinition);
       const pdfDoc = pdfMake.createPdf(docDefinition).download();
     }
+
   }
 }
 </script>
