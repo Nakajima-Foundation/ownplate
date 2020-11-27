40311d02052ff64bc192c731f81503d7c60c1750
Isamu Arimoto <isamu@to-kyo.to>
2020-11-28 08:43:31 +0900

    update lp

--- src/components/lp/communityIcons.vue
+++ src/components/lp/communityIcons.vue
@@ -1,7 +1,10 @@
 <template>
+<div class="columns is-gapless">
   <div>
+    <h2>ユーザー・コミュニティで助け合い</h2>
     fb, youtube
   </div>
+</div>
 </template>
 <script>
 export default {
--- src/components/lp/mediaLink.vue
+++ src/components/lp/mediaLink.vue
@@ -1,8 +1,11 @@
 <template>
+<div class="columns is-gapless">
   <div>
+    <h2>これらのメディアで紹介されました</h2>
     日経
     NHK
   </div>
+</div>
 </template>
 <script>
 export default {
