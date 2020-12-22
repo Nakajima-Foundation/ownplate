a21c307e353a93fab136a29ed431a641d5ed373b
Yuta <yuta.ok@gmail.com>
2020-12-22 13:17:34 -0800

    Extract the Favorite button from the SharePopup component

--- src/app/user/Restaurant/SharePopup.vue
+++ src/app/user/Restaurant/SharePopup.vue
@@ -1,33 +1,22 @@
 <template>
-  <div>
-    <!-- Share / Favorite Buttons -->
-    <div class="m-t-8 align-center">
-      <div class="op-button-text m-r-8" @click="openShare()">
-        <i class="material-icons">launch</i>
-        <span>{{$t('shopInfo.share')}}</span>
-      </div>
-
-      <!-- Favorite -->
-      <div class="is-inline-block" v-if="isUser">
-        <div class="op-button-text" @click="handleLike">
-          <!-- Like -->
-          <template v-if="likes">
-            <i class="material-icons c-status-red">favorite</i>
-            <span class="c-status-red">{{$t('shopInfo.liked')}}</span>
-          </template>
-          <!-- Liked -->
-          <template v-else>
-            <i class="material-icons">favorite_border</i>
-            <span>{{$t('shopInfo.like')}}</span>
-          </template>
-        </div>
-      </div>
+  <div class="is-inline-block">
+    <!-- Share  Buttons -->
+    <div class="op-button-text m-r-8" @click="openShare()">
+      <i class="material-icons">launch</i>
+      <span>{{ $t("shopInfo.share") }}</span>
     </div>
 
     <!-- Share Popup-->
-    <b-modal :active.sync="sharePopup" :width="488" scroll="keep" style="text-align: initial;">
+    <b-modal
+      :active.sync="sharePopup"
+      :width="488"
+      scroll="keep"
+      style="text-align: initial;"
+    >
       <div class="op-dialog p-t-24 p-l-24 p-r-24 p-b-24">
-        <div class="t-h6 c-text-black-disabled p-b-8">{{$t('shopInfo.share')}}</div>
+        <div class="t-h6 c-text-black-disabled p-b-8">
+          {{ $t("shopInfo.share") }}
+        </div>
         <div class="cols">
           <div>
             <qrcode :value="url" :options="{ width: 160 }"></qrcode>
@@ -36,17 +25,21 @@
             <nuxt-link to="#" @click.native="copyClipboard(url)" event>
               <div class="op-button-text m-t-8">
                 <i class="material-icons">file_copy</i>
-                <span>{{$t('shopInfo.copyUrl')}}</span>
+                <span>{{ $t("shopInfo.copyUrl") }}</span>
               </div>
             </nuxt-link>
-            <div class="m-l-8 t-body2 c-text-black-disabled">{{this.url}}</div>
+            <div class="m-l-8 t-body2 c-text-black-disabled">
+              {{ this.url }}
+            </div>
             <div class="m-t-24">
               <sharing-buttons :title="shopInfo.restaurantName" :url="url" />
             </div>
           </div>
         </div>
         <div class="m-t-24 align-center">
-          <div class="op-button-small tertiary" @click="closeShare()">{{$t('menu.close')}}</div>
+          <div class="op-button-small tertiary" @click="closeShare()">
+            {{ $t("menu.close") }}
+          </div>
         </div>
       </div>
     </b-modal>
@@ -119,19 +112,6 @@ export default {
     },
     closeShare() {
       this.sharePopup = false;
-    },
-    handleLike() {
-      // Notice that mounted() will automatically update duplicated restaurant info.
-      db.doc(`users/${this.user.uid}/reviews/${this.restaurantId()}`).set(
-        {
-          likes: !this.likes,
-          restaurantName: this.shopInfo.restaurantName, // duplicated for quick display
-          restProfilePhoto: this.shopInfo.restProfilePhoto, // duplicated for quick display
-          timeLiked: firestore.FieldValue.serverTimestamp(),
-          restaurantId: this.restaurantId() // Making it possible to collection query (later)
-        },
-        { merge: true }
-      );
     }
   }
 };
