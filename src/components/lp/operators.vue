cbf2342afef9f7f8a931f4ae2f4e1f9194c20635
Yuta <yuta.ok@gmail.com>
2021-01-19 13:04:05 -0800

    Add ITC logo

--- src/components/lp/operators.vue
+++ src/components/lp/operators.vue
@@ -4,7 +4,7 @@
       class="align-center is-size-3-tablet is-size-5-mobile c-text-black-disabled p-t-48"
       style="line-height: 1.4; font-weight: bold;"
     >
-      わたしたちが運営しています！
+      わたしたちが運営しています
     </div>
 
     <div class="align-center">
@@ -13,6 +13,17 @@
         class="h-80 m-t-32 m-l-16 m-r-16"
       />
       <!-- img src="/Logo-Itochu.svg" class="h-80 m-t-32 m-l-16 m-r-16" / -->
+    </div>
+
+    <div
+      class="align-center is-size-3-tablet is-size-5-mobile c-text-black-disabled p-t-48"
+      style="line-height: 1.4; font-weight: bold;"
+    >
+      わたしたちがサポートしています
+    </div>
+
+    <div class="align-center">
+      <img src="/Logo-Itochu.svg" class="h-80 m-t-48 m-l-16 m-r-16" />
     </div>
   </div>
 </template>
