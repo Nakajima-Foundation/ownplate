86570038508aa793d15217661774272a3315138a
Yuta Okajima <yuta.ok@gmail.com>
2021-02-25 01:27:35 -0800

    Add back div due to the compatibility

--- src/components/BackButton.vue
+++ src/components/BackButton.vue
@@ -1,14 +1,16 @@
 <template>
-  <nuxt-link :to="url">
-    <div
-      class="inline-flex justify-center items-center rounded-full h-9 bg-black bg-opacity-5 px-4"
-    >
-      <i class="material-icons text-lg text-op-teal mr-2">arrow_back</i>
-      <span class="text-sm font-bold text-op-teal">{{
-        $t("button.back")
-      }}</span>
-    </div>
-  </nuxt-link>
+  <div>
+    <nuxt-link :to="url">
+      <div
+        class="inline-flex justify-center items-center rounded-full h-9 bg-black bg-opacity-5 px-4"
+      >
+        <i class="material-icons text-lg text-op-teal mr-2">arrow_back</i>
+        <span class="text-sm font-bold text-op-teal">{{
+          $t("button.back")
+        }}</span>
+      </div>
+    </nuxt-link>
+  </div>
 </template>
 
 <script>
