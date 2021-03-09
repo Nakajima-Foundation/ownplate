d4a55760ea1b9f168c1192adafd928af6901b45f
Yuta Okajima <yuta.ok@gmail.com>
2021-03-09 10:56:22 -0800

    Update the name of the sent from

--- functions/src/functions/ses.ts
+++ functions/src/functions/ses.ts
@@ -1,25 +1,33 @@
-import * as functions from 'firebase-functions'
-import * as nodemailer from 'nodemailer';
+import * as functions from "firebase-functions";
+import * as nodemailer from "nodemailer";
 
-const aws_key =  functions.config() && functions.config().aws &&  functions.config().aws.ses_user || process.env.AWS_SES_USER;
-const aws_secret =  functions.config() && functions.config().aws &&  functions.config().aws.ses_pass || process.env.AWS_SES_PASS;
+const aws_key =
+  (functions.config() &&
+    functions.config().aws &&
+    functions.config().aws.ses_user) ||
+  process.env.AWS_SES_USER;
+const aws_secret =
+  (functions.config() &&
+    functions.config().aws &&
+    functions.config().aws.ses_pass) ||
+  process.env.AWS_SES_PASS;
 
 export const sendMail = async (to, title, body) => {
   const mailOptions = {
-    from: 'omochikaeri <info@omochikaeri.com>',
+    from: "おもちかえり.com <info@omochikaeri.com>",
     to,
     text: body,
     // html: html,
-    subject: title,
+    subject: title
   };
   if (aws_key && aws_secret) {
     const smtpTransporter = nodemailer.createTransport({
       port: 465,
-      host: 'email-smtp.ap-northeast-1.amazonaws.com',
+      host: "email-smtp.ap-northeast-1.amazonaws.com",
       secure: true,
       auth: {
         user: aws_key,
-        pass: aws_secret,
+        pass: aws_secret
       },
       debug: true
     });
