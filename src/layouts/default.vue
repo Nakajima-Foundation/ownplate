554f666c4b9a173ab18db7c39c841253b21a1daa
Yuta Okajima <yuta.ok@gmail.com>
2021-04-09 14:56:50 -0700

    Add Tailwind Hack

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -5,7 +5,7 @@
     <!-- w-4 h-4 w-9 h-9 w-12 h-12 w-16 h-16 w-20 h-20 w-24 h-24 w-32 h-32 w-48 h-48 w-72 w-full h-full -->
     <!-- mx-2 mx-4 mt-1 mt-2 mt-4 mb-1 mb-2 mb-8 ml-1 ml-2 ml-4 mr-1 mr-2 mr-4 -->
     <!-- p-0 p-1 p-2 p-4 px-1 px-2 px-3 py-1 py-2 pt-0 pt-2 pt-3 pt-4 pt-6 pl-6 pb-2 pb-4 pb-6 -->
-    <!-- lg:mx-0 lg:mx-4 lg:mx-auto lg:mx-6 lg:mt-0 lg:mt-6 lg:ml-4 lg:mr-4 lg:max-w-2xl -->
+    <!-- lg:mx-0 lg:mx-4 lg:mx-auto lg:mx-6 lg:mt-0 lg:mt-6 lg:ml-4 lg:mr-4 lg:max-w-sm lg:max-w-2xl -->
 
     <!-- rounded rounded-md rounded-lg rounded-full rounded-r lg:rounded-lg -->
     <!-- border-solid border-2 border-t-2 -->
@@ -115,7 +115,7 @@
       </div>
 
       <!-- Find Restaurants -->
-      <div class="text-center mt-2" v-if="isCustomer || isAnonymous ">
+      <div class="text-center mt-2" v-if="isCustomer || isAnonymous">
         <router-link to="/r">
           <div
             class="inline-flex justify-center items-center rounded-full h-12 w-56 bg-op-teal text-white font-bold"
@@ -403,7 +403,7 @@ export default {
     },
     home_path() {
       return this.isAdmin ? "/admin/restaurants/" : "/r";
-    },
+    }
   },
   methods: {
     flash() {
