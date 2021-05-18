9c0b4747d446fc2c7062bd24260fc1f8b6ae128f
Isamu Arimoto <isamu@to-kyo.to>
2021-05-18 15:23:48 +0900

    update area data

--- src/app/user/RootPage.vue
+++ src/app/user/RootPage.vue
@@ -87,7 +87,8 @@ export default {
                   { name: "千葉県", id: 11 },
                   { name: "群馬県", id: 9 },
                   { name: "埼玉県", id: 10 },
-                  { name: "栃木県", id: 8 }
+                  { name: "栃木県", id: 8 },
+                  { name: "茨城県", id: 7 }
                 ]
               },
               {
@@ -103,6 +104,7 @@ export default {
                 items: [
                   { name: "新潟県", id: 14 },
                   { name: "富山県", id: 15 },
+                  { name: "石川県", id: 16 },
                   { name: "福井県", id: 17 }
                 ]
               },
@@ -111,7 +113,9 @@ export default {
                 items: [
                   { name: "京都府", id: 25 },
                   { name: "大阪府", id: 26 },
-                  { name: "兵庫県", id: 27 }
+                  { name: "兵庫県", id: 27 },
+                  { name: "滋賀県", id: 24 },
+                  { name: "和歌山県", id: 29 },
                   // { name: "奈良県", id: 28 },
                 ]
               },
@@ -119,6 +123,7 @@ export default {
                 name: "chugoku",
                 items: [
                   { name: "広島県", id: 33 },
+                  { name: "徳島県", id: 35 },
                   { name: "愛媛県", id: 37 }
                 ]
               },
@@ -127,7 +132,8 @@ export default {
                 items: [
                   { name: "福岡県", id: 39 },
                   { name: "長崎県", id: 41 },
-                  { name: "大分県", id: 43 }
+                  { name: "大分県", id: 43 },
+                  { name: "沖縄県", id: 46 }
                 ]
               }
             ]
