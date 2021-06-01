4ec55ea01940cea3dca5bba205d580e9d722cfde
Isamu Arimoto <isamu@to-kyo.to>
2021-06-01 13:05:37 +0900

    add smaregi view

---
+++ src/app/admin/Smaregi/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <a :href="authUrl">連携する</a>
+  </div>
+</template>
+
+<script>
+import { smaregi } from "@/config/project";
+
+export default {
+  name: "Restaurant",
+  data() {
+    return {
+      authUrl: `${smaregi.authUrl}?response_type=code&client_id=${smaregi.clientId}&scope=openid+email+offline_access`,
+    };
+  },
+};
+</script>
