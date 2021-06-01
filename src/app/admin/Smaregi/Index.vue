695cf34aaece4fc5122d59c9d1c95b006b795560
Isamu Arimoto <isamu@to-kyo.to>
2021-06-01 17:48:05 +0900

    add smaregi

--- src/app/admin/Smaregi/Index.vue
+++ src/app/admin/Smaregi/Index.vue
@@ -1,18 +1,38 @@
 <template>
   <div>
     <a :href="authUrl">連携する</a>
+    <div v-for="(shop, k) in shopList" :key="k">
+      {{shop.storeName}}
+    </div>
   </div>
 </template>
 
 <script>
 import { smaregi } from "@/config/project";
+import { functions } from "~/plugins/firebase.js";
 
 export default {
   name: "Restaurant",
   data() {
     return {
       authUrl: `${smaregi.authUrl}?response_type=code&client_id=${smaregi.clientId}&scope=openid+email+offline_access`,
+      shopList: [],
     };
   },
+  async mounted() {
+    try {
+      const smaregiAuth = functions.httpsCallable("smaregiStoreList");
+      this.isValidating = true;
+      const { data } = await smaregiAuth({
+        client_id: smaregi.clientId,
+      });
+      this.shopList = data.res;
+      console.log("smaregiAuth", data);
+    } finally {
+      this.isValidating = false;
+    }
+  },
+
+
 };
 </script>
