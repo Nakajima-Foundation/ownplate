feb277e3dbb504122ae39cc4f304dc5b47e3cede
Yuta Okajima <yuta.ok@gmail.com>
2021-08-02 17:54:25 -0700

    Fix the box layout of the What's New section

--- src/components/lp/news.vue
+++ src/components/lp/news.vue
@@ -1,16 +1,15 @@
 <template>
-  <div class="mx-6 mt-6 text-center lg:grid lg:grid-cols-3 lg:gap-4">
+  <div class="mx-6 mt-6 text-center ">
     <div class="bg-white rounded-lg p-6 mt-4 lg:mt-0">
-
-        <router-link to="/news">
-          <div
-            class="h-12 rounded-full bg-op-teal inline-flex items-center px-8 shadow"
+      <router-link to="/news">
+        <div
+          class="h-12 rounded-full bg-op-teal inline-flex items-center px-8 shadow"
+        >
+          <span class="text-white text-base font-bold">
+            {{ $t("lp.whatsnew") }}</span
           >
-            <span class="text-white text-base font-bold"
-                  > {{ $t("lp.whatsnew") }}</span
-            >
-          </div>
-        </router-link>
+        </div>
+      </router-link>
     </div>
   </div>
 </template>
