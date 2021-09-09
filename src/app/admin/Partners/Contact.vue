ac91c87fe68901d50477598f5b1abeb02ec44e13
Isamu Arimoto <isamu@to-kyo.to>
2021-09-09 10:26:24 +0900

    more partner

---
+++ src/app/admin/Partners/Contact.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div v-if="id==='legssystem'">
+      <pre>
+レグセントジョイラー株式会社
+企業・店舗ITソリューション/電子マネー推進事業部
+
+〒277-0023 千葉県柏市中央2-1-5 柏TYビル３階
+TEL: 04-7160-3281 FAX: 04-7160-3286
+MAIL: customer@legcent.jp
+
+受付時間: 10:00~18:00 土曜・日曜定休
+      </pre>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: String,
+  },
+}
+</script>
