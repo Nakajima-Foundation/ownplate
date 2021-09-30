4a75d5b0f742e9c214b5323d11bfc5c8c989c02a
Isamu Arimoto <isamu@to-kyo.to>
2021-09-30 10:32:24 +0900

    fix category layout

--- src/app/user/RestaurantPage.vue
+++ src/app/user/RestaurantPage.vue
@@ -75,9 +75,9 @@
             <div class="mx-6 mt-2 lg:mx-0">
               <template v-for="(title, key) in titleLists">
                 <a :href="`#${title.id}`"
-                   class="inline-flex justify-center items-center h-9 rounded-full bg-black bg-opacity-5 mx-1 mt-2"
+                   class="inline-flex justify-center items-center h-9 rounded-full bg-black bg-opacity-5 mx-2 mt-2"
                    >
-                  <div class="text-sm font-bold text-op-teal mx-3">
+                  <div class="text-sm font-bold text-op-teal mx-2">
                     {{title.name}}
                   </div>
                 </a>
