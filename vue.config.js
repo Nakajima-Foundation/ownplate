c33225d2ad3b5d5409fa56692edd072256d50862
Isamu Arimoto <isamu@to-kyo.to>
2021-11-29 08:31:49 +0900

    update packages

--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,14 @@ const path = require("path");
 const vueSrc = "./src";
 module.exports = {
   runtimeCompiler: true,
-  css: {
-    requireModuleExtension: true
-  },
   configureWebpack: {
     resolve: {
       alias: {
         "@": path.resolve(__dirname, vueSrc),
         "~": path.resolve(__dirname, vueSrc)
       },
-      extensions: ['.js', '.vue', '.json']
-    }
+      extensions: ['.js', '.vue', '.json'],
+      fallback: { "crypto": false }
+    },
   }
 };
