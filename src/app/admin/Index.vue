18a46261269dc376579eb065da8975a07d1168d0
isamu <isamu@to-kyo.to>
2022-03-20 19:55:40 +0900

    add smaregi link

--- src/app/admin/Index.vue
+++ src/app/admin/Index.vue
@@ -325,6 +325,29 @@
             </div>
           </div>
         </div>
+
+        <!-- Mail Magazine -->
+        <div class="mt-6">
+          <div class="text-xl font-bold text-black text-opacity-40 mb-2">
+            {{ $t("admin.thirdPartyService.title") }}
+          </div>
+
+          
+          
+          <div class="bg-white shadow rounded-lg p-4">
+            <div class="text-center mt-2">
+              <router-link to="/admin/smaregi/index"
+                class="h-12 rounded-full inline-flex items-center px-6 border-2 border-op-teal"
+                           >
+                <span class="text-op-teal text-base font-bold">{{
+                  $t("admin.thirdPartyService.openSmaregi")
+                  }}</span>
+                </router-link>
+            </div>
+
+          </div>
+        </div>
+
       </div>
     </div>
     <b-modal :active.sync="isOpen" :width="488">
