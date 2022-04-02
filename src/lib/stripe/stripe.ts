82afe02311d00f1143be0ee19e3ca8b8e90fd8cb
isamu <isamu@to-kyo.to>
2022-04-02 13:25:23 +0900

    stripe move to new firebase api

--- src/lib/stripe/stripe.ts
+++ src/lib/stripe/stripe.ts
@@ -1,5 +1,6 @@
-// @ts-ignore
-import { functions } from "~/plugins/firebase";
+import { functions } from "@/lib/firebase/firebase9";
+import { httpsCallable } from "firebase/functions";
+
 
 export const getStripeInstance = () => {
   const stripeAPIToken = process.env.VUE_APP_STRIPE_API_KEY;
@@ -7,21 +8,16 @@ export const getStripeInstance = () => {
   return Stripe(stripeAPIToken);
 };
 
-export const stripeCreateIntent = functions.httpsCallable("stripeCreateIntent");
-export const stripeConfirmIntent = functions.httpsCallable(
-  "stripeConfirmIntent"
-);
-export const stripeCancelIntent = functions.httpsCallable("stripeCancelIntent");
-export const stripePaymentCancelIntent = functions.httpsCallable(
-  "stripePaymentCancelIntent"
-);
-export const stripeConnect = functions.httpsCallable("stripeConnect");
-export const stripeDisconnect = functions.httpsCallable("stripeDisconnect");
-export const stripeVerify = functions.httpsCallable("stripeVerify");
-export const stripeUpdateCustomer = functions.httpsCallable(
-  "stripeUpdateCustomer"
-);
-export const stripeReceipt = functions.httpsCallable("stripeReceipt");
+export const stripeCreateIntent = httpsCallable(functions, "stripeCreateIntent");
+export const stripeConfirmIntent = httpsCallable(functions, "stripeConfirmIntent");
+export const stripeCancelIntent = httpsCallable(functions, "stripeCancelIntent");
+export const stripePaymentCancelIntent = httpsCallable(functions, "stripePaymentCancelIntent");
+
+export const stripeConnect = httpsCallable(functions, "stripeConnect");
+export const stripeDisconnect = httpsCallable(functions, "stripeDisconnect");
+export const stripeVerify = httpsCallable(functions, "stripeVerify");
+export const stripeUpdateCustomer = httpsCallable(functions, "stripeUpdateCustomer");
+export const stripeReceipt = httpsCallable(functions, "stripeReceipt");
 
 export const stripeActions = {
   capability_updated: 1,
