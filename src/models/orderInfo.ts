321e5f10d6d4e366add56b109647d92430266bd5
isamu <isamu@to-kyo.to>
2022-04-03 07:36:24 +0900

    pdf2 to ts

--- src/models/orderInfo.ts
+++ src/models/orderInfo.ts
@@ -1,9 +1,18 @@
+export interface OrderMenuItemData {
+  category1: string;
+  category2: string;
+  itemName: string;
+  price: number;
+}
 export interface OrderInfoData {
   name: string
   number: string;
   totalCharge: number;
+  total: number;
+  tax: number;
   // options: {[key: string]: [[key: string]: string]}
   timeEstimated: any; // TODO firestore timestamp
+  menuItems: {[key: string]: OrderMenuItemData};
 }
 
 
