a044adbc768117e54a0ca3b3634795792bcc5b43
isamu <isamu@to-kyo.to>
2022-04-04 20:50:37 +0900

    add shopowner model

---
+++ src/models/ShopOwner.ts
@@ -0,0 +1,14 @@
+export interface ShopOwnerData {
+  partners?: string[];
+  admin?: boolean;
+  hidePrivacy: boolean;
+};
+
+export interface PartnerData {
+  id: string;
+  name: string;
+  logo: string;
+  ask: boolean;
+};
+
+export class ShopOwner {}
