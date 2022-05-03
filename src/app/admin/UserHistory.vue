ffe9efa089da7baa61bb300a2e3880ec662a147d
isamu <isamu@to-kyo.to>
2022-05-04 06:55:25 +0900

    check

--- src/app/admin/UserHistory.vue
+++ src/app/admin/UserHistory.vue
@@ -1,99 +1,106 @@
 <template>
   <div>
-    <!-- Header -->
-    <div class="mt-6 mx-6 lg:flex lg:items-center">
-      <!-- Back and Preview -->
-      <div class="flex space-x-4">
-        <div class="flex-shrink-0">
-          <back-button
-            :url="`/admin/restaurants/${restaurantId()}/orders/` + orderId"
-          />
-        </div>
-        <div class="flex-shrink-0">
-          <router-link :to="'/r/' + restaurantId()">
-            <div
-              class="inline-flex justify-center items-center rounded-full h-9 bg-black bg-opacity-5 px-4"
-            >
-              <i class="material-icons text-lg text-op-teal mr-2">launch</i>
-              <span class="text-sm font-bold text-op-teal">{{
-                $t("admin.viewPage")
-              }}</span>
-            </div>
-          </router-link>
-        </div>
-      </div>
-
-      <!-- Photo and Name -->
-      <div class="mt-4 lg:mt-0 lg:flex-1 lg:flex lg:items-center lg:mx-4">
-        <div class="flex items-center">
-          <div class="flex-shrink-0 rounded-full bg-black bg-opacity-10 mr-4">
-            <img
-              :src="resizedProfileImage(shopInfo, '600')"
-              class="w-9 h-9 rounded-full object-cover"
+    <template v-if="notFound">
+      <not-found />
+    </template>
+    <div v-else>
+      <!-- Header -->
+      <div class="mt-6 mx-6 lg:flex lg:items-center">
+        <!-- Back and Preview -->
+        <div class="flex space-x-4">
+          <div class="flex-shrink-0">
+            <back-button
+              :url="`/admin/restaurants/${restaurantId()}/orders/` + orderId"
             />
           </div>
-          <div class="text-base font-bold">
-            {{ shopInfo.restaurantName }}
+          <div class="flex-shrink-0">
+            <router-link :to="'/r/' + restaurantId()">
+              <div
+                class="inline-flex justify-center items-center rounded-full h-9 bg-black bg-opacity-5 px-4"
+              >
+                <i class="material-icons text-lg text-op-teal mr-2">launch</i>
+                <span class="text-sm font-bold text-op-teal">{{
+                  $t("admin.viewPage")
+                }}</span>
+              </div>
+            </router-link>
           </div>
+        </div>
+
+        <!-- Photo and Name -->
+        <div class="mt-4 lg:mt-0 lg:flex-1 lg:flex lg:items-center lg:mx-4">
+          <div class="flex items-center">
+            <div class="flex-shrink-0 rounded-full bg-black bg-opacity-10 mr-4">
+              <img
+                :src="resizedProfileImage(shopInfo, '600')"
+                class="w-9 h-9 rounded-full object-cover"
+              />
+            </div>
+            <div class="text-base font-bold">
+              {{ shopInfo.restaurantName }}
+            </div>
+          </div>
+        </div>
+
+        <!-- Notifications -->
+        <div class="mt-4 lg:mt-0 flex-shrink-0">
+          <notification-index :shopInfo="shopInfo" />
         </div>
       </div>
 
-      <!-- Notifications -->
-      <div class="mt-4 lg:mt-0 flex-shrink-0">
-        <notification-index :shopInfo="shopInfo" />
-      </div>
-    </div>
-
-    <div v-if="orders[0] && orders[0].phoneNumber" class="mt-4 text-center">
-      <div class="text-xs font-bold">
-        {{ $t("sms.phonenumber") }}
-      </div>
-      <div class="text-base mt-1">
+      <div v-if="orders[0] && orders[0].phoneNumber" class="mt-4 text-center">
+        <div class="text-xs font-bold">
+          {{ $t("sms.phonenumber") }}
+        </div>
+        <div class="text-base mt-1">
+          <div>
+            <a :href="nationalPhoneURI" class="text-base font-bold">{{
+              nationalPhoneNumber
+            }}</a>
+          </div>
+          <div class="text-base">{{ orders[0].name }}</div>
+        </div>
         <div>
-          <a :href="nationalPhoneURI" class="text-base font-bold">{{
-            nationalPhoneNumber
-          }}</a>
+          {{ $t("order.orderTimes") }}:
+          {{ $tc("order.orderTimesUnit", userLog.counter || 0) }} /
+          {{ $t("order.cancelTimes") }}:
+          {{ $tc("order.cancelTimesUnit", userLog.cancelCounter || 0) }}
         </div>
-        <div class="text-base">{{ orders[0].name }}</div>
+        <div>
+          {{ $t("order.lastOrder") }}:
+          {{
+            userLog.lastOrder
+              ? moment(userLog.lastOrder.toDate()).format("YYYY/MM/DD HH:mm")
+              : "--"
+          }}
+        </div>
       </div>
-      <div>
-        {{ $t("order.orderTimes") }}:
-        {{ $tc("order.orderTimesUnit", userLog.counter || 0) }} /
-        {{ $t("order.cancelTimes") }}:
-        {{ $tc("order.cancelTimesUnit", userLog.cancelCounter || 0) }}
-      </div>
-      <div>
-        {{ $t("order.lastOrder") }}:
-        {{
-          userLog.lastOrder
-            ? moment(userLog.lastOrder.toDate()).format("YYYY/MM/DD HH:mm")
-            : "--"
-        }}
-      </div>
-    </div>
 
-    <!-- Orders -->
-    <div class="mx-6 mt-6 grid grid-cols-1 gap-2 lg:grid-cols-3 xl:grid-cols-4">
-      <ordered-info
-        v-for="order in orders"
-        :key="order.id"
-        @selected="orderSelected($event)"
-        :order="order"
-        :isSuperView="true"
-      />
-    </div>
+      <!-- Orders -->
+      <div
+        class="mx-6 mt-6 grid grid-cols-1 gap-2 lg:grid-cols-3 xl:grid-cols-4"
+      >
+        <ordered-info
+          v-for="order in orders"
+          :key="order.id"
+          @selected="orderSelected($event)"
+          :order="order"
+          :isSuperView="true"
+        />
+      </div>
 
-    <!-- More -->
-    <div class="mx-6 mt-6 text-center" v-if="last !== undefined">
-      <b-button :disabled="last === null" @click="next" class="b-reset-tw">
-        <div
-          class="inline-flex justify-center items-center w-48 h-9 px-4 rounded-full bg-black bg-opacity-5"
-        >
-          <div class="text-sm font-bold text-op-teal">
-            {{ $t("admin.order.more") }}
+      <!-- More -->
+      <div class="mx-6 mt-6 text-center" v-if="last !== undefined">
+        <b-button :disabled="last === null" @click="next" class="b-reset-tw">
+          <div
+            class="inline-flex justify-center items-center w-48 h-9 px-4 rounded-full bg-black bg-opacity-5"
+          >
+            <div class="text-sm font-bold text-op-teal">
+              {{ $t("admin.order.more") }}
+            </div>
           </div>
-        </div>
-      </b-button>
+        </b-button>
+      </div>
     </div>
   </div>
 </template>
@@ -108,11 +115,20 @@ import moment from "moment";
 import NotificationIndex from "./Notifications/Index";
 import { parsePhoneNumber, formatNational, formatURL } from "@/utils/phoneutil";
 
+import NotFound from "@/components/NotFound";
+
 export default {
   components: {
+    NotFound,
     OrderedInfo,
     BackButton,
     NotificationIndex,
+  },
+  props: {
+    shopInfo: {
+      type: Object,
+      required: true,
+    },
   },
   metaInfo() {
     return {
@@ -127,23 +143,19 @@ export default {
   },
   data() {
     return {
-      shopInfo: {},
       limit: 30,
       last: undefined,
       orders: [],
       userLog: {},
+      notFound: null,
     };
   },
   async created() {
     this.checkAdminPermission();
-    const restaurantDoc = await db
-      .doc(`restaurants/${this.restaurantId()}`)
-      .get();
-    if (!restaurantDoc.exists) {
-      // todo not found
-      return;
+    if (!this.checkShopAccount(this.shopInfo)) {
+      this.notFound = true;
+      return true;
     }
-    this.shopInfo = restaurantDoc.data();
     this.next();
 
     this.getUserLog();
