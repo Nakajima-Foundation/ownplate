31d5477a4adea77210c6b3275081fee4901e84f8
isamu <isamu@to-kyo.to>
2022-06-19 07:59:03 +0900

    smaregi component

--- src/app/admin/Index.vue
+++ src/app/admin/Index.vue
@@ -332,49 +332,8 @@
           </div>
         </div>
 
-        <!-- Mail Magazine -->
-        <div class="mt-6">
-          <div class="text-xl font-bold text-black text-opacity-40 mb-2">
-            {{ $t("admin.thirdPartyService.title") }}
-          </div>
-
-          <div class="bg-white shadow rounded-lg p-4">
-            <div class="text-center mt-2">
-              <router-link
-                to="/admin/smaregi/index"
-                class="h-12 rounded-full inline-flex items-center px-6 border-2 border-op-teal"
-              >
-                <span class="text-op-teal text-base font-bold">{{
-                  $t("admin.thirdPartyService.openSmaregi")
-                }}</span>
-              </router-link>
-            </div>
-
-            <div class="mt-4 text-center">
-              <a
-                href="https://docs.omochikaeri.com/manuals/smaregi.pdf"
-                target="_blank"
-                class="inline-flex justify-center items-center rounded-full h-9 bg-black bg-opacity-5 px-4"
-              >
-                <span class="text-sm font-bold text-op-teal">
-                  {{ $t("admin.thirdPartyService.smaregiManualLink") }}</span
-                >
-              </a>
-            </div>
-
-            <div class="mt-4 text-center">
-              <a
-                href="https://apps.smaregi.jp/apps/762"
-                target="_blank"
-                class="inline-flex justify-center items-center rounded-full h-9 bg-black bg-opacity-5 px-4"
-              >
-                <span class="text-sm font-bold text-op-teal">
-                  {{ $t("admin.thirdPartyService.smaregiAppMarket") }}
-                </span>
-              </a>
-            </div>
-          </div>
-        </div>
+        <Smaregi />
+        
       </div>
     </div>
     <b-modal :active.sync="isOpen" :width="488">
@@ -498,6 +457,8 @@ import PaymentSection from "@/app/admin/Payment/PaymentSection.vue";
 import MessageCard from "./Messages/MessageCard.vue";
 import PartnersContact from "./Partners/Contact.vue";
 
+import Smaregi from "@/app/admin/Index/Smaregi.vue";
+
 import { getShopOwner, getPartner } from "@/utils/utils";
 
 export default {
@@ -507,6 +468,7 @@ export default {
     RestaurantEditCard,
     MessageCard,
     PartnersContact,
+    Smaregi,
   },
   metaInfo() {
     return {
---
+++ src/app/admin/Index/Smaregi.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- Smaregi -->
+  <div class="mt-6">
+    <div class="text-xl font-bold text-black text-opacity-40 mb-2">
+      {{ $t("admin.thirdPartyService.title") }}
+    </div>
+    
+    <div class="bg-white shadow rounded-lg p-4">
+      <div class="text-center mt-2">
+        <router-link
+          to="/admin/smaregi/index"
+          class="h-12 rounded-full inline-flex items-center px-6 border-2 border-op-teal"
+          >
+          <span class="text-op-teal text-base font-bold">{{
+            $t("admin.thirdPartyService.openSmaregi")
+            }}</span>
+        </router-link>
+      </div>
+      
+      <div class="mt-4 text-center">
+        <a
+          href="https://docs.omochikaeri.com/manuals/smaregi.pdf"
+          target="_blank"
+          class="inline-flex justify-center items-center rounded-full h-9 bg-black bg-opacity-5 px-4"
+          >
+          <span class="text-sm font-bold text-op-teal">
+            {{ $t("admin.thirdPartyService.smaregiManualLink") }}</span
+                                                                   >
+        </a>
+      </div>
+      
+      <div class="mt-4 text-center">
+        <a
+          href="https://apps.smaregi.jp/apps/762"
+          target="_blank"
+          class="inline-flex justify-center items-center rounded-full h-9 bg-black bg-opacity-5 px-4"
+          >
+          <span class="text-sm font-bold text-op-teal">
+            {{ $t("admin.thirdPartyService.smaregiAppMarket") }}
+          </span>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
