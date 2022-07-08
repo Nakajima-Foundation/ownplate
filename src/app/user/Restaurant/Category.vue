316efc6864f8a0f46cb47599312bc17d6811d696
isamu <isamu@to-kyo.to>
2022-07-08 11:03:21 +0900

    add category

---
+++ src/app/user/Restaurant/Category.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <div>CATEGORY</div>
+  <div v-for="(cat, k) in categoryData" :key="k">
+    {{cat.name}} <img src="/android-chrome-192x192.png" class="w-12" />
+  </div>
+</div>
+</template>
+
+<script>
+import {
+  defineComponent,
+} from "@vue/composition-api";
+
+export default defineComponent({
+  props: {
+    categoryData: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup(props, ctx) {
+    return {};
+  }  
+});
+</script>
