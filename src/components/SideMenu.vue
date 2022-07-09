dcc6455a1150c51d7b768937bb27e0e23ac771a5
isamu <isamu@to-kyo.to>
2022-07-09 17:39:39 +0900

    add manual link

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -97,6 +97,18 @@
         </a>
       </div>
 
+      <!-- Delivery Manual -->
+      <div class="text-center mt-2">
+        <a
+          href="https://docs.omochikaeri.com/manuals/delivery.pdf"
+          target="_blank"
+          class="inline-flex justify-center items-center text-sm font-bold text-op-teal"
+          @click="handleClose()"
+          >
+          {{ $t("menu.deliveryManualLink") }}
+        </a>
+      </div>
+
       <!-- Tips -->
       <div class="text-center mt-2">
         <a
