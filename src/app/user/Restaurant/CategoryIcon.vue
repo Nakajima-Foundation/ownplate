293caad1eecdd4b72dffadc89c1034298491c9b1
Sachiko Nakayama <sacn25@gmail.com>
2022-07-28 15:19:32 +0900

    Change style for category button

--- src/app/user/Restaurant/CategoryIcon.vue
+++ src/app/user/Restaurant/CategoryIcon.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="fixed left-6 bottom-28 pb-1 inline-flex flex-col items-center justify-center h-16 w-16 cursor-pointer rounded-full bg-white shadow-lg"
+    class="fixed left-6 bottom-28 pb-1.5 inline-flex flex-col items-center justify-center h-20 w-20 cursor-pointer rounded-full bg-teal-50 border-2 border-op-teal shadow-lg"
     @click="openGroupCategory"
   >
-    <i class="material-icons text-2xl text-op-teal">tune</i>
+    <i class="material-icons text-2xl text-op-teal mb-0.5">tune</i>
     <div class="text-xs font-bold text-op-teal">
       {{ $t("button.category") }}
     </div>
