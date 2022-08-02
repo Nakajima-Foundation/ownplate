ca27e55d6f8f27c3d84afae9d145b3e45441f425
isamu <isamu@to-kyo.to>
2022-08-02 18:33:44 +0900

    update address

--- src/app/user/Address.vue
+++ src/app/user/Address.vue
@@ -1,52 +1,59 @@
 <template>
-  <div class="mx-6 mt-6 lg:max-w-2xl lg:mx-auto">
-    <!-- Title -->
-    <div class="text-xl font-bold text-black text-opacity-30">
-      {{ $t("profile.address") }}
+  <div>
+    <div class="mt-6 mx-6">
+      <back-button :url="basePath + '/u/profile/'" />
     </div>
 
-    <!-- Card -->
-    <div class="bg-white rounded-lg shadow mt-2 p-6">
-      <div class="text-sm font-bold">
-        {{ $t("order.ec.zip") }}
+    <div class="mx-6 mt-6 lg:max-w-2xl lg:mx-auto">
+      
+      <!-- Title -->
+      <div class="text-xl font-bold text-black text-opacity-30">
+        {{ $t("profile.address") }}
       </div>
-      <div class="mt-1 mb-2">
-        {{ customerInfo.zip || "----" }}
+      
+      <!-- Card -->
+      <div class="bg-white rounded-lg shadow mt-2 p-6">
+        <div class="text-sm font-bold">
+          {{ $t("order.ec.zip") }}
+        </div>
+        <div class="mt-1 mb-2">
+          {{ customerInfo.zip || "----" }}
+        </div>
+        <!-- address -->
+        <div class="text-sm font-bold">
+          {{ $t("order.ec.address") }}
+        </div>
+        <div class="mt-1 mb-2">
+          {{ customerInfo.prefecture }}
+          {{ customerInfo.address || "----" }}
+        </div>
+        <div class="text-sm font-bold">
+          {{ $t("order.ec.name") }}
       </div>
-      <!-- address -->
-      <div class="text-sm font-bold">
-        {{ $t("order.ec.address") }}
-      </div>
-      <div class="mt-1 mb-2">
-        {{ customerInfo.prefecture }}
-        {{ customerInfo.address || "----" }}
-      </div>
-      <div class="text-sm font-bold">
-        {{ $t("order.ec.name") }}
-      </div>
-      <div class="mt-1 mb-2">
-        {{ customerInfo.name || "----" }}
-      </div>
-      <div class="text-center">
-        <b-button @click="resetAddress" class="b-reset-tw">
-          <div
-            class="inline-flex justify-center items-center h-16 px-6 rounded-full bg-op-teal shadow"
-            style="min-width: 288px"
-          >
-            <div class="text-xl font-bold text-white">
-              {{ $t("profile.resetAddress") }}
+        <div class="mt-1 mb-2">
+          {{ customerInfo.name || "----" }}
+        </div>
+        <div class="text-center">
+          <b-button @click="resetAddress" class="b-reset-tw">
+            <div
+              class="inline-flex justify-center items-center h-16 px-6 rounded-full bg-op-teal shadow"
+              style="min-width: 288px"
+              >
+              <div class="text-xl font-bold text-white">
+                {{ $t("profile.resetAddress") }}
+              </div>
             </div>
-          </div>
-        </b-button>
-      </div>
-      <div class="text-center mt-2 font-bold">
-        * {{ $t("profile.resetAddressMessage") }}
-      </div>
-      <div class="text-center font-bold">
-        {{ $t("profile.resetAddressModify") }}
+          </b-button>
+        </div>
+        <div class="text-center mt-2 font-bold">
+          * {{ $t("profile.resetAddressMessage") }}
+        </div>
+        <div class="text-center font-bold">
+          {{ $t("profile.resetAddressModify") }}
+        </div>
       </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
@@ -54,7 +61,12 @@ import { defineComponent, computed, ref } from "@vue/composition-api";
 import { db } from "@/plugins/firebase";
 import { useBasePath } from "@/utils/utils";
 
+import BackButton from "@/components/BackButton.vue";
+
 export default defineComponent({
+  components: {
+    BackButton,
+  },
   setup(_, ctx) {
     const basePath = useBasePath(ctx.root);
     const customerInfo = ref({})
@@ -83,6 +95,7 @@ export default defineComponent({
     return {
       customerInfo,
       resetAddress,
+      basePath
     };
   },
 });
