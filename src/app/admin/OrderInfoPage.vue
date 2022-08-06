e191719dacaba2ff60f2eb2736b56739f3211aec
isamu <isamu@to-kyo.to>
2022-08-07 07:30:59 +0900

    AdminHeader

--- src/app/admin/OrderInfoPage.vue
+++ src/app/admin/OrderInfoPage.vue
@@ -7,76 +7,7 @@
       <!-- Header -->
       <div class="mt-6 mx-6 lg:flex lg:items-center">
         <!-- Back and Preview -->
-        <div class="flex space-x-4">
-          <div class="flex-shrink-0">
-            <back-button :url="parentUrl" />
-          </div>
-          <div class="flex-shrink-0">
-            <router-link :to="'/r/' + restaurantId()">
-              <div
-                class="inline-flex justify-center items-center rounded-full h-9 bg-black bg-opacity-5 px-4"
-              >
-                <i class="material-icons text-lg text-op-teal mr-2">launch</i>
-                <span class="text-sm font-bold text-op-teal">{{
-                  $t("admin.viewPage")
-                }}</span>
-              </div>
-            </router-link>
-          </div>
-        </div>
-
-        <!-- Photo and Name -->
-        <div class="mt-4 lg:mt-0 lg:flex-1 lg:flex lg:items-center lg:mx-4">
-          <div class="flex items-center">
-            <div class="flex-shrink-0 rounded-full bg-black bg-opacity-10 mr-4">
-              <img
-                :src="resizedProfileImage(shopInfo, '600')"
-                class="w-9 h-9 rounded-full object-cover"
-              />
-            </div>
-            <div class="text-base font-bold">
-              {{ shopInfo.restaurantName }}
-            </div>
-          </div>
-        </div>
-
-        <!-- Suspend Button -->
-        <div class="mt-4 lg:mt-0 lg:mr-4 flex-shrink-0">
-          <b-button
-            tag="router-link"
-            :to="`/admin/restaurants/${restaurantId()}/suspend`"
-            class="b-reset-tw"
-          >
-            <div
-              v-if="this.shopInfo.suspendUntil"
-              class="inline-flex justify-center items-center h-9 px-4 rounded-full bg-red-700 bg-opacity-5"
-            >
-              <i class="material-icons text-lg text-red-700 mr-2"
-                >remove_shopping_cart</i
-              >
-              <div class="text-sm font-bold text-red-700">
-                {{ $t("admin.order.suspending") }}
-              </div>
-            </div>
-
-            <div
-              v-else
-              class="inline-flex justify-center items-center h-9 px-4 rounded-full bg-black bg-opacity-5"
-            >
-              <i class="material-icons text-lg text-op-teal mr-2"
-                >remove_shopping_cart</i
-              >
-              <div class="text-sm font-bold text-op-teal">
-                {{ $t("admin.order.suspendSettings") }}
-              </div>
-            </div>
-          </b-button>
-        </div>
-
-        <!-- Notifications -->
-        <div class="mt-4 lg:mt-0 flex-shrink-0">
-          <notification-index :shopInfo="shopInfo" />
-        </div>
+        <AdminHeader :shopInfo="shopInfo" :backLink="parentUrl"/>
       </div>
 
       <!-- Body -->
@@ -648,13 +579,13 @@ import {
 } from "@/lib/stripe/stripe";
 import moment from "moment-timezone";
 
-import BackButton from "@/components/BackButton";
-import NotFound from "@/components/NotFound";
 import { ownPlateConfig } from "@/config/project";
-import NotificationIndex from "./Notifications/Index";
-import OrderInfo from "@/app/user/Order/OrderInfo";
-import OrderedItem from "@/app/admin/Order/OrderedItem";
-import CustomerInfo from "@/components/CustomerInfo";
+
+import NotFound from "@/components/NotFound.vue";
+import OrderInfo from "@/app/user/Order/OrderInfo.vue";
+import OrderedItem from "@/app/admin/Order/OrderedItem.vue";
+import CustomerInfo from "@/components/CustomerInfo.vue";
+import AdminHeader from "@/app/admin/AdminHeader.vue";
 
 import { costCal } from "@/utils/commonUtils";
 import { downloadOrderPdf, printOrder, data2UrlSchema } from "@/lib/pdf/pdf2";
@@ -673,10 +604,9 @@ const timezone = moment.tz.guess();
 
 export default {
   components: {
-    BackButton,
     OrderedItem,
-    NotificationIndex,
     OrderInfo,
+    AdminHeader,
     CustomerInfo,
     NotFound,
   },
