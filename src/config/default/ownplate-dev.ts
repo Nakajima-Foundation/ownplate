cf54f54b062435c8ab888eb8b63ab55ad1f6b874
isamu <isamu@to-kyo.to>
2022-08-08 15:24:54 +0900

    add mo config

--- src/config/default/ownplate-dev.ts
+++ src/config/default/ownplate-dev.ts
@@ -40,3 +40,4 @@ export const gtmID = "GTM-M8DN5VQ";
 export const GAPIKey = "AIzaSyBopNQwD1RT2k9dLqH6WYPWIkMZF3RWXMQ";
 
 export const mo_prefixes = ["mo", "ss"];
+export const moGtmID = "GTM-PLFRX7K";
