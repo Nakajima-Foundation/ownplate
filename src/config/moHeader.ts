26a4e9728df35478229d5d2557706d76da3ba0d1
isamu <isamu@to-kyo.to>
2022-08-29 13:08:01 +0900

    update gtag

--- src/config/moHeader.ts
+++ src/config/moHeader.ts
@@ -50,9 +50,19 @@ const link = [
   },
 ];
 
-const gtmHeadTag = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${moGtmID}');`;
+//const gtmHeadTag = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${moGtmID}');`;
 
-const gtmBodyTag = `<iframe src="https://www.googletagmanager.com/ns.html?id=${moGtmID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
+//  const gtmBodyTag = `<iframe src="https://www.googletagmanager.com/ns.html?id=${moGtmID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
+
+const gtmHeadTag = [
+  `<script async src="https://www.googletagmanager.com/gtag/js?id=${moGtmID}"></script>`,
+  '<script>',
+  '  window.dataLayer = window.dataLayer || [];',
+  '  function gtag(){dataLayer.push(arguments);}',
+  "  gtag('js', new Date());",
+  `  gtag('config', '${moGtmID}');`,
+  '</script>',
+].join("");
 
 const script = [
   { src: "https://js.stripe.com/v3/" },
@@ -62,6 +72,7 @@ const script = [
   },
 ];
 
+const gtmBodyTag = "";
 const noscript = [
   {
     hid: "gtmBody",
