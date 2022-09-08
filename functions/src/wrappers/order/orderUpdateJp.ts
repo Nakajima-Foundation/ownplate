b9e10ac455a3fff07023b56cb466d0cc919cf6e4
isamu <isamu@to-kyo.to>
2022-09-09 06:29:51 +0900

    add app check

--- functions/src/wrappers/order/orderUpdateJp.ts
+++ functions/src/wrappers/order/orderUpdateJp.ts
@@ -9,7 +9,13 @@ export default functions
   .region("asia-northeast1")
   .runWith({
     memory: "1GB" as "1GB",
+    allowInvalidAppCheckToken: true,
   })
   .https.onCall(async (data, context) => {
+    if (context.app == undefined) {
+      throw new functions.https.HttpsError(
+        'failed-precondition',
+        'The function must be called from an App Check verified app.')
+    }
     return await update(db, data, context);
   });
