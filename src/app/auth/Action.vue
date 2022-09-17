e78fe337146e5d88bd3e713d86cc4270510f6ff8
isamu <isamu@to-kyo.to>
2022-09-17 12:21:14 +0900

    reset password

---
+++ src/app/auth/Action.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-if="mode == 'resetPassword'">
+      <reset-password />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from "@vue/composition-api";
+
+import ResetPassword from "./Actions/ResetPassword.vue";
+
+export default defineComponent({
+  name: "AccountAction",
+  components: {
+    ResetPassword,
+  },
+  setup(_, ctx) {
+    const mode = computed(() => {
+      return ctx.root.$route.query.mode;
+    });
+    return { mode };
+  },
+});
+</script>
