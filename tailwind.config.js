98a33e020350470f9455e2d6e9e95d1d52ccf30c
isamu <isamu@to-kyo.to>
2022-09-21 10:42:31 +0900

    update css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'op-teal': '#0097a7',
+        'ownplate-yellow': '#fbc02d',
+      },
+    },
     screens: {
       xs: "375px",
       ...defaultTheme.screens,
