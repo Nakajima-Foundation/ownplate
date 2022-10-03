2ed1ddfa497682a90447bf981fc45bd17c96a273
isamu <isamu@to-kyo.to>
2022-10-03 11:50:57 +0900

    workaround numberOfMenus

--- src/app/admin/MenuListPage.vue
+++ src/app/admin/MenuListPage.vue
@@ -353,11 +353,11 @@ export default defineComponent({
       restaurant_detacher();
     });
 
-    const { menuObj, itemsObj, numberOfMenus, loadMenu } = useMenuAndTitle(
+    const { menuObj, itemsObj, numberOfMenus, loadMenu, isLoading } = useMenuAndTitle(
       menuRestaurantId,
       props.isInMo,
       category,
-      subCategory
+      subCategory,
     );
 
     const menuLists = computed(() => {
@@ -377,6 +377,18 @@ export default defineComponent({
     });
     loadMenu();
 
+    watch(isLoading, (value) => {
+      if (!props.isInMo) {
+        if (!value) { // finish load
+          if (numberOfMenus.value !== props.shopInfo.numberOfMenus) {
+            updateDoc(doc(db, `restaurants/${restaurantId.value}`), {
+               numberOfMenus: numberOfMenus.value,
+            });
+          }
+        }
+      }
+    });
+    
     const { toggle: publicFilter, switchToggle: publicFilterToggle } =
       useAdminConfigToggle("menuPublicFilter", uid.value, false);
 
