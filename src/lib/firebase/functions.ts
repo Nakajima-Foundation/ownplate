fe22125f410b3c3680fb87e382cfcde7b1315e40
isamu <isamu@to-kyo.to>
2022-10-14 08:34:30 +0900

    remove unused functions

--- src/lib/firebase/functions.ts
+++ src/lib/firebase/functions.ts
@@ -31,16 +31,6 @@ export const subAccountInvitationDeny = httpsCallable(
 
 export const lineValidate = httpsCallable(functionsJP, "lineValidate");
 
-// export const lineAuthenticate = httpsCallable(functions, "lineAuthenticate");
-
-// export const lineSetCustomClaim = httpsCallable(
-//   functions,
-//   "lineSetCustomClaim"
-// );
-
-// export const traceProcess = httpsCallable(functions, "traceProcess");
-
-
 export const superDispatch = httpsCallable(functionsJP, "superDispatch");
 
 export const superTwilio = httpsCallable(functionsJP, "superTwilio");
