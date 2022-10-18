afa2ceeb96a34cd1dc0c6eb34c70813973589ed6
isamu <isamu@to-kyo.to>
2022-10-18 14:39:33 +0900

    add faq

---
+++ src/app/common/FAQMo.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+  <div>
+    FAQ
+  </div>
+  <div v-for="(faq, k) in faqList" :key="k">
+    <div>
+      Q: {{faq.q}}
+    </div>
+    <div>
+      A: {{faq.a}}
+    </div>
+  </div>
+</div>
+  
+</template>
+
+<script>
+import {
+  defineComponent,
+} from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {
+
+    const faqList = [
+      {
+        q: "question1",
+        a: "answer1",
+      },
+      {
+        q: "question2",
+        a: "answer2",
+      }
+    ];
+    return {
+      faqList
+    };
+  },
+});
+
+</script>
