69ad83c88fa313eacab6d9e317f036bf45ab0107
Sachiko Nakayama <sacn25@gmail.com>
2022-10-19 10:32:22 +0900

    Add pickup label to admin

--- src/app/admin/Order/OrderedInfo.vue
+++ src/app/admin/Order/OrderedInfo.vue
@@ -76,6 +76,11 @@
         <div class="mr-2 text-sm" v-if="order.isDelivery">
           <i class="material-icons"> delivery_dining </i>
         </div>
+        <!--ToDo ピックアップ注文の場合にアイコンを表示する-->
+        <div class="mr-2 text-sm text-green-600" v-if="false">
+          <i class="material-icons"> local_mall </i>
+        </div>
+
         <div
           v-if="order.tip"
           class="flex-1 text-right text-xs font-bold text-blue-500"
