970ac675e127fe5f8d75fe078c7b056933774ba8
isamu <isamu@to-kyo.to>
2022-10-27 11:29:41 +0900

    fix csv storename

--- src/app/admin/ReportPage.vue
+++ src/app/admin/ReportPage.vue
@@ -293,6 +293,9 @@ export default defineComponent({
     
     const fieldNames = computed(() => {
       return fields.value.map((field) => {
+        if (props.isInMo && field === 'restaurantName') {
+          return ctx.root.$t("order.storeName");
+        }
         return ctx.root.$t(`order.${field}`);
       });
     });
