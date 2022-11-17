ed2a536b408ca5dbef5e4419dde5c000058f20eb
isamu <isamu@to-kyo.to>
2022-11-17 12:28:59 +0900

    mo suspend modal

--- src/app/admin/AdminHeader.vue
+++ src/app/admin/AdminHeader.vue
@@ -22,49 +22,92 @@
       </div>
     </div>
 
-    <!-- Suspend Button -->
     <div class="mt-4 flex space-x-4 lg:mt-0">
-      <o-button
-        tag="router-link"
-        :to="`/admin/restaurants/${restaurantId()}/suspend`"
-        class="b-reset-tw"
-        v-if="showSuspend"
-      >
-        <div
-          v-if="suspendUntil"
-          class="inline-flex h-9 items-center justify-center rounded-full bg-red-700 bg-opacity-5 px-4"
-        >
-          <i class="material-icons mr-2 text-lg text-red-700"
-            >remove_shopping_cart</i
+      <!-- Suspend Button -->
+      <template v-if="!isInMo">
+        <o-button
+          tag="router-link"
+          :to="`/admin/restaurants/${restaurantId()}/suspend`"
+          class="b-reset-tw"
+          v-if="showSuspend"
           >
+          <div
+            v-if="suspendUntil"
+            class="inline-flex h-9 items-center justify-center rounded-full bg-red-700 bg-opacity-5 px-4"
+            >
+            <i class="material-icons mr-2 text-lg text-red-700"
+               >remove_shopping_cart</i
+                                      >
+            <div class="text-sm font-bold text-red-700">
+              {{ $t("admin.order.suspending") }}
+            </div>
+          </div>
+          
+          <div
+            v-else
+            class="inline-flex h-9 items-center justify-center rounded-full bg-black bg-opacity-5 px-4"
+            >
+            <i class="material-icons mr-2 text-lg text-op-teal"
+               >remove_shopping_cart</i
+                                      >
+            <div class="text-sm font-bold text-op-teal">
+              {{ $t("admin.order.suspendSettings") }}
+            </div>
+          </div>
+        </o-button>
+      </template>
+      <template v-else>
+        <!-- for mo suspend -->
+        <div
+          v-if="false"
+          class="inline-flex h-9 items-center justify-center rounded-full bg-red-700 bg-opacity-5 px-4 cursor-pointer"
+          @click="openMoSuspendOnModal"
+          >
+          <i class="material-icons mr-2 text-lg text-red-700"
+               >remove_shopping_cart</i
+                                      >
           <div class="text-sm font-bold text-red-700">
             {{ $t("admin.order.suspending") }}
           </div>
         </div>
-
+        
         <div
           v-else
-          class="inline-flex h-9 items-center justify-center rounded-full bg-black bg-opacity-5 px-4"
-        >
-          <i class="material-icons mr-2 text-lg text-op-teal"
-            >remove_shopping_cart</i
+          class="inline-flex h-9 items-center justify-center rounded-full bg-black bg-opacity-5 px-4 cursor-pointer"
+          @click="openMoSuspendOffModal"
           >
-          <div class="text-sm font-bold text-op-teal">
-            {{ $t("admin.order.suspendSettings") }}
-          </div>
+          <i class="material-icons mr-2 text-lg text-op-teal"
+             >remove_shopping_cart</i
+                                    >
+            <div class="text-sm font-bold text-op-teal">
+              {{ $t("admin.order.suspendSettings") }}
+            </div>
         </div>
-      </o-button>
-
+      </template>
       <!-- Notifications -->
       <div>
         <notification-index :shopInfo="shopInfo" />
       </div>
     </div>
+    <o-modal :active.sync="isOpenMoSuspendOffModal" :width="488">
+      <div class="mx-2 my-6 rounded-lg bg-white p-6 shadow-lg">
+        <div class="text-xl font-bold text-black text-opacity-40">
+          on
+        </div>
+      </div>
+    </o-modal>
+    <o-modal :active.sync="isOpenMoSuspendOnModal" :width="488">
+      <div class="mx-2 my-6 rounded-lg bg-white p-6 shadow-lg">
+        <div class="text-xl font-bold text-black text-opacity-40">
+          off
+        </div>
+      </div>
+    </o-modal>
   </div>
 </template>
 
 <script>
-import { defineComponent, computed } from "@vue/composition-api";
+import { defineComponent, computed, ref } from "@vue/composition-api";
 import BackButton from "@/components/BackButton.vue";
 import NotificationIndex from "./Notifications/Index.vue";
 import PreviewLink from "@/app/admin/common/PreviewLink.vue";
@@ -108,8 +151,24 @@ export default defineComponent({
       }
       return false;
     });
+
+    // for mo
+    const isOpenMoSuspendOnModal = ref(false);
+    const openMoSuspendOnModal = () => {
+      isOpenMoSuspendOnModal.value = true;
+    };
+    const isOpenMoSuspendOffModal = ref(false);
+    const openMoSuspendOffModal = () => {
+      isOpenMoSuspendOffModal.value = true;
+    };
     return {
       suspendUntil,
+
+      isOpenMoSuspendOnModal,
+      openMoSuspendOnModal,
+
+      isOpenMoSuspendOffModal,
+      openMoSuspendOffModal,
     };
   },
 });
