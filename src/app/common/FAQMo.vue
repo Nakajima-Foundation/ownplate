d555c91d0b8d61d34eda068cb5473954e3180822
isamu <isamu@to-kyo.to>
2022-12-08 15:08:39 +0900

    update faq

--- src/app/common/FAQMo.vue
+++ src/app/common/FAQMo.vue
@@ -27,55 +27,57 @@
       </div>
 
       <div class="mt-6 rounded-lg bg-white px-4 py-2 shadow" ref="faq_box">
-        <div v-for="(faq, k) in faqList" :key="k" class="mb-6">
+        <div v-for="(faq, k) in faqList" :key="'base_' + k" class="mb-6">
           <div class="mt-4 mb-2 font-bold">
             <a :name="`faq_` + k">
               {{ faq.q }}
             </a>
           </div>
           <div
-            v-for="(answer, j) in faq.answers"
-            :key="j"
+            v-for="(answer, k) in faq.answers"
+            :key="'answer_' + k"
             class="mt-3 text-sm leading-relaxed"
           >
             {{ answer }}
           </div>
           <div
             v-for="(type1, i) in faq.type1"
-            :key="i"
+            :key="'type1_' + i"
             class="mt-3 -ml-2 text-sm leading-relaxed"
           >
             {{ type1 }}
           </div>
           <ul
-            v-for="(answer1, h) in faq.answers1"
-            :key="h"
             class="mt-3 ml-4 list-outside list-disc text-sm leading-relaxed"
           >
-            <li>
+            <li
+              v-for="(answer1, h) in faq.answers1"
+              :key="'answer_' + h"
+              >
               {{ answer1 }}
             </li>
           </ul>
           <div
             v-for="(type2, g) in faq.type2"
-            :key="g"
+            :key="'type2_' + g"
             class="mt-3 -ml-2 text-sm leading-relaxed"
           >
             {{ type2 }}
           </div>
           <ul
-            v-for="(answer2, f) in faq.answers2"
-            :key="f"
             class="mt-3 ml-4 list-outside list-disc text-sm leading-relaxed"
           >
-            <li>
+            <li
+              v-for="(answer2, f) in faq.answers2"
+              :key="'answer2_' + f"
+              >
               {{ answer2 }}
             </li>
           </ul>
           <div class="mt-3">
             <div
               v-for="(note, e) in faq.notes"
-              :key="e"
+              :key="'note2_' + e"
               class="mt-3 text-xs leading-relaxed text-black text-opacity-50"
             >
               {{ note }}
