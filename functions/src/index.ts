2375a1c56904aabb1fd0cc9a1f0804943b26913f
isamu <isamu@to-kyo.to>
2022-12-13 05:52:25 +0900

    Revert "remove unused stripe functions"
    This reverts commit 5735b453e891c95675ea63af8ddac6227622db9b.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -34,6 +34,9 @@ exportIfNeeded("stripeConnect", "stripe/stripeConnect", exports);
 exportIfNeeded("stripeDisconnect", "stripe/stripeDisconnect", exports);
 exportIfNeeded("stripeVerify", "stripe/stripeVerify", exports);
 
+exportIfNeeded("stripeCreateIntent", "stripe/stripeCreateIntent", exports); // TODO REMOVE
+exportIfNeeded("stripeConfirmIntent", "stripe/stripeConfirmIntent", exports); // TODO REMOVE
+
 exportIfNeeded("stripeCancelIntent", "stripe/stripeCancelIntent", exports);
 exportIfNeeded("stripePaymentCancelIntent", "stripe/stripePaymentCancelIntent", exports);
 
---
+++ functions/src/wrappers/stripe/stripeConfirmIntent.ts
@@ -0,0 +1,21 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+import { confirm } from "../../functions/stripe/confirmIntent";
+import { enforceAppCheck, secretKeys } from "../firebase";
+
+const db = admin.firestore();
+
+export default functions
+  .runWith({
+    memory: "1GB" as "1GB",
+    maxInstances: 50,
+    enforceAppCheck,
+    secrets: secretKeys,
+  })
+  .https.onCall(async (data, context) => {
+    if (context.app == undefined) {
+      throw new functions.https.HttpsError("failed-precondition", "The function must be called from an App Check verified app.");
+    }
+    return await confirm(db, data, context);
+  });
