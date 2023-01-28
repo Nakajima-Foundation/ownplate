226be56b65bd72862637d4a87f415a084d91d067
isamu <isamu@to-kyo.to>
2023-01-28 15:54:47 +0900

    update admin public rules

--- src/app/admin/Index/PaymentSection.vue
+++ src/app/admin/Index/PaymentSection.vue
@@ -149,16 +149,8 @@ export default {
         this.paymentInfo = snapshot.data();
         this.inStorePayment = this.paymentInfo.inStore;
       } else {
-        let stripe = null;
-        // ---- Backward compatibility
-        const refStripe = db.doc(`/admins/${this.uid}/public/stripe`);
-        const stripeData = (await refStripe.get()).data();
-        if (stripeData) {
-          stripe = stripeData.stripeAccount;
-        }
-        // ---- End of Backward compatibility
         refPayment.set({
-          stripe,
+          stripe: null,
           inStore: false,
         });
       }
