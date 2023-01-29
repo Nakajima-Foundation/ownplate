c45326081179b72adee44d2080c1c3c99bbfdeb4
isamu <isamu@to-kyo.to>
2023-01-29 21:02:56 +0900

    refactor user/OrderPage/BeforePaid/Map

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -837,3 +837,27 @@ export const orderTypeKey = (order: OrderInfoData, isInMo: boolean) => {
 };
 
 export const isDev = firebaseConfig.projectId === "ownplate-dev";
+
+
+// https://developers-jp.googleblog.com/2019/12/how-calculate-distances-map-maps-javascript-api.html
+export const haversine_distance = (lat1: number, lng1: number, lat2: number, lng2: number) => {
+  const R = 6371.071;
+  const rlat1 = lat1 * (Math.PI / 180);
+  const rlat2 = lat2 * (Math.PI / 180);
+  const difflat = rlat2 - rlat1;
+  const difflon = (lng2 - lng1) * (Math.PI / 180);
+  
+  const d =
+        2 *
+        R *
+        Math.asin(
+          Math.sqrt(
+            Math.sin(difflat / 2) * Math.sin(difflat / 2) +
+              Math.cos(rlat1) *
+                Math.cos(rlat2) *
+              Math.sin(difflon / 2) *
+              Math.sin(difflon / 2)
+          )
+        );
+  return Math.round(d * 1000);
+};
