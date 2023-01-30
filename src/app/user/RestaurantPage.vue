be56ec675e5b9cce167ea7406a0de5b6c21fe211
isamu <isamu@to-kyo.to>
2023-01-30 17:41:02 +0900

    close

--- src/app/user/RestaurantPage.vue
+++ src/app/user/RestaurantPage.vue
@@ -378,6 +378,7 @@
             <a
               :href="`#${title.id}`"
               class="mx-1 mt-2 inline-flex h-9 items-center justify-center rounded-full bg-black bg-opacity-5"
+              @click="closeCategory"
             >
               <div class="mx-2 text-sm font-bold text-op-teal">
                 {{ title.name }}
