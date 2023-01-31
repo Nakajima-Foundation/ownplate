6487d7d42f6c7b26c02e4eb6234ab777e621b1f4
isamu <isamu@to-kyo.to>
2023-02-01 06:04:53 +0900

    update bot

--- utils/faildbot.sh
+++ utils/faildbot.sh
@@ -1,4 +1,4 @@
 CIRCLE_SHA1=`git rev-parse HEAD` 
-curl -X POST --data-urlencode "payload={\"channel\": \"#p_demae\", \"username\": \"CiecleCI\", \"text\": \"ビルドに失敗しました ${CIRCLE_SHA1}\", \"icon_emoji\": \":robot_face:\"}" ${SLACKURL}
+curl -X POST --data-urlencode "payload={\"channel\": \"#p_demae\", \"username\": \"Github\", \"text\": \"ビルドに失敗しました ${CIRCLE_SHA1}\", \"icon_emoji\": \":fire:\"}" ${SLACKURL}
 
  
