bf45d8ee28ad4ae1478b44012cb70e6a0d0372ec
isamu <isamu@to-kyo.to>
2023-02-01 05:40:54 +0900

    fix news

--- src/app/user/News.vue
+++ src/app/user/News.vue
@@ -1,58 +1,56 @@
 <template>
   <div>
-    <template v-if="newsList === undefined">
-      <not-found />
-    </template>
-    <template v-else>
-      <!-- Header -->
-      <div class="mx-6 mt-6 flex items-center space-x-4">
-        <router-link :to="'/'">
-          <div
-            class="inline-flex h-9 items-center justify-center rounded-full bg-black bg-opacity-5 px-4"
+    <!-- Header -->
+    <div class="mx-6 mt-6 flex items-center space-x-4">
+      <router-link :to="'/'">
+        <div
+          class="inline-flex h-9 items-center justify-center rounded-full bg-black bg-opacity-5 px-4"
           >
-            <i class="material-icons mr-2 text-lg text-op-teal">arrow_back</i>
-            <div class="text-sm font-bold text-op-teal">
-              {{ $t("button.back") }}
-            </div>
+          <i class="material-icons mr-2 text-lg text-op-teal">arrow_back</i>
+          <div class="text-sm font-bold text-op-teal">
+            {{ $t("button.back") }}
           </div>
-        </router-link>
-      </div>
-
-      <!-- Body -->
-      <div class="mx-auto mt-6 max-w-screen-md px-6 text-base">
-        <div class="mt-6" v-for="(news, key) in newsList" :key="key">
-          <div class="rounded-lg bg-white p-4 shadow">
-            <div class="mt-2">
-              <div
-                class="article-list mt-6"
-                v-html="md.render(news.markdown)"
+        </div>
+      </router-link>
+    </div>
+    
+    <!-- Body -->
+    <div class="mx-auto mt-6 max-w-screen-md px-6 text-base">
+      <div class="mt-6" v-for="(news, key) in newsList" :key="key">
+        <div class="rounded-lg bg-white p-4 shadow">
+          <div class="mt-2">
+            <div
+              class="article-list mt-6"
+              v-html="md.render(news.markdown)"
               />
-            </div>
           </div>
         </div>
       </div>
-    </template>
+    </div>
   </div>
 </template>
 
 <script>
+import {
+  defineComponent,
+} from "@vue/composition-api";
+
 import MarkdownIt from "markdown-it";
 import newsList from "../admin/News/data";
 
-export default {
+export default defineComponent({
   metaInfo() {
     return {
       title: ["News", this.defaultTitle].join(" / "),
     };
   },
-  data() {
-    console.log(newsList);
+  setup() {
     return {
       md: new MarkdownIt(),
       newsList,
     };
   },
-};
+});
 </script>
 
 <style lang="css" scoped>
