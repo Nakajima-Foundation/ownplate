05ba905c51bc428dd4e7c42889663c7ccfffea8b
isamu <isamu@to-kyo.to>
2023-02-13 06:40:48 +0900

    vue3

--- src/app/admin/Smaregi/Callback.vue
+++ src/app/admin/Smaregi/Callback.vue
@@ -16,20 +16,22 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed } from "@vue/composition-api";
+import { defineComponent, ref, computed } from "vue";
 
 import { smaregiAuth } from "@/lib/firebase/functions";
 import { smaregi } from "@/config/project";
 
 import BackButton from "@/components/BackButton.vue";
 
+import { useRoute, useRouter } from "vue-router";
+
 export default defineComponent({
   components: {
     BackButton,
   },
-  setup(_, ctx) {
-    const route = ctx.root.$route;
-    const router = ctx.root.$router;
+  setup() {
+    const route = useRoute();
+    const router = useRouter();
 
     const code = route.query.code as string;
 
