66f7d27aee8faee73aeb2e53c2c952b55d2c8f8a
isamu <isamu@to-kyo.to>
2023-04-07 06:52:05 +0900

    PromotionMessage

---
+++ src/app/user/Restaurant/PromotionMessage2.vue
@@ -0,0 +1,26 @@
+<template>
+  <span>
+    <template v-if="promotion.discountMethod === 'amount'">
+      {{ $n(promotion.discountValue, "currency") }}値引きキャンペーン実施中！
+    </template>
+    <template v-if="promotion.discountMethod === 'ratio'">
+      {{ promotion.discountValue }}% 値引きキャンペーン実施中！
+    </template>
+  </span>
+</template>
+
+
+<script lang="ts">
+import {
+  defineComponent,
+} from "@vue/composition-api";
+
+export default defineComponent({
+  props: {
+    promotion: {
+      type: Object,
+      required: true,
+    },
+  }
+});
+</script>
--- src/app/user/RestaurantPage.vue
+++ src/app/user/RestaurantPage.vue
@@ -28,12 +28,15 @@
           />
       </div>
 
-			<!--ToDo ディスカウントバナー、カートに商品がない状態で常に表示、金額はデータを取得する -->
-			<div v-if="false">
-			<div class="mb-2 border-4 border-green-600 text-green-600 text-center font-bold fixed left-4 right-4 mx-auto max-w-lg cursor-pointer items-center rounded-full bg-white p-3 shadow-lg bottom-3 z-30 sm:bottom-8">
-				<div class="text-xs">¥1,000(税込)以上のご利用&事前クレジット決済で</div>
-				<div class="text-lg mt-0.5 -mb-0.5">¥300値引きキャンペーン実施中！</div>
-			</div>
+			<div v-if="totalQuantities === 0 && promotion && promotion.type === 'discount'">
+			  <div class="mb-2 border-4 border-green-600 text-green-600 text-center font-bold fixed left-4 right-4 mx-auto max-w-lg cursor-pointer items-center rounded-full bg-white p-3 shadow-lg bottom-3 z-30 sm:bottom-8">
+				  <div class="text-xs">
+            <PromotionMessage1 :promotion="promotion" />
+          </div>
+				  <div class="text-lg mt-0.5 -mb-0.5">
+            <PromotionMessage2 :promotion="promotion" />
+          </div>
+			  </div>
 			</div>
 
       <!-- category modal -->
@@ -488,6 +491,9 @@ import MoPickUp from "@/app/user/Restaurant/MoPickUp.vue";
 import MoPage from "@/app/user/Mo/MoPage.vue";
 import MoSetBanner from "@/app/user/Mo/MoSetBanner.vue";
 
+import PromotionMessage1 from "@/app/user/Restaurant/PromotionMessage1.vue";
+import PromotionMessage2 from "@/app/user/Restaurant/PromotionMessage2.vue";
+
 import { usePickupTime } from "@/utils/pickup";
 
 import liff from "@line/liff";
@@ -558,6 +564,9 @@ export default defineComponent({
     Titles,
     SubCategoryList,
 
+    PromotionMessage1,
+    PromotionMessage2,
+    
     MoPickUp,
     MoPage,
     MoSetBanner,
@@ -1221,6 +1230,18 @@ export default defineComponent({
     const pageId = computed(() => {
       return ctx.root.$route.params.pageId;
     });
+    const totalQuantities = computed(() => {
+      const ret = Object.values(orders.value).reduce((total, order) => {
+        return total + arraySum(order);
+      }, 0);
+      return ret;
+    });
+    const promotion = computed(() => {
+      if (props.promotions.length > 0) {
+        return props.promotions[0];
+      }
+      return null;
+    });
     return {
       itemLists,
       titleLists: filteredTitleLists,
@@ -1239,6 +1260,8 @@ export default defineComponent({
 
       totalPrice,
       prices,
+      totalQuantities,
+      promotion,
 
       isPreview,
 
