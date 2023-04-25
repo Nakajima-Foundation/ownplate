797ac3899b86b02bffc699c19bf17dcb5596676e
isamu <isamu@to-kyo.to>
2023-04-25 13:23:25 +0900

    promotion

--- src/app/user/OrderPage/BeforePaid.vue
+++ src/app/user/OrderPage/BeforePaid.vue
@@ -52,6 +52,7 @@
             :orderInfo="orderInfo || {}"
             :shippingCost="shippingCost"
             :groupData="groupData"
+            :mode="mode"
             :promotion="selectedPromotion"
             :enablePromotion="enablePromotion"
             :discountPrice="discountPrice"
@@ -214,30 +215,29 @@
                 }}
               </div>
 
-							<!--ToDo 注意書きメッセージの表示-->
 							<div v-if="selectedPromotion && selectedPromotion.paymentRestrictions === 'stripe'">
-								<!--ToDo おもちかえりの場合は以下のメッセージを表示-->
-								<div class="border-green-600 text-green-600 text-center font-bold mx-auto w-72 items-center mt-8 -mb-3 rounded-lg bg-green-600 bg-opacity-10 px-6 py-2">
+								<!-- おもちかえりの場合は以下のメッセージを表示-->
+								<div class="border-green-600 text-green-600 text-center font-bold mx-auto w-72 items-center mt-8 -mb-3 rounded-lg bg-green-600 bg-opacity-10 px-6 py-2" v-if="mode !== 'mo'">
 									<div class="text-xs">{{ $t("order.promotionNoteCard") }}</div>
 								</div>
-								<!--ToDo MobileOrderの場合は以下のメッセージを表示-->
-								<div v-if="false">
-								<div class="border-green-600 text-green-600 text-center font-bold mx-auto w-72 items-center mt-8 -mb-3 rounded-lg bg-green-600 bg-opacity-10 px-6 py-2">
-									<div class="text-xs">{{ $t("order.promotionNoteCardMo") }}</div>
-								</div>
+								<!-- MobileOrderの場合は以下のメッセージを表示-->
+								<div v-else>
+								  <div class="border-green-600 text-green-600 text-center font-bold mx-auto w-72 items-center mt-8 -mb-3 rounded-lg bg-green-600 bg-opacity-10 px-6 py-2">
+									  <div class="text-xs">{{ $t("order.promotionNoteCardMo") }}</div>
+								  </div>
 								</div>
 							</div>
 
 							<div v-if="selectedPromotion && selectedPromotion.paymentRestrictions === 'instore'">
-								<!--ToDo おもちかえりの場合は以下のメッセージを表示-->
-								<div class="border-green-600 text-green-600 text-center font-bold mx-auto w-72 items-center mt-8 -mb-3 rounded-lg bg-green-600 bg-opacity-10 px-6 py-2">
+								<!-- おもちかえりの場合は以下のメッセージを表示 -->
+								<div class="border-green-600 text-green-600 text-center font-bold mx-auto w-72 items-center mt-8 -mb-3 rounded-lg bg-green-600 bg-opacity-10 px-6 py-2"  v-if="mode !== 'mo'">
 									<div class="text-xs">{{ $t("order.promotionNoteStore") }}</div>
 								</div>
-								<!--ToDo MobileOrderの場合は以下のメッセージを表示-->
-								<div v-if="false">
-								<div class="border-green-600 text-green-600 text-center font-bold mx-auto w-72 items-center mt-8 -mb-3 rounded-lg bg-green-600 bg-opacity-10 px-6 py-2">
-									<div class="text-xs">{{ $t("order.promotionNoteStoreMo") }}</div>
-								</div>
+								<!-- MobileOrderの場合は以下のメッセージを表示 -->
+								<div v-else>
+								  <div class="border-green-600 text-green-600 text-center font-bold mx-auto w-72 items-center mt-8 -mb-3 rounded-lg bg-green-600 bg-opacity-10 px-6 py-2">
+									  <div class="text-xs">{{ $t("order.promotionNoteStoreMo") }}</div>
+								  </div>
 								</div>
 							</div>
 
