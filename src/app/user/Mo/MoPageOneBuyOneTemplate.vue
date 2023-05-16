57148719143d60a0f1c273930f8b60128d1b35ac
Sachiko Nakayama <sacn25@gmail.com>
2023-05-16 13:52:26 +0900

    1buy1 page design

--- src/app/user/Mo/MoPageOneBuyOneTemplate.vue
+++ src/app/user/Mo/MoPageOneBuyOneTemplate.vue
@@ -1,12 +1,10 @@
 <template>
   <div>
-    <div class="bg-blue-300">
-      買うと
+    <div>
       <slot name="header"></slot>
     </div>
     <slot name="menu"></slot>
-    <div class="bg-red-300">
-      もらえる
+    <div>
       <slot name="footer"></slot>
     </div>
   </div>
