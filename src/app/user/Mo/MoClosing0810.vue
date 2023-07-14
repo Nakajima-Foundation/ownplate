363142d1f8dc354e8f7408a70eda757b3eba1f9f
Sachiko Nakayama <sacn25@gmail.com>
2023-07-14 10:55:19 +0900

    Design for closing

---
+++ src/app/user/Mo/MoClosing0810.vue
@@ -0,0 +1,14 @@
+<!--8/10(木)〜8/18(金) 注文受付終了〜サービス終了まで-->
+	<div class="mx-6 rounded-lg bg-red-700 bg-opacity-10  text-red-700 sm:max-w-7xl xl:mx-auto mb-1">
+		<div class="py-4 px-5">
+			<div class="font-bold text-2xl text-center">
+				{{ $t("mobileOrder.closing.heading") }}
+			</div>
+			<div class="font-bold mt-3">
+				{{ $t("mobileOrder.closing.paragraph3") }}
+			</div>
+			<div class="font-bold mt-2">
+				{{ $t("mobileOrder.closing.paragraph1") }}
+			</div>
+		</div>
+	</div>
