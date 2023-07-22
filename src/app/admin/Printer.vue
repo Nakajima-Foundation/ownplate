134197766972c70f9f1420e80dd809545ecee1bf
isamu <isamu@to-kyo.to>
2023-07-23 08:14:44 +0900

    fix tag

--- src/app/admin/Printer.vue
+++ src/app/admin/Printer.vue
@@ -15,7 +15,6 @@
         :moPrefix="moPrefix"
       />
 
-    </div>
     <div>
       <div class="mx-6 mt-6 rounded-lg bg-black bg-opacity-5 p-4 text-center">
         <div class="pb-2 text-sm font-bold">
