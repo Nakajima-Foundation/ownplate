7b30882b79c2620099b552b4c4334c32ffb7067e
isamu <isamu@to-kyo.to>
2023-08-04 19:08:55 +0900

    mo

--- src/app/user/OrderPage/BeforePaid/SpecifiedCommercialTransactions.vue
+++ src/app/user/OrderPage/BeforePaid/SpecifiedCommercialTransactions.vue
@@ -15,7 +15,7 @@
       </div>
       <div class="mt-4">
         <!--for omochikaeri-->
-        <div v-if="!isInMo">
+        <div>
           <div class="text-sm font-bold text-black text-opacity-30">
             {{ $t("transactionsAct.cancellation") }}
           </div>
@@ -34,23 +34,11 @@
             </li>
           </ul>
         </div>
-        <!--for MobileOrder-->
-        <div v-if="isInMo">
-          <div class="text-sm font-bold text-black text-opacity-30">
-            {{ $t("transactionsAct.cancellationMo") }}
-          </div>
-
-          <ul class="ml-5 mt-1 list-outside list-disc text-sm">
-            <li>{{ $t("transactionsAct.cancellationDescription1Mo") }}</li>
-            <li>{{ $t("transactionsAct.cancellationDescription2Mo") }}</li>
-            <li>{{ $t("transactionsAct.cancellationDescription4Mo") }}</li>
-          </ul>
-        </div>
       </div>
 
       <div class="mt-4">
         <!--for omochikaeri-->
-        <div v-if="!isInMo">
+        <div>
           <div class="text-sm font-bold text-black text-opacity-30">
             {{ $t("transactionsAct.payment") }}
           </div>
@@ -59,23 +47,6 @@
           </ul>
         </div>
 
-        <!--for MobileOrder-->
-        <div v-if="isInMo">
-          <div class="text-sm font-bold text-black text-opacity-30">
-            {{ $t("transactionsAct.paymentNoteMo") }}
-          </div>
-          <ul class="ml-5 mt-1 list-outside list-disc text-sm">
-            <li>
-              {{ $t("transactionsAct.cancellationDescription5Mo") }}
-              <a
-                @click="openTransactionsAct()"
-                class="text-sm font-bold text-op-teal underline"
-                >{{ $t("transactionsAct.link") }}</a
-              >
-              {{ $t("transactionsAct.cancellationDescription6Mo") }}
-            </li>
-          </ul>
-        </div>
       </div>
     </div>
   </div>
@@ -83,7 +54,7 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { useIsInMo } from "@/utils/utils";
+
 export default defineComponent({
   props: {
     shopInfo: {
@@ -93,13 +64,11 @@ export default defineComponent({
   },
   emits: ["openTransactionsAct"],
   setup(_, ctx) {
-    const isInMo = useIsInMo();
     const openTransactionsAct = () => {
       ctx.emit("openTransactionsAct");
     };
     return {
       openTransactionsAct,
-      isInMo,
     };
   },
 });
