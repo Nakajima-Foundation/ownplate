98033f3c8d24bdcf027230d94881a6a3f10cf554
isamu <isamu@to-kyo.to>
2023-08-11 19:54:11 +0900

    line

--- src/app/admin/Restaurants/Line/Index.vue
+++ src/app/admin/Restaurants/Line/Index.vue
@@ -92,11 +92,13 @@
         :on-cancel="closeImage"
         v-model:active="imagePopup"
         >
-        <div class="bg-white">
+        <div class="bg-white p-4">
           <div v-if="iType == 'loginClientId'">
+            <div class="font-bold text-sm">LINEログインチャンネルのチャンネル基本設定のチャンネルIDをコピーして設定してください。</div>
             <img src="/images/lines/lineLoginId.png" class="p-4" />
           </div>
           <div v-if="iType == 'loginChannelSecret'">
+            <div class="font-bold text-sm">LINEログインチャンネルのチャンネル基本設定のチャンネルシークレットをコピーして設定してください。</div>
             <img src="/images/lines/lineLoginHeader.png" class="p-4" />
             <div class="bg-gray-200">
               {{ $t("admin.line.showBelow") }}
@@ -104,10 +106,12 @@
             <img src="/images/lines/lineLoginSecret.png" class="p-4" />
           </div>
           <div v-if="iType == 'messagingAccessToken'">
+            <div class="font-bold text-sm">LINE Messaging APIのMessaging API設定のチャンネルアクセストークンをコピーして設定してください。</div>
             <img src="/images/lines/lineMessageToken.png" class="p-4" />
             <img src="/images/lines/lineMessageHeader.png" class="p-4" />
           </div>
           <div v-if="iType == 'callback'">
+            <div class="font-bold text-sm">この値をコピーして、LINEログインチャンネルのLINEログイン設定の「コールバックURL」に設定してください。</div>
             <img src="/images/lines/lineLoginCallback.png" class="p-4" />
           </div>
         </div>
