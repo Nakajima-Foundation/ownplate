25a7de45d394cc005449782a0e764199f04c5b5a
isamu <isamu@to-kyo.to>
2023-10-08 17:17:28 +0900

    add document

--- src/app/admin/Docs/Article.vue
+++ src/app/admin/Docs/Article.vue
@@ -3,10 +3,12 @@
     Not Found
   </div>
   <div v-else>
-    <div
+    <div class="m-6">
+      <div
       class="article-list mt-6 text-base font-bold text-black text-opacity-30"
       v-html="md.render(article)"
-    />
+      />
+    </div>
   </div>
 </template>
 
@@ -16,12 +18,12 @@ import MarkdownIt from "markdown-it";
 
 import { useRoute } from "vue-router";
 
-import news from "../../../../docs/NEWS.md";
 import linenews from "../../../../docs/article230930_line_official_account/line_official_account.md";
+import soldout from "../../../../docs/article231007_out_of_stock/out_of_stock.md";
 
 const articles: { [key: string]: string } = {
-  news,
   article230930_line_official_account: linenews,
+  article231007_out_of_stock: soldout,
 };
 
 export default defineComponent({
@@ -43,21 +45,28 @@ export default defineComponent({
 
 <style lang="css" scoped>
 ::v-deep(h1) {
-  @apply m-6 text-base font-bold text-black text-opacity-60;
+  @apply my-4 text-base font-bold text-black text-opacity-60;
 }
 ::v-deep(h2) {
-  @apply m-6 text-base font-bold text-black text-opacity-60;
+  @apply my-4 text-base font-bold text-black text-opacity-60;
 }
 ::v-deep(h3) {
-  @apply m-6 text-base font-bold text-black text-opacity-60;
+  @apply my-4 text-base font-bold text-black text-opacity-60;
 }
 ::v-deep(h4) {
-  @apply m-6 text-base font-bold text-black text-opacity-60;
+  @apply my-4 text-base font-bold text-black text-opacity-60;
 }
 ::v-deep(p) {
-  @apply mx-6 my-2 text-base font-bold text-black text-opacity-60;
+  @apply my-2 text-base font-bold text-black text-opacity-60;
 }
 ::v-deep(ul) {
-  @apply mx-6 my-2 text-base font-bold text-black text-opacity-60;
+  @apply my-2 text-base font-bold text-black text-opacity-60;
+}
+::v-deep(ol) {
+  @apply my-2 text-base font-bold text-black text-opacity-60;
+}
+
+::v-deep(table) {
+  @apply my-2 text-base font-bold text-black text-opacity-60;
 }
 </style>
