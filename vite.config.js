c9cbad2c7c317ff90c4045fb4ee1a87b83712adb
isamu <isamu@to-kyo.to>
2023-12-23 11:21:25 +0900

    format

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,17 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue'
-import path from 'path';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  assetsInclude: ['**/*.md', '**/*.png'],
+  plugins: [vue()],
+  assetsInclude: ["**/*.md", "**/*.png"],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   optimizeDeps: {
-    include: [
-      "fast-deep-equal",
-    ],
+    include: ["fast-deep-equal"],
   },
   server: {
     port: 3000,
