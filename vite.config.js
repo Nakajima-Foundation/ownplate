182ccc07e39bc91b578e2733e3c67385a3ac37c5
isamu <isamu@to-kyo.to>
2024-07-05 18:02:52 +0900

    flatConfig

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import checker from "vite-plugin-checker";
 import Markdown from "unplugin-vue-markdown/vite";
 
 export default defineConfig({
   plugins: [
     vue({
       include: [/\.vue$/, /\.md$/],
+    }),
+    checker({
+      typescript: true,
+      // overlay: false,
+      eslint: {
+        useFlatConfig: true,
+        lintCommand: 'eslint "./src/**/*.{js,ts,vue}"',
+      },
     }),
     Markdown(),
   ],
