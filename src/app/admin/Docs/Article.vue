9d3ae32d5cbc26aa5b06d27df022707caf386501
isamu <isamu@to-kyo.to>
2024-07-31 05:17:44 +0900

    update document index

--- src/app/admin/Docs/Article.vue
+++ src/app/admin/Docs/Article.vue
@@ -13,6 +13,8 @@
       <linenews v-if="articleId === 'article230930_line_official_account'" />
       <soldout v-if="articleId === 'article231007_out_of_stock'" />
       <lunch_n_dinner v-if="articleId === 'article231019_lunch_n_dinner'" />
+      <lead_time  v-if="articleId === 'article240725_lead_time_management'" />
+      <configurations_of_dishes  v-if="articleId === 'article240727_configurations_of_dishes'" />
     </div>
   </div>
 </template>
@@ -27,10 +29,15 @@ import linenews from "../../../../docs/article230930_line_official_account/line_
 import soldout from "../../../../docs/article231007_out_of_stock/out_of_stock.md";
 import lunch_n_dinner from "../../../../docs/article231019_lunch_n_dinner/lunch_n_dinner.md";
 
+import lead_time from "../../../../docs/article240725_lead_time_management/lead_time_management.md";
+import configurations_of_dishes from "../../../../docs/article240727_configurations_of_dishes/configurations_of_dishes.md";
+
 const articles: { [key: string]: string } = {
   article230930_line_official_account: linenews,
   article231007_out_of_stock: soldout,
   article231019_lunch_n_dinner: lunch_n_dinner,
+  article240725_lead_time_management: lead_time,
+  article240727_configurations_of_dishes: configurations_of_dishes,
 };
 
 export default defineComponent({
@@ -39,6 +46,8 @@ export default defineComponent({
     linenews,
     soldout,
     lunch_n_dinner,
+    lead_time,
+    configurations_of_dishes,
   },
   setup() {
     const route = useRoute();
