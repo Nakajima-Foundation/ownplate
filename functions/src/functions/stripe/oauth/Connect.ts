0710e7b197f79f48214aa53523ecbcd970c5a66a
isamu <isamu@to-kyo.to>
2024-12-16 05:35:37 +0900

    update

--- functions/src/functions/stripe/oauth/Connect.ts
+++ functions/src/functions/stripe/oauth/Connect.ts
@@ -1,5 +1,5 @@
 import * as admin from "firebase-admin";
-import * as functions from "firebase-functions";
+import * as functions from "firebase-functions/v1";
 import * as utils from "../../../lib/utils";
 import { validatorStripeOAuthConnect } from "../../../lib/validator";
 
--- functions/src/wrappers/apiJP.ts
+++ functions/src/wrappers/apiJP.ts
@@ -1,4 +1,4 @@
-import * as functions from "firebase-functions";
+import * as functions from "firebase-functions/v1";
 import * as express from "../functions/express/express";
 import { secretKeys } from "./firebase";
 
