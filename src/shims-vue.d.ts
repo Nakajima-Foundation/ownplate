5438270d1e31ab477490c312257e182654cc8112
isamu <isamu@to-kyo.to>
2025-01-06 11:55:10 +0900

    update

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,6 +1,6 @@
 declare module "*.vue" {
-  import { DefineComponent } from "vue";
-  const component: ReturnType<typeof defineComponent>;
+import { DefineComponent } from "vue";
+  const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, unknown>;
   export default component;
 }
 
