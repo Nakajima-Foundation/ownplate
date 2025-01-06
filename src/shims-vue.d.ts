f0dee7a139358f67f992f177c61211e89ce25148
isamu <isamu@to-kyo.to>
2025-01-06 11:58:37 +0900

    update type

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,6 @@
 declare module "*.vue" {
 import { DefineComponent } from "vue";
+  // eslint-disable-next-line
   const component: DefineComponent<Record<string, unknown>, Record<string, unknown>, unknown>;
   export default component;
 }
