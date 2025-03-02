3e29d7850e0a6688e60f69170ba039e9a4a187bd
sora <msorakubo@msn.com>
2025-03-03 01:54:01 +0900

    perf: faster

--- src/app/m/ask.vue
+++ src/app/m/ask.vue
@@ -4,17 +4,11 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { defaultTitle } from "@/utils/utils";
-import { useHead } from "@unhead/vue";
 
 export default defineComponent({
   setup() {
-    useHead({
-      title: [defaultTitle, "Redirect"].join(" / "),
-    });
+    window.location.href =
+      "https://docs.google.com/forms/d/e/1FAIpQLSfGR4kk65ynfkCRGJsvJz01HZf7AU1nGLL9Rn9i4G9-qiW6MQ/viewform";
   },
-  mounted() {
-    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSfGR4kk65ynfkCRGJsvJz01HZf7AU1nGLL9Rn9i4G9-qiW6MQ/viewform";
-  }
 });
 </script>
--- src/app/m/note.vue
+++ src/app/m/note.vue
@@ -4,18 +4,11 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import { defaultTitle } from "@/utils/utils";
-import { useHead } from "@unhead/vue";
 
 export default defineComponent({
   setup() {
-    useHead({
-      title: [defaultTitle, "Redirect"].join(" / "),
-    });
+    const currentOrigin = window.location.origin;
+    window.location.href = `${currentOrigin}?utm_source=web&utm_medium=note&utm_campaign=note`;
   },
-	mounted() {
-		const currentOrigin = window.location.origin
-		window.location.href = `${currentOrigin}?utm_source=web&utm_medium=note&utm_campaign=note`
-  }
 });
 </script>
