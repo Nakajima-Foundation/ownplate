3c237023ed1abc4493504a63dcd8c927964ab8af
isamu <isamu@to-kyo.to>
2025-08-09 05:03:07 +0900

    loading color

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -6,7 +6,7 @@
     >
       <svg
         aria-hidden="true"
-        class="h-20 w-20 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+        class="h-20 w-20 animate-spin fill-op-teal/80 text-gray-200 dark:text-gray-600"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
