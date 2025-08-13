d89bc705b9c4fd0afb88f80fa66740e7c7f7e98e
isamu <isamu@to-kyo.to>
2025-08-14 04:40:28 +0900

    order status

--- src/components/OrderStatus.vue
+++ src/components/OrderStatus.vue
@@ -1,35 +1,35 @@
 <template>
   <div
     v-if="orderState === 'order_placed'"
-    class="rounded bg-yellow-500/10 px-3 py-1 text-yellow-500 transition-colors hover:bg-yellow-500/20 hover:text-yellow-600"
+    class="rounded bg-yellow-500/10 px-3 py-1 text-yellow-500"
   >
     <slot />
   </div>
 
   <div
     v-else-if="orderState === 'waiting_payment'"
-    class="rounded bg-pink-700/10 px-3 py-1 text-pink-700 transition-colors hover:bg-pink-700/20 hover:text-pink-800"
+    class="rounded bg-pink-700/10 px-3 py-1 text-pink-700"
   >
     <slot />
   </div>
 
   <div
     v-else-if="orderState === 'order_accepted'"
-    class="rounded bg-blue-500/10 px-3 py-1 text-blue-500 transition-colors hover:bg-blue-500/20 hover:text-blue-600"
+    class="rounded bg-blue-500/10 px-3 py-1 text-blue-500"
   >
     <slot />
   </div>
 
   <div
     v-else-if="orderState === 'ready_to_pickup'"
-    class="rounded bg-green-600/10 px-3 py-1 text-green-600 transition-colors hover:bg-green-600/20 hover:text-green-700"
+    class="rounded bg-green-600/10 px-3 py-1 text-green-600"
   >
     <slot />
   </div>
 
   <div
     v-else-if="orderState === 'transaction_complete'"
-    class="rounded bg-purple-600/10 px-3 py-1 text-purple-600 transition-colors hover:bg-purple-600/20 hover:text-purple-700"
+    class="rounded bg-purple-600/10 px-3 py-1 text-purple-600"
   >
     <slot />
   </div>
