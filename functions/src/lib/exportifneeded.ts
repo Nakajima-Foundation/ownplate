ed599be36a50fe554f9d80662b4100a9f6cff711
isamu <isamu@to-kyo.to>
2025-11-14 07:13:53 +0900

    add type

--- functions/src/lib/exportifneeded.ts
+++ functions/src/lib/exportifneeded.ts
@@ -2,7 +2,7 @@ function isFunctionCalled(functionName: string): boolean {
   return !process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === functionName;
 }
 
-export default function exportIfNeeded(functionName: string, fileName: string, exports: any): void {
+export default function exportIfNeeded(functionName: string, fileName: string, exports: Record<string, unknown>): void {
   if (isFunctionCalled(functionName)) {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     exports[functionName] = require(`../wrappers/${fileName}`).default;
--- functions/src/lib/netutils.ts
+++ functions/src/lib/netutils.ts
@@ -1,7 +1,7 @@
 import * as https from "https";
 import * as url from "url";
 
-export const request = (_url: string, _options: any, postData?: any): Promise<any> => {
+export const request = <T = unknown>(_url: string, _options: https.RequestOptions, postData?: string): Promise<T> => {
   const parsedURL = url.parse(_url);
   const options = Object.assign(
     {
@@ -34,7 +34,7 @@ export const request = (_url: string, _options: any, postData?: any): Promise<an
   });
 };
 
-export const postForm = (_url: string, params: any): Promise<any> => {
+export const postForm = <T = unknown>(_url: string, params: Record<string, string | number>): Promise<T> => {
   const postData = Object.keys(params)
     .map((key) => {
       return key + "=" + encodeURIComponent(params[key]);
@@ -53,7 +53,7 @@ export const postForm = (_url: string, params: any): Promise<any> => {
   );
 };
 
-export const postJson = (_url: string, _options: any, json: any): Promise<any> => {
+export const postJson = <T = unknown>(_url: string, _options: https.RequestOptions, json: unknown): Promise<T> => {
   const postData = JSON.stringify(json);
   console.log("postJson", postData);
   const headers = {
