8f8bf1dba13e18046f9da5f8ee9146ef86bb11f2
isamu <isamu@to-kyo.to>
2025-11-15 08:36:44 +0900

    delete card

--- functions/src/functions/stripe/deleteRestaurantCard.ts
+++ functions/src/functions/stripe/deleteRestaurantCard.ts
@@ -2,6 +2,7 @@ import * as admin from "firebase-admin";
 import { CallableRequest } from "firebase-functions/v2/https";
 import * as utils from "../../lib/utils";
 import { StripeDeleteRestaurantCardData } from "../../models/functionTypes";
+import { getStripeAccount } from "./intent";
 
 // Delete card information for a specific restaurant
 export const deleteRestaurantCard = async (
@@ -17,10 +18,33 @@ export const deleteRestaurantCard = async (
   }
 
   try {
-    // Delete the stripe information for this specific restaurant owner
+    // Retrieve the stripe information for this specific restaurant owner
     const refStripeSystem = db.doc(
       `/users/${customerUid}/owner/${ownerUid}/system/stripe`
     );
+    const stripeInfo = (await refStripeSystem.get()).data();
+
+    // Try to delete the payment method from Stripe if it exists
+    // Even if this fails, we continue to delete the local database records
+    if (stripeInfo && stripeInfo.payment_method) {
+      try {
+        const stripeAccount = await getStripeAccount(db, ownerUid);
+        const stripe = utils.get_stripe_v2();
+
+        // Detach the payment method from the customer
+        await stripe.paymentMethods.detach(
+          stripeInfo.payment_method,
+          { stripeAccount }
+        );
+        console.log(`Payment method ${stripeInfo.payment_method} detached successfully`);
+      } catch (stripeError) {
+        // Log the error but continue with database deletion
+        console.error("Failed to detach payment method from Stripe:", stripeError);
+      }
+    }
+
+    // Delete the stripe information for this specific restaurant owner
+    // This should always succeed even if Stripe API call failed
     await refStripeSystem.delete();
 
     const refStripeReadOnly = db.doc(
