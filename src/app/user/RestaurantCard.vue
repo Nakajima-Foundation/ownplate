357b816c7e640819d7726bf1111a4266a3a21c55
isamu <isamu@to-kyo.to>
2026-09-24 05:11:47 +0900

    update

--- src/app/user/RestaurantCard.vue
+++ src/app/user/RestaurantCard.vue
@@ -18,6 +18,7 @@
           :src="shopInfo.restProfilePhoto"
           alt=""
           class="h-12 w-12 rounded-full object-cover"
+          @error="smallImageErrorHandler"
         />
         <div class="ml-3">
           <div class="font-bold">{{ shopInfo.restaurantName }}</div>
@@ -79,7 +80,7 @@ import { db } from "@/lib/firebase/firebase9";
 import { doc, onSnapshot, getDoc, Unsubscribe } from "firebase/firestore";
 import { stripeDeleteRestaurantCard } from "@/lib/firebase/functions";
 import { RestaurantInfoData } from "@/models/RestaurantInfo";
-import { useUserData } from "@/utils/utils";
+import { smallImageErrorHandler, useUserData } from "@/utils/utils";
 import { useGeneralStore } from "@/store";
 import { useDialogStore } from "@/store/dialog";
 import moment from "moment";
@@ -191,6 +192,7 @@ export default defineComponent({
     });
 
     return {
+      smallImageErrorHandler,
       restaurantId,
       shopInfo,
       storedCard,
